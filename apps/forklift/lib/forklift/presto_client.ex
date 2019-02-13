84f089a31589bde2ea4fd5ba72d6fd19bbdda7a6
Timothy Regan <tregan@pillartechnology.com>
2019-02-13 15:52:34 -0500

    SMRT-893 Update PrestoClient to prefetch results
    This is currently necessary with Prestige to get the
    insert statement to execute. This might not be the
    desired behavior for Prestige long-term.
    
    co-authored-by: Tim Regan <tregan@pillartechnology.com>

--- apps/forklift/lib/forklift/presto_client.ex
+++ apps/forklift/lib/forklift/presto_client.ex
@@ -2,12 +2,17 @@ defmodule Forklift.PrestoClient do
   alias Forklift.Statement
 
   def upload_data(dataset_id, messages) do
-    schema = RegistryStore.get_schema(dataset_id)
+    RegistryStore.get_schema(dataset_id)
+    |> Statement.build(messages)
+    |> execute_statement()
 
-    statement = Statement.build(schema, messages)
-
-    Prestige.execute(statement, catalog: "hive", schema: "default")
+    :ok
   rescue
     e -> IO.inspect(e, label: "Unhandled Presto Client error")
+  end
+
+  defp execute_statement(statement) do
+    Prestige.execute(statement, catalog: "hive", schema: "default")
+    |> Prestige.prefetch() |> IO.inspect(label: "prestige return")
   end
 end
