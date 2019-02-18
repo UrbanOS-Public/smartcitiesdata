6c66aa7823528e583f9c8a0bf521f6ac5cac3a73
Mihail Chirita <mihailrc@gmail.com>
2019-02-18 14:30:45 -0500

    Initial commit

---
+++ apps/valkyrie/lib/valkyrie.ex
@@ -0,0 +1,18 @@
+defmodule Valkyrie do
+  @moduledoc """
+  Documentation for Valkyrie.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Valkyrie.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
