705267195bc0ae7e04e80e99ecb6a3ea7410eb3c
Timothy Regan <tregan@pillartechnology.com>
2019-02-21 08:34:33 -0500

    Better debug logs

--- apps/forklift/lib/forklift/presto_client.ex
+++ apps/forklift/lib/forklift/presto_client.ex
@@ -2,6 +2,7 @@ defmodule Forklift.PrestoClient do
   alias Forklift.Statement
 
   def upload_data(dataset_id, messages) do
+    System.get_env("PRESTO_URL") |> IO.inspect(label: "Uploading #{length(messages)} messages to...")
     Forklift.DatasetRegistryServer.get_schema(dataset_id)
     |> Statement.build(messages)
     |> execute_statement()
