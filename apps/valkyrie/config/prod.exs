7517139c85e23bc5906a7bd877a88f1010b7c51c
Timothy Regan <tregan@pillartechnology.com>
2019-03-05 13:24:39 -0500

    SMRT-969 Added prod configuration
    co-authored-by: jeffgrunewald <jeff@grunewalddesign.com>

--- apps/valkyrie/config/prod.exs
+++ apps/valkyrie/config/prod.exs
@@ -1,1 +1,14 @@
 use Mix.Config
+
+config :kaffe,
+  consumer: [
+    consumer_group: "valkyrie-consumer-group",
+    message_handler: Valkyrie.MessageHandler,
+    offset_reset_policy: :reset_to_earliest,
+    rebalance_delay_ms: 1000,
+    worker_allocation_strategy: :worker_per_topic_partition,
+    start_with_earliest_message: true
+  ],
+  producer: [
+    partition_strategy: :md5
+  ]
