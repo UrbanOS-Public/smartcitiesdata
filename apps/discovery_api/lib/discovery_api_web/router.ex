eb472f4d0629e41fc8c9a1df468ab985559987a4
paul-jacob <paul-jacob@users.noreply.github.com>
2019-03-06 10:35:49 -0500

    SMRT-959 Removes browser pipeline to prevent healthcheck from being logged. (#53)
    co-authored-by: Paul Linville <plinville@pillartechnology.com>

--- apps/discovery_api/lib/discovery_api_web/router.ex
+++ apps/discovery_api/lib/discovery_api_web/router.ex
@@ -16,17 +16,7 @@ defmodule DiscoveryApiWeb.Router do
     plug(:accepts, ["json"])
   end
 
-  pipeline :browser do
-    plug(:accepts, ["html"])
-    plug(:fetch_session)
-    plug(:fetch_flash)
-    plug(:protect_from_forgery)
-    plug(:put_secure_browser_headers)
-  end
-
   scope "/", DiscoveryApiWeb do
-    # Use the default browser stack
-    pipe_through(:browser)
     get("/healthcheck", HealthCheckController, :index)
   end
 
