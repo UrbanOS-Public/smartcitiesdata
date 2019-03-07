715d5eb744481d8f6da7fb0f5b54112ccad403ca
jeffgrunewald <jeff@grunewalddesign.com>
2019-03-07 15:04:39 -0500

    SMRT-986: Handling match errors parsing incompatible messages
    co-authored-by: Jake Prem <jakeprem@gmail.com>

--- apps/valkyrie/lib/valkyrie/message_handler.ex
+++ apps/valkyrie/lib/valkyrie/message_handler.ex
@@ -7,32 +7,35 @@ defmodule Valkyrie.MessageHandler do
   def handle_messages(messages) do
     Logger.info("#{__MODULE__}: Received #{length(messages)} messages.")
 
-    Enum.each(messages, fn %{key: key, value: value} ->
-      start_time = DataMessage.Timing.current_time()
-
-      {:ok, new_value} = DataMessage.new(value)
-
-      # ----
-      # Do validations here
-      # ----
-
-      {:ok, new_value} =
-        new_value
-        |> DataMessage.add_timing(
-          DataMessage.Timing.new(
-            :valkyrie,
-            :timing,
-            start_time,
-            DataMessage.Timing.current_time()
-          )
-        )
-        |> DataMessage.encode()
-
-      Kaffe.Producer.produce_sync(key, new_value)
-    end)
-
+    Enum.each(messages, &handle_message/1)
     Logger.info("#{__MODULE__}: All messages handled without crashing.")
 
     :ok
+  end
+
+  def handle_message(%{key: key, value: value}) do
+    start_time = DataMessage.Timing.current_time()
+
+    {:ok, new_value} = DataMessage.new(value)
+
+    # ----
+    # Do validations here
+    # ----
+
+    {:ok, new_value} =
+      new_value
+      |> DataMessage.add_timing(
+        DataMessage.Timing.new(
+          :valkyrie,
+          :timing,
+          start_time,
+          DataMessage.Timing.current_time()
+        )
+      )
+      |> DataMessage.encode()
+
+    Kaffe.Producer.produce_sync(key, new_value)
+  rescue
+    MatchError -> :ok
   end
 end
