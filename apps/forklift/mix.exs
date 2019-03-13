724ed59081d16daca61139b4e68b369730acdeb7
Mihail Chirita <mihailrc@gmail.com>
2019-03-13 09:25:08 -0400

    SMRT-972 Working Presto Compose
    Divo config pending
    
    co-authored-by: Timothy Regan <tregan@pillartechnology.com>

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -25,6 +25,7 @@ defmodule Forklift.MixProject do
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:jason, "~> 1.1"},
       {:kaffe, "~> 1.9.1"},
+      {:divo, "~> 0.2.1", only: [:dev, :test, :integration], organization: "smartcolumbus_os"},
       {:prestige, "~> 0.2.0", organization: "smartcolumbus_os"},
       {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false},
       {:scos_ex, "~> 1.0.0", organization: "smartcolumbus_os"},
