8216e3b723fc75096b7d6ae2f7e976f54e73ae66
Nathaniel Stevens <nstevens@pillartechnology.com>
2019-03-13 16:01:41 -0400

    SMRT-942 Refactors message writer test.
    co-authored-by: Paul Linville <plinville@pillartechnology.com>

--- apps/forklift/test/forklift/message_writer_test.exs
+++ apps/forklift/test/forklift/message_writer_test.exs
@@ -5,46 +5,34 @@ defmodule Forklift.MessageWriterTest do
   alias Forklift.{PrestoClient, MessageWriter, RedisClient}
 
   test "does all the things" do
-    id_a = "forklift:dataset:KeyA:5"
-    id_b = "forklift:dataset:KeyB:6"
+    messages_a = create_messages("forklift:dataset:KeyA:5")
+    messages_b = create_messages("forklift:dataset:KeyB:6")
 
-    messages_a =
-      Mockaffe.create_messages(:data, :basic, 2)
-      |> Enum.map(&Map.put(&1, :dataset_id, id_a))
-      |> Enum.map(&Jason.encode!/1)
-      |> Enum.map(&{id_a, &1})
-
-    messages_b =
-      Mockaffe.create_messages(:data, :basic, 2)
-      |> Enum.map(&Map.put(&1, :dataset_id, id_b))
-      |> Enum.map(&Jason.encode!/1)
-      |> Enum.map(&{id_b, &1})
-
-    messages = messages_a ++ messages_b
-    presto_messages = messages |> Enum.map(fn {k, v} -> v end)
-
-    allow(RedisClient.read_all_batched_messages(), return: messages)
+    allow(RedisClient.read_all_batched_messages(), return: messages_a ++ messages_b)
     allow(RedisClient.delete(any()), return: :ok)
 
-    m_a =
-      messages_a
-      |> Enum.map(&elem(&1, 1))
-      |> Enum.map(fn x ->
-        {:ok, msg} = SCOS.DataMessage.new(x)
-        msg
-      end)
+    expected_for_id_a = create_presto_expectation(messages_a)
+    expected_for_id_b = create_presto_expectation(messages_b)
 
-    m_b =
-      messages_b
-      |> Enum.map(&elem(&1, 1))
-      |> Enum.map(fn x ->
-        {:ok, msg} = SCOS.DataMessage.new(x)
-        msg
-      end)
-
-    expect(PrestoClient.upload_data("KeyA", m_a), return: :ok)
-    expect(PrestoClient.upload_data("KeyB", m_b), return: :ok)
+    expect(PrestoClient.upload_data("KeyA", expected_for_id_a), return: :ok)
+    expect(PrestoClient.upload_data("KeyB", expected_for_id_b), return: :ok)
 
     MessageWriter.handle_info(:work, %{})
+  end
+
+  def create_messages(dataset_id) do
+    Mockaffe.create_messages(:data, :basic, 2)
+    |> Enum.map(&Map.put(&1, :dataset_id, dataset_id))
+    |> Enum.map(&Jason.encode!/1)
+    |> Enum.map(&{dataset_id, &1})
+  end
+
+  def create_presto_expectation(messages) do
+    messages
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.map(fn x ->
+      {:ok, msg} = SCOS.DataMessage.new(x)
+      msg
+    end)
   end
 end
