c41aa171b40b969f8ba97e470f45c7b4c63e8ec6
Ben Adams <badams@pillartechnology.com>
2019-03-20 08:50:22 -0400

    SMRT-830 Refactored query controller to be more readable
    co-authored-by: Timothy Regan <tregan@pillartechnology.com>

--- apps/discovery_api/lib/discovery_api_web/controllers/dataset_query_controller.ex
+++ apps/discovery_api/lib/discovery_api_web/controllers/dataset_query_controller.ex
@@ -8,48 +8,51 @@ defmodule DiscoveryApiWeb.DatasetQueryController do
 
   def query(conn, %{"dataset_id" => dataset_id} = params, "csv") do
     %{:system_name => system_name} = DiscoveryApi.Data.Retriever.get_dataset(dataset_id)
-    where_clause = Map.get(params, "where", "") |> build_where()
-    order_by_clause = Map.get(params, "orderBy", "") |> build_orderby()
-    limit_clause = Map.get(params, "limit", "") |> build_limit()
-    group_by_clause = Map.get(params, "groupBy", "") |> build_groupby()
-    columns = Map.get(params, "columns", "*") |> String.replace(",", ", ")
 
-    column_names =
-      "describe hive.default.#{system_name}"
-      |> Prestige.execute()
-      |> Prestige.prefetch()
-      |> Enum.map(fn [col | _tail] -> col end)
+    column_names = get_column_names(system_name)
 
-    ["SELECT", columns, "FROM #{system_name}", where_clause, order_by_clause, limit_clause, group_by_clause]
-    |> Enum.reject(&is_nil/1)
-    |> Enum.join(" ")
+    params
+    |> build_query(system_name)
     |> Prestige.execute(catalog: "hive", schema: "default")
     |> map_data_stream_for_csv(column_names)
     |> stream_data(conn, system_name)
   end
 
-  defp build_where(""), do: nil
-
-  defp build_where(clause) do
-    "WHERE #{clause}"
+  defp get_column_names(system_name) do
+    "describe hive.default.#{system_name}"
+    |> Prestige.execute()
+    |> Prestige.prefetch()
+    |> Enum.map(fn [col | _tail] -> col end)
   end
 
-  defp build_orderby(""), do: nil
+  defp build_query(params, system_name) do
+    columns = Map.get(params, "columns", "*")
 
-  defp build_orderby(clause) do
-    "ORDER BY #{clause}"
+    ["SELECT"]
+    |> build_columns(columns)
+    |> Enum.concat(["FROM #{system_name}"])
+    |> add_clause("where", params)
+    |> add_clause("orderBy", params)
+    |> add_clause("limit", params)
+    |> add_clause("groupBy", params)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.join(" ")
   end
 
-  defp build_limit(""), do: nil
-
-  defp build_limit(clause) do
-    "LIMIT #{clause}"
+  defp add_clause(clauses, type, map) do
+    value = Map.get(map, type, "")
+    clauses ++ [build_clause(type, value)]
   end
 
-  defp build_groupby(""), do: nil
+  defp build_clause(_, ""), do: nil
+  defp build_clause("where", value), do: "WHERE #{value}"
+  defp build_clause("orderBy", value), do: "ORDER BY #{value}"
+  defp build_clause("limit", value), do: "LIMIT #{value}"
+  defp build_clause("groupBy", value), do: "GROUP BY #{value}"
 
-  defp build_groupby(clause) do
-    "GROUP BY #{clause}"
+  defp build_columns(clauses, columns) do
+    cleaned_columns = columns |> String.replace(",", ", ")
+    clauses ++ [cleaned_columns]
   end
 
   defp map_data_stream_for_csv(stream, table_headers) do
