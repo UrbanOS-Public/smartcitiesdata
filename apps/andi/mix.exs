808d1aecb5702b595e9fabe62c334348d54af967
Johnson Denen <johnson.denen@gmail.com>
2019-03-27 14:30:31 -0400

    Dependency management
    - Update smart_city_registry to v2.5.0
    - Add uuid
    - Add smart_city_test

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -41,9 +41,12 @@ defmodule Andi.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false},
-      {:smart_city_registry, "~> 2.2", organization: "smartcolumbus_os"},
+      {:smart_city_registry, "~> 2.5", organization: "smartcolumbus_os"},
       {:divo, "~> 1.0.1", only: [:dev, :integration], organization: "smartcolumbus_os"},
-      {:paddle, "~> 0.1.4"}
+      {:paddle, "~> 0.1.4"},
+      {:httpoison, "~> 1.5", only: :integration},
+      {:uuid, "~> 1.1"},
+      {:smart_city_test, "~> 0.2.0", organization: "smartcolumbus_os", only: [:test, :integration]}
     ]
   end
 end
