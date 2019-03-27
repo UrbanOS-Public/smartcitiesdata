fbf5fb5ef57e94f1d86c0b10586c8b3010d9781f
Paul Linville <plinville@pillartechnology.com>
2019-03-27 10:22:40 -0400

    Add required dep for hex and bump version
    co-authored-by: Paul Schwendenman <pschwendenman@pillartechnology.com>

--- apps/dead_letter/mix.exs
+++ apps/dead_letter/mix.exs
@@ -4,7 +4,7 @@ defmodule Yeet.MixProject do
   def project do
     [
       app: :yeet,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -23,6 +23,7 @@ defmodule Yeet.MixProject do
   defp deps do
     [
       {:credo, "~> 0.10", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev},
       {:jason, "~> 1.1"}
     ]
   end
