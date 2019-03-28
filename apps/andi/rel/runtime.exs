aec5e798c0026c7143088e6f1442b4b659f3335f
Johnson Denen <johnson.denen@gmail.com>
2019-03-28 11:20:26 -0400

    Pull LDAP info from ldap secret

--- apps/andi/rel/runtime.exs
+++ apps/andi/rel/runtime.exs
@@ -1,13 +1,14 @@
 use Mix.Config
 
-config :kaffe,
-  producer: [
-    endpoints: [{String.to_atom(System.get_env("KAFKA_BROKERS")), 9092}]
-  ]
-
 config :smart_city_registry,
   redis: [
     host: System.get_env("REDIS_HOST")
   ]
 
-config :paddle, Paddle, host: System.get_env("LDAP_HOST")
+config :andi,
+  ldap_user: System.get_env("LDAP_USER") |> Andi.LdapUtils.decode_dn!(),
+  ldap_pass: System.get_env("LDAP_PASS")
+
+config :paddle, Paddle,
+  host: System.get_env("LDAP_HOST"),
+  base: System.get_env("LDAP_BASE")
