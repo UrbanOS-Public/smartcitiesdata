e54330658a2cd380e97753cda9eb5809c100b00a
JarredOlson <jolson@pillartechnology.com>
2019-03-29 14:25:46 -0400

    Updating dependencies

--- apps/flair/mix.exs
+++ apps/flair/mix.exs
@@ -29,7 +29,7 @@ defmodule Flair.MixProject do
       {:kafka_ex, "~> 0.9"},
       {:jason, "~> 1.1"},
       {:statistics, "~> 0.6"},
-      {:smart_city_data, "~> 2.0", organization: "smartcolumbus_os"},
+      {:smart_city_data, "~> 2.1", organization: "smartcolumbus_os"},
       {:prestige, "~> 0.2.0", organization: "smartcolumbus_os"},
       {:distillery, "~> 2.0"},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
