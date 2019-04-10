5b871c65d594380446f80c8aa9d5d5320d069315
Jessie Morris <jmorris2@pillartechnology.com>
2019-04-10 16:27:21 -0400

    Removing helm-config due to duplicated resource values
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>
    co-authored-by: Paul Linville <plinville@pillartechnology.com>

--- apps/flair/mix.exs
+++ apps/flair/mix.exs
@@ -4,7 +4,7 @@ defmodule Flair.MixProject do
   def project do
     [
       app: :flair,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
