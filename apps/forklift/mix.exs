18e32fce0d3e0cd9bd4c091df1db69846d397899
Nathaniel Stevens <nstevens@pillartechnology.com>
2019-04-18 13:52:12 -0400

    SMRT-1116 Version bump
    co-authored-by: Jake Prem <jakeprem@gmail.com>

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -4,7 +4,7 @@ defmodule Forklift.MixProject do
   def project do
     [
       app: :forklift,
-      version: "0.1.0",
+      version: "0.1.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
