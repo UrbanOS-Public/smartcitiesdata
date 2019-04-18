938bf26534aca1de258835d57ed86efe018187f1
Richard Jones <rajones@hntb.com>
2019-04-18 13:32:25 -0400

    Required fields from subSchemas are handled
    co-authored-by: Austin Kucera <akucera@pillartechnology.com>

--- apps/flair/lib/flair/quality.ex
+++ apps/flair/lib/flair/quality.ex
@@ -5,14 +5,26 @@ defmodule Flair.Quality do
 
   def get_required_fields(dataset_id) do
     Dataset.get!(dataset_id).technical.schema
-    |> Enum.filter(&(Map.get(&1, :required, nil) == true))
-    |> Enum.map(&Map.get(&1, :name))
-
-    # Get schema from registry topic
+    |> Enum.map(fn field -> get_sub_required_fields(field) end)
+    |> List.flatten()
   end
 
-  def get_data_from_redis() do
-    # not sure about this one yet
+  defp get_sub_required_fields(required_field) do
+    cond do
+      Map.get(required_field, :required, false) == false ->
+        []
+
+      Map.get(required_field, :subSchema, nil) == nil ->
+        [Map.get(required_field, :name)]
+
+      true ->
+        sub_field =
+          required_field
+          |> Map.get(:subSchema)
+          |> Enum.map(fn sub_field -> get_sub_required_fields(sub_field) end)
+
+        sub_field ++ [Map.get(required_field, :name)]
+    end
   end
 
   def calculate_nulls do
--- apps/flair/test/unit/flair/quality_test.exs
+++ apps/flair/test/unit/flair/quality_test.exs
@@ -6,13 +6,60 @@ defmodule Flair.QualityTest do
   alias SmartCity.TestDataGenerator, as: TDG
 
   describe "get list of required fields correctly" do
-    test "get_schema/? returns correct schema for dataset_id" do
+    test "get_required_fields/1  returns nested required fields for dataset_id" do
       overrides = %{
         technical: %{
           schema: [
             %{name: "required field", type: "string", required: true},
             %{name: "optional field", type: "string", required: false},
-            %{name: "optional field2", type: "string"}
+            %{name: "optional field", type: "string"},
+            %{
+              name: "required nested field",
+              type: "map",
+              required: true,
+              subSchema: [
+                %{name: "required sub field", type: "string", required: true},
+                %{
+                  name: "next_of_kin",
+                  type: "map",
+                  required: true,
+                  subSchema: [
+                    %{name: "Not required", type: "string", required: false},
+                    %{name: "required_sub_schema_field", type: "date", required: true},
+                    %{name: "Not required not specified", type: "string"}
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      }
+
+      dataset = TDG.create_dataset(overrides)
+      allow(Dataset.get!(dataset.id), return: dataset)
+
+      assert [
+               "required field",
+               "required sub field",
+               "required_sub_schema_field",
+               "next_of_kin",
+               "required nested field"
+             ] ==
+               Flair.Quality.get_required_fields(dataset.id)
+    end
+
+    test "get_required_fields/1 does not return nested required fields whose parent is optional" do
+      overrides = %{
+        technical: %{
+          schema: [
+            %{name: "required field", type: "string", required: true},
+            %{
+              name: "optional parent field",
+              type: "map",
+              subSchema: [
+                %{name: "required sub field", type: "string", required: true}
+              ]
+            }
           ]
         }
       }
@@ -21,6 +68,28 @@ defmodule Flair.QualityTest do
       allow(Dataset.get!(dataset.id), return: dataset)
 
       assert ["required field"] == Flair.Quality.get_required_fields(dataset.id)
+    end
+
+    test "get_required_fields/1 does not return when there are no required fields" do
+      overrides = %{
+        technical: %{
+          schema: [
+            %{name: "optional parent field", type: "string", required: false},
+            %{
+              name: "optional parent field",
+              type: "map",
+              subSchema: [
+                %{name: "optional sub field", type: "string"}
+              ]
+            }
+          ]
+        }
+      }
+
+      dataset = TDG.create_dataset(overrides)
+      allow(Dataset.get!(dataset.id), return: dataset)
+
+      assert [] == Flair.Quality.get_required_fields(dataset.id)
     end
   end
 end
