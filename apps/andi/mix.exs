bd6fabfb9249fd73c48c78fabd5819a336f019c0
Brian Balser <bbalser@pillartechnology.com>
2019-04-22 09:35:00 -0400

    Bump version to 0.1.1

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_paths: Mix.env() |> test_paths(),
