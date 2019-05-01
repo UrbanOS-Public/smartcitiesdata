bf6311e9009a128ed93ce8fa319ec97e2d2dd3aa
Jake Prem <jakeprem@gmail.com>
2019-05-01 10:13:35 -0400

    Add initial docs and type specs to RailStream module

--- apps/reaper/lib/rail_stream.ex
+++ apps/reaper/lib/rail_stream.ex
@@ -1,12 +1,22 @@
 defmodule RailStream do
   @moduledoc """
   An abstraction over the `Stream` module to implement Railway Oriented Programming.
-  Jake's brilliant idea!
   """
+
+  @type element :: {:ok, any} | {:error, any} | any
+  @type modified_element :: element
+  @type original_value :: any
+  @type reason :: any
+  @type ignored :: any
 
   @doc """
+  Creates a stream that will apply the given `fun` to each non-error tuple on enumeration.
 
+  ## Parameters
+  - enum: An `Enumerable` of values, or :ok/:error tuples
+  - fun: A function that will be applied to each non-error tuple element of `enum`
   """
+  @spec map(Enumerable.t(), (element -> modified_element)) :: Enumerable.t()
   def map(enum, fun) when is_function(fun, 1) do
     Stream.map(enum, fn item ->
       case item do
@@ -18,8 +28,9 @@ defmodule RailStream do
   end
 
   @doc """
-
+  Creates a stream that will reject non-error tuple elements according to the given function on enumeration. Error tuples will be left in place.
   """
+  @spec reject(Enumerable.t(), (element -> boolean())) :: Enumerable.t()
   def reject(enum, fun) when is_function(fun, 1) do
     Stream.reject(enum, fn item ->
       case item do
@@ -31,8 +42,11 @@ defmodule RailStream do
   end
 
   @doc """
+  Executes the given function for each error tuple in the enum.
 
+  Useful for adding side effects and handling errors to the stream.
   """
+  @spec each_error(Enumerable.t(), (reason, original_value -> ignored)) :: Enumerable.t()
   def each_error(enum, fun) when is_function(fun, 2) do
     Stream.each(enum, fn item ->
       case item do
