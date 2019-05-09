fb58aab9685604e3174342d2358ddc00d015eeab
Benjamin Brewer <benjamin.brewer@accenture.com>
2019-05-09 13:23:12 -0400

    SMRT-1126 - update overrides to be fall backs to nil/empty in some cases
    co-authored-by: Kevin Patrick <kpatrick@pillartechnology.com>

--- apps/discovery_api/lib/discovery_api/data/mapper.ex
+++ apps/discovery_api/lib/discovery_api/data/mapper.ex
@@ -9,38 +9,37 @@ defmodule DiscoveryApi.Data.Mapper do
   @doc """
   Map a `SmartCity.Dataset` to a `DiscoveryApi.Data.Model`
   """
-  def to_data_model(%Dataset{} = dataset, %Organization{} = organization) do
+  def to_data_model(%Dataset{id: id, technical: tech, business: biz}, %Organization{} = organization) do
     %Model{
-      id: dataset.id,
-      name: dataset.technical.dataName,
-      title: dataset.business.dataTitle,
-      keywords: dataset.business.keywords,
-      modifiedDate: dataset.business.modifiedDate,
-      fileTypes: get_file_type(dataset.technical.sourceFormat),
-      description: dataset.business.description,
-      systemName: dataset.technical.systemName,
-      sourceUrl: dataset.technical.sourceUrl,
-      sourceType: dataset.technical.sourceType,
-      sourceFormat: dataset.technical.sourceFormat,
-      private: dataset.technical.private,
-      accessLevel: ternary(dataset.technical.private, "non-public", "public"),
-      contactName: dataset.business.contactName,
-      contactEmail: dataset.business.contactEmail,
-      license:
-        ternary(dataset.technical.private, dataset.business.license, "http://opendefinition.org/licenses/cc-by/"),
-      rights: dataset.business.rights,
-      homepage: dataset.business.homepage,
-      spatial: dataset.business.spatial,
-      temporal: dataset.business.temporal,
-      publishFrequency: dataset.business.publishFrequency,
+      id: id,
+      name: tech.dataName,
+      title: biz.dataTitle,
+      keywords: biz.keywords,
+      modifiedDate: biz.modifiedDate,
+      fileTypes: get_file_type(tech.sourceFormat),
+      description: biz.description,
+      systemName: tech.systemName,
+      sourceUrl: tech.sourceUrl,
+      sourceType: tech.sourceType,
+      sourceFormat: tech.sourceFormat,
+      private: tech.private,
+      accessLevel: ternary(tech.private, "non-public", "public"),
+      contactName: biz.contactName,
+      contactEmail: biz.contactEmail,
+      license: ternary(blank?(biz.license) and not tech.private, "http://opendefinition.org/licenses/cc-by/", biz.license),
+      rights: biz.rights,
+      homepage: biz.homepage,
+      spatial: biz.spatial,
+      temporal: biz.temporal,
+      publishFrequency: biz.publishFrequency,
       conformsToUri: "https://project-open-data.cio.gov/v1.1/schema/",
-      describedByUrl: dataset.business.describedByUrl,
-      describedByMimeType: dataset.business.describedByMimeType,
-      parentDataset: dataset.business.parentDataset,
-      issuedDate: dataset.business.issuedDate,
-      language: dataset.business.language,
-      referenceUrls: dataset.business.referenceUrls,
-      categories: dataset.business.categories,
+      describedByUrl: biz.describedByUrl,
+      describedByMimeType: biz.describedByMimeType,
+      parentDataset: biz.parentDataset,
+      issuedDate: biz.issuedDate,
+      language: biz.language,
+      referenceUrls: biz.referenceUrls,
+      categories: biz.categories,
       organization: organization.orgTitle,
       organizationDetails: %{
         id: organization.id,
@@ -65,4 +64,8 @@ defmodule DiscoveryApi.Data.Mapper do
 
   defp ternary(condition, success, _failure) when condition, do: success
   defp ternary(_condition, _success, failure), do: failure
+
+  defp blank?(nil), do: true
+  defp blank?(""), do: true
+  defp blank?(_), do: false
 end
