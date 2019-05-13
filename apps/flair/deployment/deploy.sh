960f52447c5754c0a11b45238ca6c312878eee95
Richard Jones <rajones@hntb.com>
2019-05-13 10:50:57 -0400

    Defaulting to prod values and overriding with environment specific if it exists
    co-authored-by: Ben Brewer <benjamin.brewer@accenture.com>

--- apps/flair/deployment/deploy.sh
+++ apps/flair/deployment/deploy.sh
@@ -4,10 +4,15 @@ current_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 GIT_COMMIT_HASH=${1:-latest}
 ENVIRONMENT=${2:-dev}
+OVERRIDE_FILE="${current_directory}/values_prod.yaml"
+
+if [ -f "${current_directory}/values_$ENVIRONMENT.yaml" ]; then
+  OVERRIDE_FILE="${current_directory}/values_$ENVIRONMENT.yaml"
+fi
 
 helm init --client-only
 helm upgrade --install flair \
   ${current_directory}/../chart \
   --namespace=streaming-services \
   --set image.tag="$GIT_COMMIT_HASH" \
-  --values ${current_directory}/values_$ENVIRONMENT.yaml
+  --values "${OVERRIDE_FILE}"
