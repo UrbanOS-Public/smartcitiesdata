23955410ef96bce0e0e5226ecf0be9ef5c2d1d8d
jeffgrunewald <jeff@grunewalddesign.com>
2019-05-14 09:59:56 -0400

    Undoing failed ldap integration test; checkpointing cred-retriever

--- apps/reaper/lib/reaper/credential_retriever.ex
+++ apps/reaper/lib/reaper/credential_retriever.ex
@@ -2,20 +2,59 @@ defmodule Reaper.CredentialRetriever do
   @moduledoc """
   Retrieves credentials for use in accessing restricted datasets.
   """
+  require Logger
 
   def retrieve(dataset_id) do
-    vault =
+    with {:ok, jwt} <- get_kubernetes_token(),
+         {:ok, vault} <- instantiate_vault_conn(jwt),
+         {:ok, credentials} <- Vault.read(vault, "secrets/smartcity/ingestion/#{dataset_id}"),
+         {:ok, decoded_credentials} <- Jason.decode(credentials) do
+      {:ok, decoded_credentials}
+    else
+      {:error, :enoent} ->
+        Logger.error("Dataset credentials not found")
+        {:error, :local_secret_not_found}
+
+      {:error, error} ->
+        Logger.error("Unable to connect to vault: #{error}")
+        {:error, :failed_to_authorize}
+    end
+
+    # {:error, ["Http adapter error", ":socket_closed_remotely"]} - host not found
+    # {:error, ["invalid role name \"demo-ro\""]}
+    # {:error, ["Missing credentials - role and jwt are required.", %{jwt: nil, role: "demo-role"}]}
+  end
+
+  defp get_kubernetes_token() do
+    case File.read("/var/run/secrets/kubernetes.io/serviceaccount/token") do
+      {:error, :enoent} ->
+        Logger.error("Secret token file not found")
+        {:error, :local_secret_not_found}
+
+      token ->
+        token
+    end
+  end
+
+  defp instantiate_vault_conn(token) do
+    try do
       Vault.new(
         engine: Vault.Engine.KVV1,
         auth: Vault.Auth.Kubernetes,
-        host: "http://vault.vault:8200"
+        host: "http://vault.vault:8200",
+        token_expires_at: set_login_ttl(20, :second)
       )
+      |> Vault.auth(%{role: "demo-role", jwt: token})
+    rescue
+      UndefinedFunctionError ->
+        Logger.error("Incorrect auth engine configuration")
+        {:error, :incorrect_auth_engine}
 
-    with {:ok, jwt} <- File.read("/var/run/secrets/kubernetes.io/serviceaccount/token"),
-         {:ok, token, _ttl} <- Vault.Auth.Kubernetes.login(vault, %{role: "demo-role", jwt: jwt}),
-         {:ok, credentials} <- Vault.read(vault, "secrets/smartcity/ingestion/#{dataset_id}"),
-         {:ok, decoded_credentials} <- Jason.decode(credentials) do
-      decoded_credentials
+      MatchError ->
+        Logger.error("Incorrect secrets host or token configuration")
+        {:error, :incorrect_secrets_host}
     end
   end
+
+  defp set_login_ttl(time, interval), do: NaiveDateTime.utc_now() |> NaiveDateTime.add(time, interval)
 end
--- apps/reaper/test/unit/reaper/credential_retriever_test.exs
+++ apps/reaper/test/unit/reaper/credential_retriever_test.exs
@@ -8,17 +8,39 @@ defmodule Reaper.CredentialRetrieverTest do
       dataset_id = 1
       role = "demo-role"
       jwt = "asjdhfsa"
-      token = "token"
       credentials = ~s({"username": "admin", "password": "1234"})
       credentials_decoded = Jason.decode!(credentials)
       vault = [engine: :engine, host: :host]
 
       allow File.read("/var/run/secrets/kubernetes.io/serviceaccount/token"), return: {:ok, jwt}
       allow Vault.new(any()), return: vault
-      allow Vault.Auth.Kubernetes.login(vault, %{role: role, jwt: jwt}), return: {:ok, token, 99_999}
+      allow Vault.auth(vault, %{role: role, jwt: jwt}), return: {:ok, vault}
       allow Vault.read(vault, "secrets/smartcity/ingestion/#{dataset_id}"), return: {:ok, credentials}
 
-      assert CredentialRetriever.retrieve(dataset_id) == credentials_decoded
+      assert CredentialRetriever.retrieve(dataset_id) == {:ok, credentials_decoded}
+    end
+
+    test "returns error when kubernetes token file is not found" do
+      dataset_id = 1
+
+      allow File.read("/var/run/secrets/kubernetes.io/serviceaccount/token"), return: {:error, :enoent}
+
+      assert CredentialRetriever.retrieve(dataset_id) == {:error, :local_secret_not_found}
+    end
+
+    test "returns error when unable to authorize" do
+      dataset_id = 1
+      role = "demo-role"
+      jwt = "asjdhfsa"
+      vault = [engine: :engine, host: :host]
+
+      allow File.read("/var/run/secrets/kubernetes.io/serviceaccount/token"), return: {:ok, jwt}
+      allow Vault.new(any()), return: vault
+
+      allow Vault.auth(vault, %{role: role, jwt: jwt}),
+        return: {:error, ["Http adapter error", ":socket_closed_remotely"]}
+
+      assert CredentialRetriever.retrieve(dataset_id) == {:error, :failed_to_authorize}
     end
   end
 end
