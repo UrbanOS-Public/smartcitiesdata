0ead2b53bd481c2e88e0897883972a81208ba522
Johnson Denen <johnson.denen@gmail.com>
2019-05-14 10:09:07 -0400

    Spaces use AND logic for title/description, OR logic for keywords

--- apps/discovery_api/lib/discovery_api/search/data_model_searchinator.ex
+++ apps/discovery_api/lib/discovery_api/search/data_model_searchinator.ex
@@ -3,17 +3,39 @@ defmodule DiscoveryApi.Search.DataModelSearchinator do
   alias DiscoveryApi.Data.Model
 
   def search(query \\ "") do
-    search_criteria = String.downcase(query)
-
     Model.get_all()
-    |> Enum.filter(&satisfies_search_criteria?(&1, search_criteria))
+    |> Enum.filter(fn model -> result?(model, query) end)
   end
 
-  defp satisfies_search_criteria?(%Model{} = model, search_criteria) do
+  defp result?(%Model{} = model, query) do
+    search_criteria = String.downcase(query)
+
     [model.title, model.description, model.organization, model.keywords]
-    |> List.flatten()
     |> Enum.reject(&is_nil/1)
+    |> Enum.any?(fn value -> satisfies_search_criteria?(value, search_criteria) end)
+  end
+
+  defp satisfies_search_criteria?(value, query) when is_list(value) do
+    partial_match?(value, query) || exact_match?(value, query)
+  end
+
+  defp satisfies_search_criteria?(value, query) do
+    value
+    |> String.downcase()
+    |> String.contains?(query)
+  end
+
+  defp partial_match?(value, query) do
+    for keyword <- Enum.map(value, &String.downcase/1),
+        search_term <- String.split(query) do
+      keyword == search_term
+    end
+    |> Enum.any?()
+  end
+
+  defp exact_match?(value, query) do
+    value
     |> Enum.map(&String.downcase/1)
-    |> Enum.any?(&String.contains?(&1, search_criteria))
+    |> Enum.any?(fn keyword -> keyword == query end)
   end
 end
--- apps/discovery_api/test/unit/discovery_api/search/data_model_searchinator_test.exs
+++ apps/discovery_api/test/unit/discovery_api/search/data_model_searchinator_test.exs
@@ -8,59 +8,65 @@ defmodule DiscoveryApi.Search.DataModelSearchinatorTest do
   alias DiscoveryApi.Data.Model
 
   describe "search/1" do
-    setup do
-      mock_models = [
+    data_test "search #{field} for #{search_term} - #{description}" do
+      models = [
         Helper.sample_model(%{
           id: 1,
-          title: "Jarred love pdf",
-          description: "Something super cool, sometimes",
-          organization: "Paul Co.",
-          keywords: ["stuff", "thIngs", "town"]
+          title: "This is a testable title - FINDME",
+          description: "An wrong description",
+          organization: "My organization is a match",
+          keywords: ["one"]
         }),
         Helper.sample_model(%{
           id: 2,
-          title: "Jessie hates/loves paperwork",
-          description: "Cool beans",
-          organization: "Mia Co.",
-          keywords: ["stuff town"]
+          title: "Test title should not be found",
+          description: "A description to match - FINDME",
+          organization: "My Organization",
+          keywords: ["two", "foo bar", "abc xyz"]
         }),
         Helper.sample_model(%{
           id: 3,
-          title: "This one has no description, sometimes",
-          organization: "Ben Co."
+          title: "This is a",
+          description: "A description",
+          organization: "Not the organization for me - FINDME",
+          keywords: ["three", "abc 123", "not the organization"]
         }),
-        Helper.sample_model(%{id: 4, organization: "testOrg"}),
-        Helper.sample_model(%{id: 5, organization: "Bogus"})
+        Helper.sample_model(%{
+          id: 4,
+          title: "Blah blah blah",
+          description: "",
+          organization: "",
+          keywords: ["findme"]
+        })
       ]
 
-      allow(Model.get_all(), return: mock_models)
-      :ok
-    end
+      allow(Model.get_all(), return: models)
 
-    data_test("partial matches #{description}") do
-      results = DataModelSearchinator.search(search_term)
+      results =
+        search_term
+        |> DataModelSearchinator.search()
+        |> Enum.map(&Map.get(&1, :id))
 
-      actual_ids = Enum.map(results, &Map.get(&1, :id))
-
-      assert expected_ids == actual_ids, error_diff(description, expected_ids, actual_ids)
+      assert expected == results, error_diff(description, field, expected, results)
 
       where([
-        [:search_term, :expected_ids, :description],
-        ["love", [1, 2], "based on same field (title)"],
-        ["super cool", [1], "spaces are ANDs"],
-        ["jarred sometimes", [], "no total match"],
-        ["jaRreD", [1], "based on a field, case insensitive"],
-        ["description", [3], "based on title"],
-        ["super", [1], "based on description"],
-        ["testor", [4], "based on organization"],
-        ["stuff town", [2], "based on keywords, exact match"],
-        ["Ben", [3], "when model is missing a field (description)"],
-        ["asdfasdfasdf", [], "are not returned when no matches are found"]
+        [:search_term, :field, :expected, :description],
+        ["this is a", "title", [1, 3], "spaces use AND logic"],
+        ["this IS a tEst", "title", [1], "case insensitive"],
+        ["a description", "description", [2, 3], "spaces use AND logic"],
+        ["a description To MATCH", "description", [2], "case insensitive"],
+        ["my organization", "organization", [1, 2], "spaces use AND logic"],
+        ["my Organization IS a Match", "organization", [1], "case insensitive"],
+        ["one three", "keywords", [1, 3], "spaces use OR logic"],
+        ["foo bar", "keywords", [2], "exact query match"],
+        ["abc", "keywords", [], "no match"],
+        ["findme", "all", [1, 2, 3, 4], "search across all fields"],
+        ["not the organization", "organization/keywords", [3], "one result per dataset"]
       ])
     end
   end
 
-  defp error_diff(description, expected, actual) do
-    "failed for case: #{description} - #{inspect(expected)} v #{inspect(actual)}"
+  defp error_diff(description, field, expected, actual) do
+    "failed for case: #{description} (#{field}) - #{inspect(expected)} v #{inspect(actual)}"
   end
 end
