b1f3b0c35cd7780940cea12b982b56bd0860f942
jakeprem <jakeprem@users.noreply.github.com>
2019-06-03 14:10:30 -0400

    Update version
    No code changes, just verifying release infrastructure

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_paths: Mix.env() |> test_paths(),
