d629974c38695151afb0f1c05784c1cc814efcb2
badams-scos <benjamin.adams@accenture.com>
2019-06-05 15:29:11 -0400

    SMRT-1297 - dry the test up a little
    co-authored-by: Benjamin Brewer <benjamin.brewer@accenture.com>

--- apps/discovery_api/test/unit/discovery_api/stats/stats_calculator_test.exs
+++ apps/discovery_api/test/unit/discovery_api/stats/stats_calculator_test.exs
@@ -7,8 +7,8 @@ defmodule DiscoveryApi.Stats.StatsCalculatorTest do
 
   @dataset_id "eaad54e8-fcb6-4f0e-99ac-bf51887ed102"
 
-  describe "produce_completeness_stats/1" do
-    test "Writes stats data to redis for non-remote datasets" do
+  describe "produce_completeness_stats/1 positive cases" do
+    setup do
       dataset = mock_non_remote_dataset()
 
       allow(Dataset.get_all!(), return: [dataset], meck_options: [:passthrough])
@@ -23,28 +23,40 @@ defmodule DiscoveryApi.Stats.StatsCalculatorTest do
       )
 
       keys = ["forklift:last_insert_date:#{dataset.id}", "discovery_api:completion_calculated_date:#{dataset.id}"]
+
+      stats = %{
+        id: dataset.id,
+        completeness: 0.8333333333333334,
+        record_count: 3,
+        fields: %{
+          "name" => %{count: 3, required: false},
+          "age" => %{count: 2, required: false}
+        }
+      }
+
+      {:ok, %{dataset: dataset, keys: keys, stats: stats}}
+    end
+
+    test "Writes stats to redis for non-remote datasets", %{dataset: dataset, keys: keys, stats: stats} do
       allow(Persistence.get_many(keys), return: ["2019-06-05T14:01:36.466729Z", "2019-06-05T13:59:09.630290Z"])
 
       StatsCalculator.produce_completeness_stats()
 
-      assert_called(
-        Persistence.persist(
-          "discovery-api:stats:#{dataset.id}",
-          %{
-            id: dataset.id,
-            completeness: 0.8333333333333334,
-            record_count: 3,
-            fields: %{
-              "name" => %{count: 3, required: false},
-              "age" => %{count: 2, required: false}
-            }
-          }
-        )
-      )
-
+      assert_called(Persistence.persist("discovery-api:stats:#{dataset.id}", stats))
       assert_called(Persistence.persist("discovery-api:completeness_calculated_date:#{dataset.id}", any()))
     end
 
+    test "Writes stats to redis when data has loaded, but no stats have been calculated", %{dataset: dataset, keys: keys, stats: stats} do
+      allow(Persistence.get_many(keys), return: ["2019-06-05T13:59:09.630290Z", nil])
+
+      StatsCalculator.produce_completeness_stats()
+
+      assert_called(Persistence.persist("discovery-api:stats:#{dataset.id}", stats))
+      assert_called(Persistence.persist("discovery-api:completeness_calculated_date:#{dataset.id}", any()))
+    end
+  end
+
+  describe "produce_completeness_stats/1 negative cases" do
     test "Does not calculate statistics for remote datasets" do
       dataset = mock_remote_dataset()
 
@@ -107,46 +119,9 @@ defmodule DiscoveryApi.Stats.StatsCalculatorTest do
 
       refute_called(Persistence.persist("discovery-api:completeness_calculated_date:#{dataset.id}", any()))
     end
-
-    test "Works when forklift has loaded data, but no completeness has ever been calculated" do
-      dataset = mock_non_remote_dataset()
-      allow(Dataset.get_all!(), return: [dataset], meck_options: [:passthrough])
-
-      allow(Persistence.persist(any(), any()), return: :does_not_matter)
-
-      allow(Prestige.execute(any(), any()),
-        return: [
-          %{"age" => 78, "name" => "Alex Trebek"},
-          %{"age" => 72, "name" => "Pat Sajak"},
-          %{"age" => nil, "name" => "Wayne Brady"}
-        ]
-      )
-
-      keys = ["forklift:last_insert_date:#{dataset.id}", "discovery_api:completion_calculated_date:#{dataset.id}"]
-      allow(Persistence.get_many(keys), return: ["2019-06-05T13:59:09.630290Z", nil])
-
-      StatsCalculator.produce_completeness_stats()
-
-      assert_called(
-        Persistence.persist(
-          "discovery-api:stats:#{dataset.id}",
-          %{
-            id: dataset.id,
-            completeness: 0.8333333333333334,
-            record_count: 3,
-            fields: %{
-              "name" => %{count: 3, required: false},
-              "age" => %{count: 2, required: false}
-            }
-          }
-        )
-      )
-
-      assert_called(Persistence.persist("discovery-api:completeness_calculated_date:#{dataset.id}", any()))
-    end
   end
 
-  def mock_non_remote_dataset() do
+  defp mock_non_remote_dataset() do
     %SmartCity.Dataset{
       _metadata: %SmartCity.Dataset.Metadata{expectedBenefit: [], intendedUse: []},
       business: nil,
@@ -175,7 +150,7 @@ defmodule DiscoveryApi.Stats.StatsCalculatorTest do
     }
   end
 
-  def mock_remote_dataset() do
+  defp mock_remote_dataset() do
     %SmartCity.Dataset{
       _metadata: %SmartCity.Dataset.Metadata{expectedBenefit: [], intendedUse: []},
       business: nil,
