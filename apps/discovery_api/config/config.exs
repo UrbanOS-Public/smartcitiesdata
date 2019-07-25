6aa93a4994aaeeff1319ffbc371b07db1d7b3ebd
bbrewer-papa-x <benjamin.brewer@accenture.com>
2019-07-25 10:51:51 -0400

    downgrade cowboy until backpressure issue is fixed so we don't fill up memory
    cowboy issue: https://github.com/ninenines/cowboy/issues/1376
    
    smartcitiesdata/discovery-api#
    
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>

--- apps/discovery_api/config/config.exs
+++ apps/discovery_api/config/config.exs
@@ -13,7 +13,7 @@ config :discovery_api, DiscoveryApiWeb.Endpoint,
   render_errors: [view: DiscoveryApiWeb.ErrorView, accepts: ~w(json)],
   pubsub: [name: DiscoveryApi.PubSub, adapter: Phoenix.PubSub.PG2],
   instrumenters: [DiscoveryApiWeb.Endpoint.Instrumenter],
-  http: [port: 4000]
+  http: [port: 4000, protocol_options: [idle_timeout: :infinity]]
 
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
