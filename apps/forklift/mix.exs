ce07765561e7d5ff2b0383ab1d48d965d1a4a693
smillardHNTB <37001890+smillardHNTB@users.noreply.github.com>
2019-08-08 10:39:55 -0400

    Make version number 1.0.0-static (#85)

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -4,7 +4,7 @@ defmodule Forklift.MixProject do
   def project do
     [
       app: :forklift,
-      version: "0.4.8",
+      version: "1.0.0-static",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
