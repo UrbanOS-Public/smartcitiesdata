51ef69482036c3d959afa55e0799d92235e20a82
smillardHNTB <37001890+smillardHNTB@users.noreply.github.com>
2019-08-08 08:34:07 -0400

    Make version number 1.0.0-static (#108)

--- apps/discovery_streams/mix.exs
+++ apps/discovery_streams/mix.exs
@@ -4,7 +4,7 @@ defmodule DiscoveryStreams.Mixfile do
   def project do
     [
       app: :discovery_streams,
-      version: "2.0.3",
+      version: "1.0.0-static",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
