50e8f9a8239be387910ca023fba366cd6e2c9a83
abrock-accenture <anthony.brock@accenture.com>
2019-08-14 14:13:10 -0400

    flattening lists within download results
    smartcitiesdata/discovery-api#206
    
    co-authored-by: Scott Millard <smillard@hntb.com>

--- apps/discovery_api/test/unit/discovery_api_web/controllers/dataset_download_controller_test.exs
+++ apps/discovery_api/test/unit/discovery_api_web/controllers/dataset_download_controller_test.exs
@@ -108,6 +108,48 @@ defmodule DiscoveryApiWeb.DatasetDownloadControllerTest do
     end
   end
 
+  describe "fetching csv data with array of integers" do
+    test "returns flattened array as string in single column in CSV format", %{conn: conn} do
+      dataset_id = "pedro"
+      url = "/api/v1/dataset/#{dataset_id}/download"
+
+      model =
+        Helper.sample_model(%{
+          id: dataset_id,
+          systemName: "#{@org_name}__paco",
+          name: "paco",
+          private: false,
+          lastUpdatedDate: nil,
+          queries: 7,
+          downloads: 9,
+          organizationDetails: %{
+            orgName: @org_name
+          }
+        })
+
+      allow(SystemNameCache.get(@org_name, model.name), return: dataset_id)
+      allow(Model.get(dataset_id), return: model)
+
+      allow(Prestige.execute("describe #{model.systemName}"),
+        return: []
+      )
+
+      allow(Prestige.execute("select * from #{model.systemName}"),
+        return: [[1, [2, 3, 4]]]
+      )
+
+      allow(Prestige.prefetch(any()),
+        return: [["id", "1"], ["int_array", [2, 3, 4]]]
+      )
+
+      allow(Redix.command!(any(), any()), return: :does_not_matter)
+
+      actual = conn |> get(url) |> response(200)
+
+      assert "id,int_array\n1,\"2,3,4\"\n" == actual
+    end
+  end
+
   describe "fetching json data" do
     setup do
       model =
