7ba8ab6cca5fe06f14da1877ac52121394ee2ccd
Timothy Regan <tregan@pillartechnology.com>
2019-08-14 11:15:09 -0400

    Add bounding box logic to features preview controller
    smartcitiesdata/discovery-api#188
    
    co-authored-by: Cameron Marsh <cmarsh@pillartechnology.com>

--- apps/discovery_api/lib/discovery_api_web/controllers/dataset_preview_controller.ex
+++ apps/discovery_api/lib/discovery_api_web/controllers/dataset_preview_controller.ex
@@ -11,7 +11,12 @@ defmodule DiscoveryApiWeb.DatasetPreviewController do
     |> PrestoService.preview()
     |> return_preview(columns, conn)
   rescue
-    _e in Prestige.Error -> json(conn, %{data: [], meta: %{columns: []}, message: "Something went wrong while fetching the preview."})
+    _e in Prestige.Error ->
+      json(conn, %{
+        data: [],
+        meta: %{columns: []},
+        message: "Something went wrong while fetching the preview."
+      })
   end
 
   def fetch_geojson_features(conn, _params) do
@@ -21,7 +26,9 @@ defmodule DiscoveryApiWeb.DatasetPreviewController do
       PrestoService.preview(dataset_name, 10)
       |> Enum.map(&decode_feature_result(&1))
 
-    render(conn, "features.json", %{features: features, dataset_name: dataset_name})
+    bbox = calculate_bounding_box(features)
+
+    render(conn, "features.json", %{features: features, dataset_name: dataset_name, bbox: bbox})
   end
 
   defp decode_feature_result(feature) do
@@ -30,5 +37,38 @@ defmodule DiscoveryApiWeb.DatasetPreviewController do
     |> Jason.decode!()
   end
 
-  defp return_preview(rows, columns, conn), do: json(conn, %{data: rows, meta: %{columns: columns}})
+  defp return_preview(rows, columns, conn),
+    do: json(conn, %{data: rows, meta: %{columns: columns}})
+
+  defp calculate_bounding_box(features_list) do
+    coords = []
+
+    features_list
+    |> Enum.reduce(coords, fn %{"geometry" => %{"coordinates" => coordinates}}, coords ->
+      [coordinates | coords]
+    end)
+    |> reduce_coordinates()
+    |> List.flatten()
+    |> Enum.reduce([nil, nil, nil, nil], fn {x, y}, acc -> update_bbox(x, y, acc) end)
+  end
+
+  defp update_bbox(x, y, [min_x, min_y, max_x, max_y]) do
+    [
+      min(x, min_x),
+      min(y, min_y),
+      get_max(x, max_x),
+      get_max(y, max_y)
+    ]
+  end
+
+  defp get_max(a, nil), do: a
+  defp get_max(a, b), do: max(a, b)
+
+  defp reduce_coordinates([x, y]) when is_list(x) == false do
+    {x, y}
+  end
+
+  defp reduce_coordinates(coords) do
+    Enum.map(coords, &reduce_coordinates/1)
+  end
 end
--- apps/discovery_api/lib/discovery_api_web/views/dataset_preview_view.ex
+++ apps/discovery_api/lib/discovery_api_web/views/dataset_preview_view.ex
@@ -1,10 +1,11 @@
 defmodule DiscoveryApiWeb.DatasetPreviewView do
   use DiscoveryApiWeb, :view
 
-  def render("features.json", %{features: features, dataset_name: name}) do
+  def render("features.json", %{features: features, dataset_name: name, bbox: bbox}) do
     %{
       name: name,
       features: features,
+      bbox: bbox,
       type: "FeatureCollection"
     }
   end
