62976e7f58cb96bd66c387057ff01835f88c9639
abrock-accenture <anthony.brock@accenture.com>
2019-08-29 16:55:43 -0400

    multiple_data_view now supports geojson; added integration test to prove it
    smartcitiesdata/discovery_api#221
    
    co-authored-by: Nathaniel Stevens <nathaniel.scott.stevens@gmail.com>

--- apps/discovery_api/lib/discovery_api_web/views/multiple_data_view.ex
+++ apps/discovery_api/lib/discovery_api_web/views/multiple_data_view.ex
@@ -3,7 +3,7 @@ defmodule DiscoveryApiWeb.MultipleDataView do
   import DiscoveryApiWeb.Utilities.StreamUtils
 
   def accepted_formats() do
-    ["csv", "json"]
+    ["csv", "json", "geojson"]
   end
 
   def render_as_stream(:data, "json", %{stream: stream}) do
@@ -20,6 +20,18 @@ defmodule DiscoveryApiWeb.MultipleDataView do
     stream
     |> Stream.transform(false, &do_transform/2)
     |> map_data_stream_for_csv()
+  end
+
+  def render_as_stream(:data, "geojson", %{stream: stream}) do
+    type = "FeatureCollection"
+
+    data =
+      stream
+      |> Stream.map(&Map.get(&1, "feature"))
+      |> Stream.intersperse(",")
+
+    [["{\"type\": \"#{type}\", \"features\": "], ["["], data, ["],"]]
+    |> Stream.concat()
   end
 
   defp do_transform(el, false) do
