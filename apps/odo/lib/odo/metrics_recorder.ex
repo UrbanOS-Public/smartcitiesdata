7fbc63bac494f135a02f584a6f5277b502b78061
abrock-accenture <anthony.brock@accenture.com>
2019-09-04 10:24:05 -0400

    logging when fails to record metrics
    smartcitiesdata/odo#3

--- apps/odo/lib/odo/metrics_recorder.ex
+++ apps/odo/lib/odo/metrics_recorder.ex
@@ -1,4 +1,6 @@
 defmodule Odo.MetricsRecorder do
+  require Logger
+
   @metric_collector Application.get_env(:odo, :collector)
 
   def record_file_conversion_metrics(dataset_id, file_key, success, start_date_time \\ DateTime.utc_now()) do
@@ -11,12 +13,17 @@ defmodule Odo.MetricsRecorder do
       start: DateTime.to_unix(start_date_time)
     ]
 
-    @metric_collector.record_metrics(
-      [
-        @metric_collector.gauge_metric(success_value, "file_conversion_success", labels),
-        @metric_collector.gauge_metric(duration, "file_conversion_duration", labels)
-      ],
-      "odo"
-    )
+    result =
+      @metric_collector.record_metrics(
+        [
+          @metric_collector.gauge_metric(success_value, "file_conversion_success", labels),
+          @metric_collector.gauge_metric(duration, "file_conversion_duration", labels)
+        ],
+        "odo"
+      )
+      |> case do
+        {:error, err} -> Logger.warn("Unable to record file conversion metrics : #{inspect(err)}")
+        _ -> :ok
+      end
   end
 end
