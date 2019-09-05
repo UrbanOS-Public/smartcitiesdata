837ce78d0fba124ebcc35f878a4f63399e0b8a61
abrock-accenture <anthony.brock@accenture.com>
2019-09-05 16:45:44 -0400

    Handling mixed case schema field keys
    smartcitiesdata/discovery_api#233
    
    co-authored-by: Richard Jones <rajones@hntb.com>

--- apps/discovery_api/lib/discovery_api_web/utilities/json_field_decoder.ex
+++ apps/discovery_api/lib/discovery_api_web/utilities/json_field_decoder.ex
@@ -7,11 +7,16 @@ defmodule DiscoveryApiWeb.Utilities.JsonFieldDecoder do
   end
 
   def decode_one_datum([column | remaining_schema], datum) do
-    case column.type do
-      "json" ->
-        decode_one_datum(remaining_schema, Map.put(datum, column.name, Jason.decode!(datum[column.name])))
+    downcased_column_name = String.downcase(column.name)
 
-      _ ->
+    cond do
+      column.type == "json" and Map.has_key?(datum, downcased_column_name) ->
+        decode_one_datum(
+          remaining_schema,
+          Map.put(datum, downcased_column_name, Jason.decode!(datum[downcased_column_name]))
+        )
+
+      true ->
         decode_one_datum(remaining_schema, datum)
     end
   end
--- apps/discovery_api/test/unit/discovery_api_web/utilities/json_field_decoder_test.exs
+++ apps/discovery_api/test/unit/discovery_api_web/utilities/json_field_decoder_test.exs
@@ -4,15 +4,16 @@ defmodule DiscoveryApiWeb.JsonFieldDecoderTest do
   import Checkov
   alias DiscoveryApiWeb.Utilities.JsonFieldDecoder
 
-  describe "ensure_decoded/2" do
-    data_test "returns based on schema assigned to it" do
+  describe "decode_one_datum/2" do
+    data_test "returns based on schema assigned to it - #{description}" do
       actual_value = JsonFieldDecoder.decode_one_datum(schema, input)
 
       assert actual_value == expected_output
 
       where([
-        [:expected_output, :input, :schema],
+        [:description, :expected_output, :input, :schema],
         [
+          "no json in the schema returns values given",
           %{"id" => 2, "name" => "tony"},
           %{"id" => 2, "name" => "tony"},
           [
@@ -21,6 +22,7 @@ defmodule DiscoveryApiWeb.JsonFieldDecoderTest do
           ]
         ],
         [
+          "json field is decoded",
           %{"id" => 1, "name" => %{"name" => "robert"}},
           %{"id" => 1, "name" => "{\"name\": \"robert\"}"},
           [
@@ -29,11 +31,13 @@ defmodule DiscoveryApiWeb.JsonFieldDecoderTest do
           ]
         ],
         [
+          "no schema returns values given",
           %{"id" => 1, "name" => "{\"name\": \"robert\"}"},
           %{"id" => 1, "name" => "{\"name\": \"robert\"}"},
           []
         ],
         [
+          "json fields with nested json are decoded",
           %{
             "id" => 1,
             "bins" => %{
@@ -43,14 +47,33 @@ defmodule DiscoveryApiWeb.JsonFieldDecoderTest do
           },
           %{
             "id" => 1,
-            "bins" => "{\"bins\":{\"day\":{},\"hour\":{},\"minute\":{}},\"streets\":{\"day\":{},\"hour\":{},\"minute\":{}}}"
+            "bins" =>
+              "{\"bins\":{\"day\":{},\"hour\":{},\"minute\":{}},\"streets\":{\"day\":{},\"hour\":{},\"minute\":{}}}"
           },
           [
             %{name: "id", type: "integer"},
             %{name: "bins", type: "json"}
           ]
+        ],
+        [
+          "unknown json field in schema returns values given",
+          %{"id" => 1},
+          %{"id" => 1},
+          [
+            %{name: "not_found", type: "json"}
+          ]
+        ],
+        [
+          "mixed cased key in schema is downcased to match lowercase database column name",
+          %{"mixedcasekey" => %{"name" => "billy"}},
+          %{"mixedcasekey" => "{\"name\": \"billy\"}"},
+          [
+            %{name: "mIxEdCaSeKeY", type: "json"}
+          ]
         ]
       ])
+
+      _suppress_warning = description
     end
 
     test "raises on invalid json" do
