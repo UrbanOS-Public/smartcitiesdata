7198d18c2db8b68cc98183079ec4455d8d0ec496
kennyatpillar <kdrobnack@pillartechnology.com>
2019-09-09 10:51:41 -0400

    Add migration method for updating applications to event stream by reposting all datasets
    smartcitiesdata/valkyrie#69
    
    co-authored-by: Kenny Drobnack <kdrobnack@pillartechnology.com>

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -31,28 +31,28 @@ defmodule Andi.MixProject do
     [
       {:brook, "~> 0.1.2"},
       {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:placebo, "~> 1.2", only: [:dev, :test, :integration]},
-      {:phoenix, "~> 1.4"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.1"},
-      {:plug_cowboy, "~> 2.0"},
-      {:mix_test_watch, "~> 0.9", only: :dev, runtime: false},
-      {:smart_city, "~> 2.7"},
-      {:smart_city_registry, "~> 5.0"},
+      # distillery breaks @ 2.1.0 due to elixir 1.9 support
+      {:distillery, "2.0.14"},
       {:divo, "~> 1.1", only: [:dev, :integration]},
       {:divo_kafka, "~> 0.1.5", only: [:dev, :integration]},
       {:divo_redis, "~> 0.1.4", only: [:dev, :integration]},
+      {:gettext, "~> 0.11"},
+      {:husky, "~> 1.0", only: :dev, runtime: false},
+      {:jason, "~> 1.1"},
+      {:mix_test_watch, "~> 0.9", only: :dev, runtime: false},
       {:paddle, "~> 0.1"},
-      {:tesla, "~> 1.2", only: :integration},
-      {:uuid, "~> 1.1"},
+      {:phoenix, "~> 1.4"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:placebo, "~> 1.2", only: [:dev, :test, :integration]},
+      {:plug_cowboy, "~> 2.0"},
+      {:smart_city, "~> 2.7"},
+      {:smart_city_registry, "~> 5.0"},
       {:smart_city_test, "~> 0.5", only: [:test, :integration]},
       {:sobelow, "~> 0.8", only: :dev},
-      {:husky, "~> 1.0", only: :dev, runtime: false},
-      # distillery breaks @ 2.1.0 due to elixir 1.9 support
-      {:distillery, "2.0.14"}
+      {:tesla, "~> 1.2", only: :integration},
+      {:uuid, "~> 1.1"}
     ]
   end
 end
---
+++ apps/andi/test/integration/brook_migration_test.exs
@@ -0,0 +1,63 @@
+defmodule Andi.BrookMigrationTest do
+  use ExUnit.Case
+  use Divo
+
+  import SmartCity.TestHelper, only: [eventually: 1, eventually: 3]
+  alias SmartCity.Dataset
+  alias SmartCity.TestDataGenerator, as: TDG
+  import SmartCity.Event, only: [dataset_update: 0]
+
+  use Placebo
+
+  setup_all do
+    :ok
+  end
+
+  describe "migrate_to_brook/0" do
+    test "migrates datasets to brook events" do
+      allow(Brook.Event.send(any(), :andi, any()), return: :does_not_matter)
+
+      dataset1 = %SmartCity.Registry.Dataset{
+        id: "dataset1",
+        _metadata: %SmartCity.Registry.Dataset.Metadata{},
+        business: %SmartCity.Registry.Dataset.Business{},
+        technical: %SmartCity.Registry.Dataset.Technical{}
+      }
+
+      {:ok, dataset_event1} =
+        SmartCity.Dataset.new(
+          Map.from_struct(%{
+            dataset1
+            | _metadata: Map.from_struct(dataset1._metadata),
+              business: Map.from_struct(dataset1.business),
+              technical: Map.from_struct(dataset1.technical)
+          })
+        )
+
+      dataset2 = %SmartCity.Registry.Dataset{
+        id: "dataset2",
+        _metadata: %SmartCity.Registry.Dataset.Metadata{},
+        business: %SmartCity.Registry.Dataset.Business{},
+        technical: %SmartCity.Registry.Dataset.Technical{}
+      }
+
+      {:ok, dataset_event2} =
+        SmartCity.Dataset.new(
+          Map.from_struct(%{
+            dataset2
+            | _metadata: Map.from_struct(dataset2._metadata),
+              business: Map.from_struct(dataset2.business),
+              technical: Map.from_struct(dataset2.technical)
+          })
+        )
+
+      SmartCity.Registry.Dataset.write(dataset1)
+      SmartCity.Registry.Dataset.write(dataset2)
+
+      Andi.BrookMigration.migrate_to_brook()
+
+      expect(Brook.Event.send(dataset_update(), :andi, dataset_event1))
+      expect(Brook.Event.send(dataset_update(), :andi, dataset_event2))
+    end
+  end
+end
