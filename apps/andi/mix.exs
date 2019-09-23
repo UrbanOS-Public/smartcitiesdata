2bf9ee9a77017bec4d97832560f60b9ba7c5b90f
JarredOlson <jarred.olson@accenture.com>
2019-09-23 12:55:20 -0400

    bump version to indicate additional API endpoint
    co-authored-by: Benjamin Brewer <benjamin.brewer@accenture.com>

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.3.2",
+      version: "0.4.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
