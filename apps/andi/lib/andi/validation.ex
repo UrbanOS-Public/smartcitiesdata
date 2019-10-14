35f20c294c38c4965120f36bd6afa1c50d1f9e25
kennyatpillar <kdrobnack@pillartechnology.com>
2019-10-14 11:13:38 -0400

    Adds functions to check format of modified_date
    smartcitiesdata/smartcitiesdata/smartcitiesdata#149
    
    co-authored-by: Richard Jones <rajones@hntb.com>

---
+++ apps/andi/lib/andi/validation.ex
@@ -0,0 +1,25 @@
+defmodule DatasetValidator do
+  def validate(dataset) do
+    SimplyValidate.validate(dataset, [
+      modified_date_iso8601(),
+      description_required()
+    ])
+  end
+
+  defp description_required do
+    {&(&1.description != ""), "Description must be provided"}
+  end
+
+  defp modified_date_iso8601 do
+    {&is_valid_modified_date?(&1.modified_date), "Modified date must be in a valid IOS8601 timestamp format"}
+  end
+
+  defp is_valid_modified_date?(modified_date) when modified_date == "", do: true
+
+  defp is_valid_modified_date?(modified_date) when modified_date != "" do
+    case DateTime.from_iso8601(modified_date) do
+      {:ok, _, _} -> true
+      {:error, _} -> false
+    end
+  end
+end
