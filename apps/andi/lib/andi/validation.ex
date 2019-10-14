14c8a8d692ae3a672d239f946cc3aaf0c8bf2908
rajones <rajones@hntb.com>
2019-10-14 13:11:30 -0400

    Update apps/andi/lib/andi/validation.ex
    Co-Authored-By: Johnson Denen <johnson.denen@gmail.com>

--- apps/andi/lib/andi/validation.ex
+++ apps/andi/lib/andi/validation.ex
@@ -16,7 +16,7 @@ defmodule DatasetValidator do
     {&is_valid_modified_date?(&1.modified_date), "Modified date must be in a valid IOS8601 timestamp format"}
   end
 
-  defp is_valid_modified_date?(modified_date) when modified_date == "", do: true
+  defp is_valid_modified_date?(""), do: true
 
   defp is_valid_modified_date?(modified_date) when modified_date != "" do
     case DateTime.from_iso8601(modified_date) do
