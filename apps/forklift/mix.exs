9a915bcdfd697da5a5cbb68f81c948c800468cad
Johnson Denen <johnson.denen@gmail.com>
2019-10-15 12:40:26 -0400

    Bump forklift version

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -4,7 +4,7 @@ defmodule Forklift.MixProject do
   def project do
     [
       app: :forklift,
-      version: "0.8.2",
+      version: "0.9.0",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
