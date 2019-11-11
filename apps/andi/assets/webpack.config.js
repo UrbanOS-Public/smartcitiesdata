7f6a39614cbf719bda3dab58dcfeb695d4d71a5b
Jake Prem <jakeprem@gmail.com>
2019-11-11 11:38:33 -0500

    Add styling to Dataset Status Page search

--- apps/andi/assets/webpack.config.js
+++ apps/andi/assets/webpack.config.js
@@ -33,6 +33,18 @@ module.exports = (env, options) => ({
         use: [
           MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '../fonts/'
+            }
+          }
+        ]
       }
     ]
   },
