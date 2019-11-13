4c40e9acfa6b7b41b0c1f96a78ab449b06045a48
ManApart <akucera@pillartechnology.com>
2019-11-13 15:42:56 -0500

    pr feedback

--- apps/andi/lib/andi/migration/migrations.ex
+++ apps/andi/lib/andi/migration/migrations.ex
@@ -23,8 +23,15 @@ defmodule Andi.Migration.Migrations do
   end
 
   def migrate_modified_dates do
-    if is_nil(Brook.get!(@instance, :migration, "modified_date_migration_completed")) do
-      Brook.Event.send(@instance, "migration:modified_dates", :andi, %{})
-    end
+    has_migration_run?("modified_date_migration_completed")
+    |> migrate_modified_dates()
   end
+
+  defp has_migration_run?(migration_name) do
+    Brook.get!(@instance, :migration, migration_name)
+  end
+
+  defp migrate_modified_dates(nil), do: Brook.Event.send(@instance, "migration:modified_dates", :andi, %{})
+
+  defp migrate_modified_dates(_), do: nil
 end
--- apps/andi/lib/andi/migration/modified_date_migration.ex
+++ apps/andi/lib/andi/migration/modified_date_migration.ex
@@ -21,17 +21,7 @@ defmodule Andi.Migration.ModifiedDateMigration do
     if dataset.business.modifiedDate != corrected_date do
       log_bad_date(dataset.id, dataset.business.modifiedDate, corrected_date)
 
-      updated_business =
-        dataset.business
-        |> Map.from_struct()
-        |> Map.put(:modifiedDate, corrected_date)
-
-      {:ok, updated_dataset} =
-        dataset
-        |> Map.from_struct()
-        |> Map.put(:technical, Map.from_struct(dataset.technical))
-        |> Map.put(:business, updated_business)
-        |> SmartCity.Dataset.new()
+      updated_dataset = update_modified_date(dataset, corrected_date)
 
       Brook.Event.send(@instance, dataset_update(), :andi, updated_dataset)
       Brook.ViewState.merge(:dataset, updated_dataset.id, updated_dataset)
@@ -42,9 +32,25 @@ defmodule Andi.Migration.ModifiedDateMigration do
     Logger.warn("Could not migrate invalid dataset #{inspect(invalid_dataset)}")
   end
 
-  defp log_bad_date(id, old_date, new_date) do
-    if new_date == "" && old_date != "" do
-      Logger.warn("[#{id}] unable to parse business.modifiedDate '#{inspect(old_date)}' in modified_date_migration")
-    end
+  defp log_bad_date(id, old_date, "") when old_date != "" do
+    Logger.warn("[#{id}] unable to parse business.modifiedDate '#{inspect(old_date)}' in modified_date_migration")
+  end
+
+  defp log_bad_date(_id, _old_date, _new_date), do: nil
+
+  defp update_modified_date(dataset, corrected_date) do
+    updated_business =
+      dataset.business
+      |> Map.from_struct()
+      |> Map.put(:modifiedDate, corrected_date)
+
+    {:ok, updated_dataset} =
+      dataset
+      |> Map.from_struct()
+      |> Map.put(:technical, Map.from_struct(dataset.technical))
+      |> Map.put(:business, updated_business)
+      |> SmartCity.Dataset.new()
+
+    updated_dataset
   end
 end
