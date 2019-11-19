a7bdd2fcb535586b9e736de112cf649ff42936c4
Austin <akucera@pillartechnology.com>
2019-11-19 12:01:22 -0500

    Status indicator (#330)
    co-authored-by: Scott Millard <smillard@hntb.com>

--- apps/andi/lib/andi_web/live/dataset_live_view.ex
+++ apps/andi/lib/andi_web/live/dataset_live_view.ex
@@ -3,6 +3,8 @@ defmodule AndiWeb.DatasetLiveView do
   alias AndiWeb.Router.Helpers, as: Routes
   alias AndiWeb.DatasetLiveView.Table
 
+  @ingested_time_topic "ingested_time_topic"
+
   def render(assigns) do
     ~L"""
     <div class="datasets-index">
@@ -31,6 +33,7 @@ defmodule AndiWeb.DatasetLiveView do
   end
 
   def mount(_session, socket) do
+    AndiWeb.Endpoint.subscribe(@ingested_time_topic)
     {:ok, assign(socket, datasets: nil, search_text: nil, order: {"data_title", "asc"}, params: %{})}
   end
 
@@ -46,17 +49,24 @@ defmodule AndiWeb.DatasetLiveView do
     {:noreply, live_redirect(socket, to: Routes.live_path(socket, __MODULE__, params))}
   end
 
+  def handle_info(%{topic: @ingested_time_topic, payload: %{"id" => id, "ingested_time" => ingested_time}}, socket) do
+    updated_datasets = update_ingest_time(id, ingested_time, socket.assigns.datasets)
+    updated_state = assign(socket, :datasets, updated_datasets)
+
+    {:noreply, updated_state}
+  end
+
   def handle_params(params, _uri, socket) do
     order_by = Map.get(params, "order-by", "data_title")
     order_dir = Map.get(params, "order-dir", "asc")
     search_text = Map.get(params, "search", "")
 
-    datasets =
+    view_models =
       filter_on_search_change(search_text, socket)
       |> sort_by_dir(order_by, order_dir)
 
     {:noreply,
-     assign(socket, search_text: search_text, datasets: datasets, order: %{order_by => order_dir}, params: params)}
+     assign(socket, search_text: search_text, datasets: view_models, order: %{order_by => order_dir}, params: params)}
   end
 
   def handle_event("search", %{"search-value" => value}, socket) do
@@ -64,19 +74,29 @@ defmodule AndiWeb.DatasetLiveView do
     {:noreply, live_redirect(socket, to: Routes.live_path(socket, __MODULE__, search_params))}
   end
 
-  # Private Functions
   defp filter_on_search_change(search_value, socket) do
-    case search_value != socket.assigns.search_text do
-      true -> Andi.DatasetCache.get_datasets() |> filter_datasets(search_value) |> Enum.map(&to_view_model/1)
-      _ -> socket.assigns.datasets
+    case search_value == socket.assigns.search_text do
+      false ->
+        Andi.DatasetCache.get_all()
+        |> ignore_incomplete_models
+        |> filter_models(search_value)
+        |> Enum.map(&to_view_model/1)
+
+      _ ->
+        socket.assigns.datasets
     end
   end
 
-  defp filter_datasets(datasets, ""), do: datasets
+  defp ignore_incomplete_models(models) do
+    Enum.reject(models, fn model -> is_nil(model["dataset"]) end)
+  end
 
-  defp filter_datasets(datasets, value) do
-    Enum.filter(datasets, fn dataset ->
-      search_contains?(dataset.business.orgTitle, value) || search_contains?(dataset.business.dataTitle, value)
+  defp filter_models(models, ""), do: models
+
+  defp filter_models(models, value) do
+    Enum.filter(models, fn model ->
+      search_contains?(model["dataset"].business.orgTitle, value) ||
+        search_contains?(model["dataset"].business.dataTitle, value)
     end)
   end
 
@@ -84,18 +104,37 @@ defmodule AndiWeb.DatasetLiveView do
     String.downcase(str) =~ String.downcase(search_str)
   end
 
-  defp sort_by_dir(datasets, order_by, order_dir) do
+  defp sort_by_dir(models, order_by, order_dir) do
     case order_dir do
-      "asc" -> Enum.sort_by(datasets, fn dataset -> Map.get(dataset, order_by) end)
-      "desc" -> Enum.sort_by(datasets, fn dataset -> Map.get(dataset, order_by) end, &>=/2)
-      _ -> datasets
+      "asc" -> Enum.sort_by(models, fn model -> Map.get(model, order_by) end)
+      "desc" -> Enum.sort_by(models, fn model -> Map.get(model, order_by) end, &>=/2)
+      _ -> models
     end
   end
 
-  defp to_view_model(dataset) do
+  defp update_ingest_time(id, ingested_time, datasets) do
+    exisiting_index = Enum.find_index(datasets, fn dataset -> id == dataset["id"] end)
+
+    case is_nil(exisiting_index) do
+      true ->
+        datasets
+
+      _ ->
+        updated_dataset =
+          datasets
+          |> Enum.at(exisiting_index)
+          |> Map.put("ingested_time", ingested_time)
+
+        List.replace_at(datasets, exisiting_index, updated_dataset)
+    end
+  end
+
+  defp to_view_model(model) do
     %{
-      "org_title" => dataset.business.orgTitle,
-      "data_title" => dataset.business.dataTitle
+      "id" => model["id"],
+      "org_title" => model["dataset"].business.orgTitle,
+      "data_title" => model["dataset"].business.dataTitle,
+      "ingested_time" => Map.get(model, "ingested_time")
     }
   end
 end
--- apps/andi/test/unit/andi_web/live/dataset_live_view_test.exs
+++ apps/andi/test/unit/andi_web/live/dataset_live_view_test.exs
@@ -2,6 +2,9 @@ defmodule AndiWeb.DatasetLiveViewTest do
   use AndiWeb.ConnCase
   use Phoenix.ConnTest
   import Phoenix.LiveViewTest
+  import Andi, only: [instance_name: 0]
+  import SmartCity.Event, only: [data_ingest_end: 0]
+  import SmartCity.TestHelper, only: [eventually: 1]
 
   alias Andi.DatasetCache
 
@@ -11,6 +14,20 @@ defmodule AndiWeb.DatasetLiveViewTest do
   @url_path "/datasets/live"
 
   setup do
+    Brook.Test.with_event(instance_name(), fn ->
+      instance_name()
+      |> Brook.get_all_values!(:dataset)
+      |> Enum.each(fn dataset ->
+        Brook.ViewState.delete(:dataset, dataset.id)
+      end)
+
+      instance_name()
+      |> Brook.get_all_values!(:ingested_time)
+      |> Enum.each(fn timestamp ->
+        Brook.ViewState.delete(:ingested_time, timestamp["id"])
+      end)
+    end)
+
     GenServer.call(DatasetCache, :reset)
   end
 
@@ -18,7 +35,7 @@ defmodule AndiWeb.DatasetLiveViewTest do
     test "loads all datasets", %{conn: conn} do
       datasets = Enum.map(1..3, fn _x -> TDG.create_dataset([]) end)
 
-      DatasetCache.put_datasets(datasets)
+      DatasetCache.put(datasets)
 
       assert {:ok, _view, html} = live(conn, @url_path)
 
@@ -35,6 +52,23 @@ defmodule AndiWeb.DatasetLiveViewTest do
       assert floki_get_text(html, ".datasets-index__title") =~ "All Datasets"
       assert floki_get_text(html, ".datasets-index__table") =~ "No Datasets"
     end
+
+    test "does not load datasets that only contain a timestamp", %{conn: conn} do
+      datasets = Enum.map(1..3, fn _x -> TDG.create_dataset([]) end)
+      timestamp = %{"id" => "timestamp_id", "ingested_time" => "create_ts"}
+
+      DatasetCache.put(datasets)
+      DatasetCache.put(timestamp)
+
+      assert {:ok, _view, html} = live(conn, @url_path)
+      table_text = floki_get_text(html, ".datasets-index__table")
+
+      assert 3 == Floki.find(html, ".datasets-index__table tr") |> Enum.count()
+
+      Enum.each(datasets, fn dataset ->
+        assert table_text =~ dataset.business.dataTitle
+      end)
+    end
   end
 
   describe "Live connection with search params in URL" do
@@ -49,7 +83,7 @@ defmodule AndiWeb.DatasetLiveViewTest do
       dataset_a = TDG.create_dataset(business: %{dataTitle: "data_a"})
       dataset_b = TDG.create_dataset(business: %{dataTitle: "data_b"})
 
-      DatasetCache.put_datasets([dataset_a, dataset_b])
+      DatasetCache.put([dataset_a, dataset_b])
 
       # For most of our tests, we can let live/2 handle both the static connection
       # and the live update. That wasn't working correctly for this one so doing the
@@ -77,7 +111,7 @@ defmodule AndiWeb.DatasetLiveViewTest do
       dataset_a = TDG.create_dataset(business: %{orgTitle: "org_a"})
       dataset_b = TDG.create_dataset(business: %{orgTitle: "org_b"})
 
-      DatasetCache.put_datasets([dataset_a, dataset_b])
+      DatasetCache.put([dataset_a, dataset_b])
 
       {:ok, view, _html} = live(conn, @url_path)
 
@@ -91,7 +125,7 @@ defmodule AndiWeb.DatasetLiveViewTest do
       dataset_a = TDG.create_dataset(business: %{dataTitle: "data_a"})
       dataset_b = TDG.create_dataset(business: %{dataTitle: "data_b"})
 
-      DatasetCache.put_datasets([dataset_a, dataset_b])
+      DatasetCache.put([dataset_a, dataset_b])
 
       {:ok, view, _html} = live(conn, @url_path)
 
@@ -105,7 +139,7 @@ defmodule AndiWeb.DatasetLiveViewTest do
       dataset_a = TDG.create_dataset(business: %{dataTitle: "data_a"})
       dataset_b = TDG.create_dataset(business: %{dataTitle: "data_b"})
 
-      DatasetCache.put_datasets([dataset_a, dataset_b])
+      DatasetCache.put([dataset_a, dataset_b])
 
       {:ok, view, _html} = live(conn, @url_path)
 
@@ -133,7 +167,7 @@ defmodule AndiWeb.DatasetLiveViewTest do
       dataset_a = TDG.create_dataset(business: %{orgTitle: "org_a"})
       dataset_b = TDG.create_dataset(business: %{orgTitle: "org_b"})
 
-      DatasetCache.put_datasets([dataset_a, dataset_b])
+      DatasetCache.put([dataset_a, dataset_b])
 
       {:ok, view, _html} = live(conn, @url_path)
 
@@ -147,7 +181,7 @@ defmodule AndiWeb.DatasetLiveViewTest do
       dataset_a = TDG.create_dataset(business: %{dataTitle: "data_a"})
       dataset_b = TDG.create_dataset(business: %{dataTitle: "data_b"})
 
-      DatasetCache.put_datasets([dataset_a, dataset_b])
+      DatasetCache.put([dataset_a, dataset_b])
 
       {:ok, view, _html} = live(conn, @url_path)
 
@@ -169,6 +203,41 @@ defmodule AndiWeb.DatasetLiveViewTest do
                  |> get_search_input_value()
       end)
     end
+  end
+
+  test "data_ingest_end events update ingested time", %{conn: conn} do
+    dataset = TDG.create_dataset(%{})
+
+    DatasetCache.put(dataset)
+    assert {:ok, view, _html} = live(conn, @url_path)
+    table_text = floki_get_text(render(view), ".datasets-index__table")
+    assert not (table_text =~ "check")
+
+    Brook.Test.send(instance_name(), data_ingest_end(), :andi, dataset)
+
+    eventually(fn ->
+      table_text = floki_get_text(render(view), ".datasets-index__table")
+      assert table_text =~ "check"
+    end)
+  end
+
+  test "data_ingest_end events does not change dataset order", %{conn: conn} do
+    datasets = Enum.map(1..3, fn _x -> TDG.create_dataset(%{}) end)
+    dataset = Enum.at(datasets, 1)
+
+    DatasetCache.put(datasets)
+
+    assert {:ok, view, _html} = live(conn, @url_path)
+    initial_table_text = floki_get_text(render(view), ".datasets-index__table")
+
+    Brook.Test.send(instance_name(), data_ingest_end(), :andi, dataset)
+
+    eventually(fn ->
+      table_text = floki_get_text(render(view), ".datasets-index__table")
+      assert table_text =~ "check"
+      # If we remove the check that was added, is everything else the same?
+      assert initial_table_text == String.replace(table_text, "check", "")
+    end)
   end
 
   defp get_search_input_value(html) do
