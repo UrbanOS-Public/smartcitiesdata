834766e67b938f0d4f1d8e07b8541d90f5597c9f
ryan.r.conaway <Ryan@conaway58.com>
2019-11-20 16:14:55 -0500

    Fixed smart_city_registry redis config

--- apps/andi/runtime.exs
+++ apps/andi/runtime.exs
@@ -17,8 +17,7 @@ endpoint =
   |> Enum.map(fn entry -> String.split(entry, ":") end)
   |> Enum.map(fn [host, port] -> {String.to_atom(host), String.to_integer(port)} end)
 
-config :smart_city_registry,
-       args: redix_args
+config :smart_city_registry, :redis, redix_args
 
 config :andi,
   ldap_user: System.get_env("LDAP_USER") |> Andi.LdapUtils.decode_dn!(),
