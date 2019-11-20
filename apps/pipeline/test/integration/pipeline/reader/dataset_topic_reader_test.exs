4f2fc10d63f7fdcbda71dc209876fd30bc0e48f4
Johnson Denen <johnson.denen@gmail.com>
2019-11-20 09:14:45 -0500

    Fix pipeline integration test instability

--- apps/pipeline/test/integration/pipeline/reader/dataset_topic_reader_test.exs
+++ apps/pipeline/test/integration/pipeline/reader/dataset_topic_reader_test.exs
@@ -198,16 +198,3 @@ defmodule Pipeline.Reader.DatasetTopicReaderTest do
     end
   end
 end
-
-defmodule Pipeline.TestHandler do
-  use Elsa.Consumer.MessageHandler
-
-  def init(_ \\ []) do
-    {:ok, []}
-  end
-
-  def handle_messages(messages, state) do
-    Registry.put_meta(Pipeline.TestRegistry, :messages, messages)
-    {:ack, state}
-  end
-end
--- apps/pipeline/test/integration/pipeline/reader/topic_reader_test.exs
+++ apps/pipeline/test/integration/pipeline/reader/topic_reader_test.exs
@@ -169,16 +169,3 @@ defmodule Pipeline.Reader.TopicReaderTest do
     end
   end
 end
-
-defmodule Pipeline.TestHandler do
-  use Elsa.Consumer.MessageHandler
-
-  def init(_ \\ []) do
-    {:ok, []}
-  end
-
-  def handle_messages(messages, state) do
-    Registry.put_meta(Pipeline.TestRegistry, :messages, messages)
-    {:ack, state}
-  end
-end
