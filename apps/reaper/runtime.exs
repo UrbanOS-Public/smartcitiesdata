61c810d74c28ea41597ebe5bc8476d1e4339d47c
ryan.r.conaway <Ryan@conaway58.com>
2019-11-21 11:26:10 -0500

    Removed print statements from reaper

--- apps/reaper/runtime.exs
+++ apps/reaper/runtime.exs
@@ -17,13 +17,13 @@ end)
 kafka_brokers = System.get_env("KAFKA_BROKERS")
 redis_host = System.get_env("REDIS_HOST")
 redis_password = System.get_env("REDIS_PASSWORD")
-all_redis_args = [host: redis_host, password: redis_password] |> IO.inspect(label: "FOO")
+all_redis_args = [host: redis_host, password: redis_password]
 redix_args = Enum.filter(all_redis_args, fn
 	{_, nil} -> false
 	{_, ""} -> false
 	_ -> true
 end)
-|> IO.inspect(label: "BAR")
+
 endpoints =
   kafka_brokers
   |> String.split(",")
