609a7650b734038cf908553bf1e797c7ce73f144
jeffgrunewald <jeff@grunewalddesign.com>
2019-11-25 14:28:53 -0500

    Fixing kafka config replication factor

--- apps/estuary/config/integration.exs
+++ apps/estuary/config/integration.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :estuary,
   divo: [
-    {DivoKafka, [create_topics: "topic-one:1:3,topic-two:1:1", outside_host: "localhost"]}
+    {DivoKafka, [create_topics: "topic1:1:1,topic2:1:1:compact", outside_host: "localhost"]}
   ],
   divo_wait: [dwell: 700, max_tries: 50]
