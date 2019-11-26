b48eb87bc0f8dcd4924b38fe47b6a332e1d8216e
Matt Dunson <dunson.142000@gmail.com>
2019-11-26 10:36:27 -0500

    Configuring mix.exs file
    smartcitiesdata/smartcitiesdata#338
    
    co-authored-by: Sukaant Chaudhary <sukaant.chaudhary@gmail.com>

--- apps/estuary/mix.exs
+++ apps/estuary/mix.exs
@@ -6,8 +6,15 @@ defmodule Estuary.MixProject do
       app: :estuary,
       version: "0.1.0",
       elixir: "~> 1.9",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      aliases: aliases(),
+      docs: docs(),
+      elixirc_paths: elixirc_paths(Mix.env()),
       test_paths: test_paths(Mix.env())
     ]
   end
@@ -29,6 +36,23 @@ defmodule Estuary.MixProject do
       {:distillery, "~> 2.1"}
     ]
   end
+
+  defp aliases do
+    [verify: ["format --check-formatted", "credo"]]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: "https://www.github.com/smartcitiesdata/smartcitiesdata",
+      extras: [
+        "README.md"
+      ]
+    ]
+  end
+
+  defp elixirc_paths(env) when env in [:test, :integration], do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp test_paths(:integration), do: ["test/integration"]
   defp test_paths(_), do: ["test/unit"]
