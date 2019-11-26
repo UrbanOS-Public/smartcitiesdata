5eee63f0246f4ad443287cc5b7bed0d90de0ce10
Matt Dunson <dunson.142000@gmail.com>
2019-11-26 12:24:07 -0500

    CI for estuary (#349)
    * Added estuary to umbrella, and added to travis.yml.
    
    co-authored-by: Sukaant Chaudhary <sukaant.chaudhary@gmail.com>

--- apps/estuary/mix.exs
+++ apps/estuary/mix.exs
@@ -5,9 +5,16 @@ defmodule Estuary.MixProject do
     [
       app: :estuary,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      aliases: aliases(),
+      docs: docs(),
+      elixirc_paths: elixirc_paths(Mix.env()),
       test_paths: test_paths(Mix.env())
     ]
   end
@@ -23,11 +30,29 @@ defmodule Estuary.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:elsa, "~> 0.11"},
+      {:elsa, "~> 0.10.0"},
       {:divo, "~> 1.1", only: [:dev, :integration]},
-      {:divo_kafka, "~> 0.1.6", only: [:dev, :integration]}
+      {:divo_kafka, "~> 0.1.5", only: [:dev, :integration]},
+      {:distillery, "~> 2.1"}
     ]
   end
+
+  defp aliases do
+    [verify: ["format --check-formatted", "credo"]]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: "https://www.github.com/smartcitiesdata/smartcitiesdata",
+      extras: [
+        "README.md"
+      ]
+    ]
+  end
+
+  defp elixirc_paths(env) when env in [:test, :integration], do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp test_paths(:integration), do: ["test/integration"]
   defp test_paths(_), do: ["test/unit"]
