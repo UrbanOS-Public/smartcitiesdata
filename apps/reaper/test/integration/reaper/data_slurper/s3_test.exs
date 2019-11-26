e84c586a7da01298df902051406edb7a9b00a14b
rajones@hntb.com <rajones@hntb.com>
2019-11-26 09:14:33 -0500

    Fix s3 slurper test timing issue
    smartcitiesdata/reaper#145
    
    co-authored-by: ManApart <akucera@pillartechnology.com>

--- apps/reaper/test/integration/reaper/data_slurper/s3_test.exs
+++ apps/reaper/test/integration/reaper/data_slurper/s3_test.exs
@@ -37,15 +37,11 @@ defmodule Reaper.S3ExtractorTest do
     test "previously hosted file is converted from file:ingest:end event" do
       topic = "raw-#{@id}"
 
-      eventually(
-        fn ->
-          assert Brook.get!(@instance, :extractions, @id) != nil
-          assert {:ok, _, messages} = Elsa.fetch(@endpoints, topic, partition: 0)
-          assert Enum.all?(messages, fn %Elsa.Message{value: value} -> String.contains?(value, "geojson-dataset") end)
-        end,
-        1000,
-        40
-      )
+      eventually(fn ->
+        assert Brook.get!(@instance, :extractions, @id) != nil
+        assert {:ok, _, messages} = Elsa.fetch(@endpoints, topic, partition: 0)
+        assert Enum.all?(messages, fn %Elsa.Message{value: value} -> String.contains?(value, "geojson-dataset") end)
+      end)
     end
   end
 end
