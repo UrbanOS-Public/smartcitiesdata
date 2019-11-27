b9b9909eded99a56dcf71d374e125f4980d5c31e
kcarra <kevincarrabine23@gmail.com>
2019-11-27 09:09:08 -0500

    Removed creation of kafka topics when divo starts

--- apps/estuary/config/integration.exs
+++ apps/estuary/config/integration.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :estuary,
   divo: [
-    {DivoKafka, [create_topics: "event-stream-integration:1:1", outside_host: "localhost"]}
+    {DivoKafka, [outside_host: "localhost"]}
   ],
   divo_wait: [dwell: 700, max_tries: 50],
   elsa_endpoint: [localhost: 9092],
