bc6371215e5e4526f0725f24c5e246af01b39c3a
kcarra <kevincarrabine23@gmail.com>
2019-11-27 09:30:52 -0500

    Config cleanup

--- apps/estuary/config/dev.exs
+++ apps/estuary/config/dev.exs
@@ -1,5 +1,1 @@
 use Mix.Config
-
-config :estuary,
-  elsa_endpoint: [localhost: 9092],
-  event_stream_topic: "event-stream"
--- apps/estuary/config/integration.exs
+++ apps/estuary/config/integration.exs
@@ -4,6 +4,4 @@ config :estuary,
   divo: [
     {DivoKafka, [outside_host: "localhost"]}
   ],
-  divo_wait: [dwell: 700, max_tries: 50],
-  elsa_endpoint: [localhost: 9092],
-  event_stream_topic: "event-stream-integration"
+  divo_wait: [dwell: 700, max_tries: 50]
