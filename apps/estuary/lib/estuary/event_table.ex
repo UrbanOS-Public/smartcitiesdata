3bd6ccdc86cb4951f0f938f8a25beed87946ca13
sukaant <sukaant.chaudhary@gmail.com>
2019-12-02 16:16:41 -0500

    Adding env variable for table
    smartcitiesdata/smartcitiesdata#338
    
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>

--- apps/estuary/lib/estuary/event_table.ex
+++ apps/estuary/lib/estuary/event_table.ex
@@ -3,9 +3,11 @@ defmodule Estuary.EventTable do
   This modules does the CRUD operation for event_stream in the presto database
   """
 
+  @event_stream_table_name Application.get_env(:estuary, :event_stream_table_name)
+
   def create_table do
     Prestige.execute(
-      "CREATE TABLE IF NOT EXISTS event_stream (author varchar, create_ts bigint, data varchar, type varchar)"
+      "CREATE TABLE IF NOT EXISTS #{@event_stream_table_name} (author varchar, create_ts bigint, data varchar, type varchar)"
     )
     |> Prestige.prefetch()
   end
