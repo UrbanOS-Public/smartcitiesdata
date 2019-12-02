c4fc659c006fef0ceaf9d1f2ba56ecd61dc68365
Austin <akucera@pillartechnology.com>
2019-12-02 12:29:51 -0500

    add mime types (#24)

--- apps/odo/config/config.exs
+++ apps/odo/config/config.exs
@@ -3,4 +3,9 @@ use Mix.Config
 config :odo,
   collector: StreamingMetrics.PrometheusMetricCollector
 
+config :mime, :types, %{
+  "application/gtfs+protobuf" => ["gtfs"],
+  "application/vnd.ogc.wms_xml" => ["wms"]
+}
+
 import_config "#{Mix.env()}.exs"
