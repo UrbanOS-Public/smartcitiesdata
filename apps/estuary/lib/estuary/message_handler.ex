a588bb67b3325f1b322fcb80c24948935c811fd6
Matt Dunson <dunson.142000@gmail.com>
2019-12-04 17:06:39 -0500

    Beginning messagehandler implmenation.
    smartcitiesdata/estuary#344
    
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>

--- apps/estuary/lib/estuary/message_handler.ex
+++ apps/estuary/lib/estuary/message_handler.ex
@@ -4,6 +4,9 @@ defmodule Estuary.MessageHandler do
 
   def handle_messages(messages) do
     IO.inspect(messages, label: "MESSAGESSS!!!!!!!!!!!!!!!!!!!!!!")
+    event = messages |> Enum.at(0) |> get_in("value");
+    # Todo convert from string to map
+    Estuary.EventTable.insert_event(event.author, event.create_ts, event.data, event.type)
     Logger.debug("Messages #{inspect(messages)} were sent to the eventstream")
     :ack
   end
