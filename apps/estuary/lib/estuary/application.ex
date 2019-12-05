cfcb5da4faf2ad6c9faa3806a67f3d5a04dc768e
sukaant <sukaant.chaudhary@gmail.com>
2019-12-05 12:18:44 -0500

    Refactoring for CD
    smartcitiesdata/smartcitiesdata#340
    
    co-authored-by: kevin Carrabine <kevincarrabine23@gmail.com>

--- apps/estuary/lib/estuary/application.ex
+++ apps/estuary/lib/estuary/application.ex
@@ -2,13 +2,9 @@ defmodule Estuary.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-
   use Application
 
   alias Estuary.EventTable
-
-  @elsa_endpoint Application.get_env(:estuary, :elsa_endpoint)
-  @event_stream_topic Application.get_env(:estuary, :event_stream_topic)
 
   @spec start(any, any) :: {:error, any} | {:ok, pid}
   def start(_type, _args) do
@@ -25,12 +21,12 @@ defmodule Estuary.Application do
 
   defp elsa_options do
     [
-      endpoints: @elsa_endpoint,
+      endpoints: Application.get_env(:estuary, :elsa_endpoint),
       connection: :estuary_elsa,
-      producer: [topic: @event_stream_topic],
+      producer: [topic: Application.get_env(:estuary, :event_stream_topic)],
       group_consumer: [
         group: "estuary-consumer-group",
-        topics: [@event_stream_topic],
+        topics: [Application.get_env(:estuary, :event_stream_topic)],
         handler: Estuary.MessageHandler,
         config: [
           begin_offset: :earliest,
@@ -41,9 +37,18 @@ defmodule Estuary.Application do
   end
 
   defp validate_topic_exists do
-    case Elsa.Topic.exists?(@elsa_endpoint, @event_stream_topic) do
-      true -> :ok
-      false -> Elsa.Topic.create(@elsa_endpoint, @event_stream_topic)
+    case Elsa.Topic.exists?(
+           Application.get_env(:estuary, :elsa_endpoint),
+           Application.get_env(:estuary, :event_stream_topic)
+         ) do
+      true ->
+        :ok
+
+      false ->
+        Elsa.Topic.create(
+          Application.get_env(:estuary, :elsa_endpoint),
+          Application.get_env(:estuary, :event_stream_topic)
+        )
     end
   end
 end
--- apps/estuary/lib/estuary/event_table.ex
+++ apps/estuary/lib/estuary/event_table.ex
@@ -3,11 +3,9 @@ defmodule Estuary.EventTable do
   This module will create event_stream table and insert data in the table
   """
 
-  @event_stream_table_name Application.get_env(:estuary, :event_stream_table_name)
-
   def create_table do
     Prestige.execute(
-      "CREATE TABLE IF NOT EXISTS #{@event_stream_table_name} (author varchar, create_ts bigint, data varchar, type varchar)"
+      "CREATE TABLE IF NOT EXISTS #{Application.get_env(:estuary, :event_stream_table_name)} (author varchar, create_ts bigint, data varchar, type varchar)"
     )
     |> Prestige.prefetch()
   end
