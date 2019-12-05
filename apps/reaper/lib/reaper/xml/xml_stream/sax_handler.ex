8b532b848a2a1b484172633f3c08dbfb9ae61059
Jake Prem <jakeprem@gmail.com>
2019-12-05 14:17:59 -0500

    Update SaxHandler to be more modular and easier to understand

--- apps/reaper/lib/reaper/xml/xml_stream/sax_handler.ex
+++ apps/reaper/lib/reaper/xml/xml_stream/sax_handler.ex
@@ -13,6 +13,31 @@ defmodule XMLStream.SaxHandler do
     def update(%__MODULE__{}=state, fields) do
       struct!(state, fields)
     end
+
+    @spec pop(%State{}, :stack | :tag_stack | :tag_path) :: {any(), %State{}}
+    def pop(%__MODULE__{}=state, key) when key in [:stack, :tag_stack, :tag_path] do
+      [item | rest] = Map.fetch!(state, key)
+
+      {item, State.update(state, [{key, rest}])}
+    end
+
+    def pop_current_tag(%__MODULE__{}=state, tag_name) do
+      {{^tag_name, attributes, content}, state} = State.pop(state, :stack)
+
+      {{tag_name, attributes, Enum.reverse(content)}, state}
+    end
+
+    def at_path?(%__MODULE__{tag_stack: tag_stack, tag_path: tag_path}, tag_name) do
+      [tag_name | tag_stack] == tag_path
+    end
+
+    def ascend(%__MODULE__{}=state) do
+      State.pop(state, :tag_stack)
+    end
+
+    def descend(%__MODULE__{tag_stack: tag_stack}=state, tag_name) do
+      State.update(state, tag_stack: [tag_name | tag_stack])
+    end
   end
 
   @behaviour Saxy.Handler
@@ -36,28 +61,33 @@ defmodule XMLStream.SaxHandler do
   ###############
   ## Callbacks ##
   ###############
+  @impl true
   def handle_event(:start_document, _, %State{}=state) do
     ok(state)
   end
 
+  @impl true
   def handle_event(:end_document, _, %State{}=state) do
     ok(state)
   end
 
+  @impl true
   def handle_event(:start_element, {tag_name, attributes}, %State{}=state) do
-    state = State.update(state, tag_stack: [tag_name | state.tag_stack])
-
-    if state.tag_stack == state.tag_path or state.accumulate do
+    if State.at_path?(state, tag_name) or state.accumulate do
       tag = {tag_name, attributes, []}
 
       state
+      |> State.descend(tag_name)
       |> State.update(stack: [tag | state.stack], accumulate: true)
       |> ok()
     else
-      ok(state)
+      state
+      |> State.descend(tag_name)
+      |> ok()
     end
   end
 
+  @impl true
   def handle_event(:characters, chars, %State{stack: stack, accumulate: true} = state) do
       [{tag_name, attributes, content} | stack] = stack
 
@@ -70,43 +100,37 @@ defmodule XMLStream.SaxHandler do
 
   def handle_event(:characters, _chars, %State{accumulate: false}=state), do: ok(state)
 
-  def handle_event(
-        :end_element,
-        tag_name,
-        %State{stack: stack, accumulate: accumulate, tag_stack: [_stack_tag_name | tag_stack]} = state
-      ) do
-    state = %{state | tag_stack: tag_stack}
+  @impl true
+  def handle_event(:end_element, tag_name, %State{accumulate: true} = state) do
+    {^tag_name, state} = State.ascend(state)
+    {current_tag, state} = State.pop_current_tag(state, tag_name)
 
-    if accumulate do
-      [{^tag_name, attributes, content} | stack] = stack
-
-      current = {tag_name, attributes, Enum.reverse(content)}
-
-      case stack do
-        [] ->
-          if [tag_name | state.tag_stack] == state.tag_path do
-            state.emitter.(current)
-
-            state
-            |> State.update(stack: [], accumulate: false)
-            |> ok()
-          else
-            state
-            |> State.update(stack: [current])
-            |> ok()
-          end
-
-        [parent | rest] ->
-          {parent_tag_name, parent_attributes, parent_content} = parent
-          parent = {parent_tag_name, parent_attributes, [current | parent_content]}
+    should_emit = State.at_path?(state, tag_name)
+    case state.stack do
+      [] when should_emit ->
+          state.emitter.(current_tag)
 
           state
-          |> State.update(stack: [parent | rest])
+          |> State.update(accumulate: false)
           |> ok()
-      end
-    else
-      ok(state)
+      [] ->
+          state
+          |> State.update(stack: [current_tag])
+          |> ok()
+
+      [parent | rest] ->
+        parent = update_parent(parent, current_tag)
+
+        state
+        |> State.update(stack: [parent | rest])
+        |> ok()
     end
+  end
+
+  def handle_event(:end_element, _tag_name, %State{accumulate: false}=state) do
+    {_tag, state} = State.ascend(state)
+
+    ok(state)
   end
 
   #######################
@@ -127,5 +151,9 @@ defmodule XMLStream.SaxHandler do
     selector
     |> String.split("/")
     |> Enum.reverse()
+  end
+
+  defp update_parent({tag_name, attributes, content}=_parent_tag, child_tag) do
+    {tag_name, attributes, [child_tag | content]}
   end
 end
