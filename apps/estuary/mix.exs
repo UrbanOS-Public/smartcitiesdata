eb183ed85ddf1c69ae4bc31fb23f5e0755b64546
Matt Dunson <dunson.142000@gmail.com>
2019-12-16 16:48:18 -0500

    update version of estuary

--- apps/estuary/mix.exs
+++ apps/estuary/mix.exs
@@ -4,7 +4,7 @@ defmodule Estuary.MixProject do
   def project do
     [
       app: :estuary,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
