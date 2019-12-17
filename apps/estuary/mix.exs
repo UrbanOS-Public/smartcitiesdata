39c46fdd2fcb1e7d51082148b1a85324244bd637
Matt Dunson <dunson.142000@gmail.com>
2019-12-17 11:03:08 -0500

    updated the minor version of estuary since it now requires a new charts variable

--- apps/estuary/mix.exs
+++ apps/estuary/mix.exs
@@ -4,7 +4,7 @@ defmodule Estuary.MixProject do
   def project do
     [
       app: :estuary,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
