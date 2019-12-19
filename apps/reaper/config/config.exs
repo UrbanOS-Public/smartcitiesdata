920adaa0732509d23ec5281079754e7c856978d3
Jeff Grunewald <jeff@grunewalddesign.com>
2019-12-19 16:10:35 -0500

    removing duplicate declarations of custom mime types

--- apps/reaper/config/config.exs
+++ apps/reaper/config/config.exs
@@ -1,10 +1,5 @@
 use Mix.Config
 
-config :mime, :types, %{
-  "application/gtfs+protobuf" => ["gtfs"],
-  "application/vnd.ogc.wms_xml" => ["wms"]
-}
-
 config :reaper,
   produce_retries: 10,
   produce_timeout: 100
