6aa7f6da9ddc5d0f938fa7726576a9d51f1869f3
Kenny Drobnack <34035289+kennyatpillar@users.noreply.github.com>
2020-01-06 13:19:28 -0500

    Fix forklift migration (#426)
    Fixed migration issues
    
    smartcitiesdata/smartcitiesdata#206
    
    * Fixed an issue with migrating last_insert_date to event stream
    * Added code to TTL expire old redis keys after migration is complete.
    
    co-authored-by: Nathaniel Stevens <nstevens@pillartechnology.com>

--- apps/forklift/test/integration/forklift/migrations_test.exs
+++ apps/forklift/test/integration/forklift/migrations_test.exs
@@ -15,7 +15,23 @@ defmodule Forklift.MigrationsTest do
       "forklift:last_insert_date:7ab08634-3eda-4b05-a754-5eb6cab31326" => "2019-12-04T17:44:02.645233Z"
     }
 
+    expected = [
+      %{
+        "id" => "38a830be-1408-41ae-8d2b-e1309f41c4cc",
+        "timestamp" => "2019-10-04T17:44:02.645233Z"
+      },
+      %{
+        "id" => "043c12aa-0964-4a25-b74a-62b4eebdc0fa",
+        "timestamp" => "2019-11-04T17:44:02.645233Z"
+      },
+      %{
+        "id" => "7ab08634-3eda-4b05-a754-5eb6cab31326",
+        "timestamp" => "2019-12-04T17:44:02.645233Z"
+      }
+    ]
+
     {:ok, redix} = Redix.start_link(Keyword.put(Application.get_env(:redix, :args), :name, :redix))
+
     Process.unlink(redix)
 
     Enum.each(last_insert_dates, fn {k, v} -> Redix.command(:redix, ["SET", k, v]) end)
@@ -24,13 +40,27 @@ defmodule Forklift.MigrationsTest do
 
     Application.ensure_all_started(:forklift)
 
-    Process.sleep(30_000)
-
     eventually(fn ->
-      assert Elsa.Fetch.search_keys([{'127.0.0.1', 9092}], "event-stream", data_write_complete()) |> Enum.to_list() |> length == 3
+      actual = get_data_write_complete_events()
+
+      assert Enum.count(actual) == 3
+
+      assert MapSet.new(actual) == MapSet.new(expected)
+
+      last_insert_dates
+      |> Map.keys()
+      |> Enum.each(fn k -> assert Redix.command!(:redix, ["TTL", k]) > 0 end)
     end)
 
     Application.stop(:forklift)
+  end
+
+  def get_data_write_complete_events() do
+    Elsa.Fetch.search_keys([{'127.0.0.1', 9092}], "event-stream", data_write_complete())
+    |> Enum.to_list()
+    |> Enum.map(fn %Elsa.Message{value: value} ->
+      Jason.decode!(value)["data"] |> Jason.decode!() |> Map.drop(["__brook_struct__"])
+    end)
   end
 
   defp kill(pid) do
