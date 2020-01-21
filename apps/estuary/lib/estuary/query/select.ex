fc4ed21e2aaa80bf5dda968d356f5dd07066a7e2
sukaant <sukaant.chaudhary@gmail.com>
2020-01-21 15:56:35 -0500

    Create generic Select

--- apps/estuary/lib/estuary/query/select.ex
+++ apps/estuary/lib/estuary/query/select.ex
@@ -1,15 +1,13 @@
 defmodule Estuary.Query.Select do
   @moduledoc false
 
-  def select_table do
-    insert
-
+  def select_table(value) do
     events =
-      "SELECT author, create_ts, data, type 
-    FROM hive.default.event_stream 
-    ORDER BY create_ts
-    DESC LIMIT 1000"
-      |> Prestige.execute(user: "estury", by_names: true)
+      "SELECT #{translate_columns(value["columns"])}
+      FROM #{value["table_name"]} #{translate_order(value["order_by"], value["order"])}
+      LIMIT #{translate_limit(value["limit"])}"
+      |> IO.inspect()
+      |> Prestige.execute(by_names: true)
       |> Prestige.prefetch()
 
     {:ok, events}
@@ -17,41 +15,35 @@ defmodule Estuary.Query.Select do
     error -> {:error, error}
   end
 
-  defp insert do
-    insert_statement = "insert into hive.default.event_stream 
-    (author, create_ts, data, type) 
-    values('Author', 1234, 'Data', 'Type')"
-    Prestige.execute(insert_statement, user: "estury") |> Stream.run()
+  defp translate_columns(columns) do
+    case !is_nil(columns) do
+      true ->
+        columns
+        |> Enum.reject(&(byte_size(&1) == 0))
+        |> Enum.map_join(", ", & &1)
+
+      _ ->
+        "*"
+    end
   end
 
-  #   def compose(config, data) do
-  #     columns = config.schema
+  defp translate_order(order_by, order) do
+    if !is_nil(order_by) and byte_size(order_by) != 0 do
+      "ORDER BY #{order_by} #{translate_order(order)}"
+    end
+  end
 
-  #     columns_fragment =
-  #       columns
-  #       |> Enum.map(&Map.get(&1, :name))
-  #       |> Enum.map(&to_string/1)
-  #       |> Enum.map(&~s|"#{&1}"|)
-  #       |> Enum.join(",")
+  defp translate_order(order) do
+    case order == "DESC" do
+      true -> "DESC"
+      _ -> "ASC"
+    end
+  end
 
-  #     data_fragment =
-  #       data
-  #       |> Enum.map(&format_columns(columns, &1))
-  #       |> Enum.map(&to_row_string/1)
-  #       |> Enum.join(",")
-
-  #     {:ok, ~s|SELECT #{columns_fragment} FROM "#{table}" ORDER BY #{column} "{order}" LIMIT #{limit}|}
-  #   rescue
-  #     e ->
-  #       Logger.error("Unhandled Statement Builder error: #{inspect(e)}")
-  #       {:error, e}
-  #   end
-
-  #   payload: %{
-  #     "columns" => ["author", "create_ts", "data", "type"],
-  #     "table" => "event_stream",
-  #     "column" => "create_ts",
-  #     "order" => "DESC",
-  #     "limit" => "1000"
-  #   }
+  defp translate_limit(limit) do
+    case is_integer(limit) do
+      true -> limit
+      _ -> "ALL"
+    end
+  end
 end
--- apps/estuary/lib/estuary/services/event_retrieval_service.ex
+++ apps/estuary/lib/estuary/services/event_retrieval_service.ex
@@ -2,24 +2,22 @@ defmodule Estuary.Services.EventRetrievalService do
   @moduledoc """
   Interface for retrieving events.
   """
-  import Estuary, only: [instance_name: 0]
   alias Estuary.Query.Select
 
   def get_all() do
-    case Select.select_table() do
+    case Select.select_table(make_select_table_schema) do
       {:ok, events} -> events
       {:error, reason} -> raise reason
     end
   end
 
-  #   def get_all(instance \\ instance_name()) do
-  #     Brook.get_all_values(instance, :dataset)
-  #   end
-
-  #   def get_all!(instance \\ instance_name()) do
-  # case get_all(instance) do
-  #   {:ok, events} -> events
-  #   {:error, reason} -> raise reason
-  # end
-  #   end  
+  defp make_select_table_schema do
+    %{
+      "columns" => ["author", "create_ts", "data", "type"],
+      "table_name" => "event_stream",
+      "order_by" => "",
+      "order" => "DESC",
+      "limit" => 1000
+    }
+  end
 end
