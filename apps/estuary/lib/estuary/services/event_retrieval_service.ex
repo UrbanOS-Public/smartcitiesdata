c2eacc9a18ccf5c1cddf806c5bf3f620ba5c51fd
sukaant <sukaant.chaudhary@gmail.com>
2020-01-21 16:04:17 -0500

    Adding order by

--- apps/estuary/lib/estuary/services/event_retrieval_service.ex
+++ apps/estuary/lib/estuary/services/event_retrieval_service.ex
@@ -5,7 +5,7 @@ defmodule Estuary.Services.EventRetrievalService do
   alias Estuary.Query.Select
 
   def get_all() do
-    case Select.select_table(make_select_table_schema) do
+    case Select.select_table(make_select_table_schema()) do
       {:ok, events} -> events
       {:error, reason} -> raise reason
     end
@@ -15,7 +15,7 @@ defmodule Estuary.Services.EventRetrievalService do
     %{
       "columns" => ["author", "create_ts", "data", "type"],
       "table_name" => "event_stream",
-      "order_by" => "",
+      "order_by" => "create_ts",
       "order" => "DESC",
       "limit" => 1000
     }
