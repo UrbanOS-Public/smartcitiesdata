15bfacb33f741ea35492d2093993266f5665c9d4
Timothy Regan <tregan@pillartechnology.com>
2020-01-27 11:28:01 -0500

    Version bump
    smartcitiesdata/smartcitiesdata#438

--- apps/reaper/mix.exs
+++ apps/reaper/mix.exs
@@ -4,7 +4,7 @@ defmodule Reaper.MixProject do
   def project do
     [
       app: :reaper,
-      version: "0.14.0",
+      version: "0.15.0",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
