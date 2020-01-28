120703ca82cc21319eff5e6bbe8ac3b86fdbce00
kcarra <kevincarrabine23@gmail.com>
2020-01-28 15:39:21 -0500

    bumped umbrella apps versions

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -4,7 +4,7 @@ defmodule Forklift.MixProject do
   def project do
     [
       app: :forklift,
-      version: "0.10.4",
+      version: "0.10.5",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
--- apps/valkyrie/mix.exs
+++ apps/valkyrie/mix.exs
@@ -4,7 +4,7 @@ defmodule Valkyrie.MixProject do
   def project do
     [
       app: :valkyrie,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
