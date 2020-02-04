f5699a62e6facd44093fe40f39e8414b135a0fb4
sukaant <sukaant.chaudhary@gmail.com>
2020-02-04 10:39:00 -0500

    Fixing unit test

--- apps/estuary/test/unit/estuary/services/event_retrieval_service_test.exs
+++ apps/estuary/test/unit/estuary/services/event_retrieval_service_test.exs
@@ -7,22 +7,24 @@ defmodule Estuary.Services.EventRetrievalServiceTest do
 
   @tag capture_log: true
   test "should return the events from the table" do
-    expected_events = [
-      %{
-        "author" => "Author-2020-01-21 23:29:20.171519Z",
-        "create_ts" => 1_579_649_360,
-        "data" => "Data-2020-01-21 23:29:20.171538Z",
-        "type" => "Type-2020-01-21 23:29:20.171543Z"
-      },
-      %{
-        "author" => "Author-2020-01-21 23:25:52.522084Z",
-        "create_ts" => 1_579_649_152,
-        "data" => "Data-2020-01-21 23:25:52.522107Z",
-        "type" => "Type-2020-01-21 23:25:52.522111Z"
-      }
-    ]
+    expected_events =
+      {:ok,
+       [
+         %{
+           "author" => "Author-2020-01-21 23:29:20.171519Z",
+           "create_ts" => 1_579_649_360,
+           "data" => "Data-2020-01-21 23:29:20.171538Z",
+           "type" => "Type-2020-01-21 23:29:20.171543Z"
+         },
+         %{
+           "author" => "Author-2020-01-21 23:25:52.522084Z",
+           "create_ts" => 1_579_649_152,
+           "data" => "Data-2020-01-21 23:25:52.522107Z",
+           "type" => "Type-2020-01-21 23:25:52.522111Z"
+         }
+       ]}
 
-    allow(PrestigeHelper.execute_query(any()), return: {:ok, expected_events})
+    allow(PrestigeHelper.execute_query(any()), return: expected_events)
     assert expected_events = EventRetrievalService.get_all()
   end
 end
