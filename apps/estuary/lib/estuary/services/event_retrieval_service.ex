e1655c850f67ddfd9337217fc6827f8e06659e95
sukaant <sukaant.chaudhary@gmail.com>
2020-02-04 14:19:27 -0500

    Adding Limit

--- apps/estuary/lib/estuary/services/event_retrieval_service.ex
+++ apps/estuary/lib/estuary/services/event_retrieval_service.ex
@@ -6,11 +6,13 @@ defmodule Estuary.Services.EventRetrievalService do
   alias Estuary.Query.Helper.PrestigeHelper
 
   def get_all do
-    "SELECT events.author, events.create_ts, events.data, events.type
+    """
+    SELECT events.author, events.create_ts, events.data, events.type
     FROM (SELECT author, create_ts, data, type
     FROM #{DatasetSchema.table_name()}
-    WHERE create_ts >= to_unixtime(now() - interval '3' hour) LIMIT 1000) events
-    ORDER BY events.create_ts DESC"
+    WHERE create_ts >= to_unixtime(now() - interval '3' hour) LIMIT 5000) events
+    ORDER BY events.create_ts DESC LIMIT 1000
+    """
     |> PrestigeHelper.execute_query()
   end
 end
