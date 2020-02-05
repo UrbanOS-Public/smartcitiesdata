9c67d4331f41c0f21fa4ef43978b8c918004829c
Timothy Regan <tregan@pillartechnology.com>
2020-02-05 09:30:11 -0500

    bumping minor version of all apps using smart_city

--- apps/valkyrie/mix.exs
+++ apps/valkyrie/mix.exs
@@ -4,7 +4,7 @@ defmodule Valkyrie.MixProject do
   def project do
     [
       app: :valkyrie,
-      version: "1.0.2",
+      version: "1.0.3",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
