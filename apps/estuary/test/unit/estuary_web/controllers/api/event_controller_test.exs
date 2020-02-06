12b797278038519571b06844d5558346c7c8a71b
sukaant <sukaant.chaudhary@gmail.com>
2020-02-06 16:21:14 -0500

    Fixing unit test

--- apps/estuary/test/unit/estuary_web/controllers/api/event_controller_test.exs
+++ apps/estuary/test/unit/estuary_web/controllers/api/event_controller_test.exs
@@ -1,12 +1,13 @@
 defmodule EstuaryWeb.API.EventControllerTest do
   use EstuaryWeb.ConnCase
+  # use Plug.Test
   use Placebo
   alias Estuary.Services.EventRetrievalService
 
   describe "GET events from /api/v1/events" do
     @tag capture_log: true
     test "should return a 200 and the events when events are found in the database", %{conn: conn} do
-      expected_events = [
+      events = [
         %{
           "author" => "Author-2020-01-21 23:29:20.171519Z",
           "create_ts" => 1_579_649_360,
@@ -21,14 +22,13 @@ defmodule EstuaryWeb.API.EventControllerTest do
         }
       ]
 
-      allow(EventRetrievalService.get_all(), return: {:ok, expected_events})
+      expected_events =
+        "[{\"author\":\"Author-2020-01-21 23:29:20.171519Z\",\"create_ts\":1579649360,\"data\":\"Data-2020-01-21 23:29:20.171538Z\",\"type\":\"Type-2020-01-21 23:29:20.171543Z\"},{\"author\":\"Author-2020-01-21 23:25:52.522084Z\",\"create_ts\":1579649152,\"data\":\"Data-2020-01-21 23:25:52.522107Z\",\"type\":\"Type-2020-01-21 23:25:52.522111Z\"}]"
+
+      allow(EventRetrievalService.get_all(), return: {:ok, events})
 
       conn = get(conn, "/api/v1/events")
-
-      actual_events =
-        conn
-        |> json_response(200)
-
+      actual_events = conn.resp_body
       assert expected_events == actual_events
     end
 
