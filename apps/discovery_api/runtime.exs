21f872cff92e932209edecf59ec4447dec4bd959
Timothy Regan <tregan@pillartechnology.com>
2020-02-11 09:20:10 -0500

    untested runtime (#520)
    * untested runtime
    
    * use consistent pattern

--- apps/discovery_api/runtime.exs
+++ apps/discovery_api/runtime.exs
@@ -1,12 +1,14 @@
 use Mix.Config
 
-redis_host = System.get_env("REDIS_HOST")
-redis_password = System.get_env("REDIS_PASSWORD", "")
-all_redis_args = [host: redis_host, password: redis_password]
-redix_args = Enum.filter(all_redis_args, fn
-  {_, ""} -> false
-  _ -> true
-end)
+get_redix_args = fn (host, password) ->
+  [host: host, password: password]
+  |> Enum.filter(fn
+    {_, nil} -> false
+    {_, ""} -> false
+    _ -> true
+  end)
+end
+redix_args = get_redix_args.(System.get_env("REDIS_HOST"), System.get_env("REDIS_PASSWORD"))
 
 kafka_brokers = System.get_env("KAFKA_BROKERS")
 endpoint =
