63c50487e10f6ad8347c4fcb9fc05eb7ba42d78b
sukaant <sukaant.chaudhary@gmail.com>
2020-02-12 13:01:52 -0500

    Removing unrelated changes

--- apps/valkyrie/test/unit/valkyrie/topic_manager_test.exs
+++ apps/valkyrie/test/unit/valkyrie/topic_manager_test.exs
@@ -11,8 +11,8 @@ defmodule Valkyrie.TopicManagerTest do
   @output_topic_prefix Application.get_env(:valkyrie, :output_topic_prefix)
 
   test "returns the input and output topic names" do
-    allow(Elsa.create_topic(any(), any()), return: :doesnt_matter)
-    allow(Elsa.topic?(any(), any()), return: true)
+    allow Elsa.create_topic(any(), any()), return: :doesnt_matter
+    allow Elsa.topic?(any(), any()), return: true
     dataset = TDG.create_dataset(id: @dataset_id)
 
     topics = TopicManager.setup_topics(dataset)
@@ -22,25 +22,36 @@ defmodule Valkyrie.TopicManagerTest do
   end
 
   test "creates a topic with the provided input topic name" do
-    allow(Elsa.create_topic(any(), any()), return: :doesnt_matter)
-    allow(Elsa.topic?(any(), any()), return: true)
+    allow Elsa.create_topic(any(), any()), return: :doesnt_matter
+    allow Elsa.topic?(any(), any()), return: true
     dataset = TDG.create_dataset(id: @dataset_id)
 
     TopicManager.setup_topics(dataset)
 
-    assert_called(Elsa.create_topic(@endpoints, "#{@input_topic_prefix}-#{@dataset_id}"))
+    assert_called Elsa.create_topic(@endpoints, "#{@input_topic_prefix}-#{@dataset_id}")
   end
 
   test "verifies input and output topics are available" do
-    allow(Elsa.create_topic(any(), any()), return: :doesnt_matter)
-    allow(Elsa.topic?(any(), "#{@input_topic_prefix}-#{@dataset_id}"), seq: [false, false, true])
-    allow(Elsa.topic?(any(), "#{@output_topic_prefix}-#{@dataset_id}"), seq: [false, false, true])
+    allow Elsa.create_topic(any(), any()), return: :doesnt_matter
+    allow Elsa.topic?(any(), "#{@input_topic_prefix}-#{@dataset_id}"), seq: [false, false, true]
+    allow Elsa.topic?(any(), "#{@output_topic_prefix}-#{@dataset_id}"), seq: [false, false, true]
     dataset = TDG.create_dataset(id: @dataset_id)
 
     TopicManager.setup_topics(dataset)
 
-    assert_called(Elsa.topic?(@endpoints, "#{@input_topic_prefix}-#{@dataset_id}"), times(3))
-    assert_called(Elsa.topic?(@endpoints, "#{@output_topic_prefix}-#{@dataset_id}"), times(3))
+    assert_called Elsa.topic?(@endpoints, "#{@input_topic_prefix}-#{@dataset_id}"), times(3)
+    assert_called Elsa.topic?(@endpoints, "#{@output_topic_prefix}-#{@dataset_id}"), times(3)
+  end
+
+  test "raises an error when it times out waiting for a topic" do
+    allow Elsa.create_topic(any(), any()), return: :doesnt_matter
+    allow Elsa.topic?(any(), "#{@input_topic_prefix}-#{@dataset_id}"), return: true
+    allow Elsa.topic?(any(), "#{@output_topic_prefix}-#{@dataset_id}"), return: false
+    dataset = TDG.create_dataset(id: @dataset_id)
+
+    assert_raise RuntimeError, "Timed out waiting for #{@output_topic_prefix}-#{@dataset_id} to be available", fn ->
+      TopicManager.setup_topics(dataset)
+    end
   end
 
   test "should delete input and output topic when the topic names are provided" do
@@ -48,18 +59,5 @@ defmodule Valkyrie.TopicManagerTest do
     TopicManager.delete_topics(@dataset_id)
     assert_called(Elsa.delete_topic(@endpoints, "#{@input_topic_prefix}-#{@dataset_id}"))
     assert_called(Elsa.delete_topic(@endpoints, "#{@output_topic_prefix}-#{@dataset_id}"))
-  end
-
-  test "raises an error when it times out waiting for a topic" do
-    allow(Elsa.create_topic(any(), any()), return: :doesnt_matter)
-    allow(Elsa.topic?(any(), "#{@input_topic_prefix}-#{@dataset_id}"), return: true)
-    allow(Elsa.topic?(any(), "#{@output_topic_prefix}-#{@dataset_id}"), return: false)
-    dataset = TDG.create_dataset(id: @dataset_id)
-
-    assert_raise RuntimeError,
-                 "Timed out waiting for #{@output_topic_prefix}-#{@dataset_id} to be available",
-                 fn ->
-                   TopicManager.setup_topics(dataset)
-                 end
   end
 end
