5e3f977b6faa3238803bee24416dd5cde010c724
sukaant <sukaant.chaudhary@gmail.com>
2020-02-24 15:10:21 -0500

    Removing helper

--- apps/reaper/lib/reaper/event/handlers/dataset_delete.ex
+++ apps/reaper/lib/reaper/event/handlers/dataset_delete.ex
@@ -3,15 +3,13 @@ defmodule Reaper.Event.Handlers.DatasetDelete do
 
   require Logger
 
-  alias Reaper.Event.Handlers.Helper.DatasetHelper
+  alias Reaper.Event.Handlers.DatasetDisable
   alias Reaper.Topic.TopicManager
 
-  def handle(%SmartCity.Dataset{id: dataset_id}) do
+  def handle(%SmartCity.Dataset{id: dataset_id} = dataset) do
     Logger.debug("#{__MODULE__}: Deleting Datatset: #{dataset_id}")
 
-    with :ok <- DatasetHelper.deactivate_quantum_job(dataset_id),
-         :ok <- DatasetHelper.retry_stopping_dataset(Reaper.Horde.Registry, dataset_id),
-         :ok <- DatasetHelper.retry_stopping_dataset(Reaper.Cache.Registry, dataset_id),
+    with :ok <- DatasetDisable.handle(dataset),
          :ok <- TopicManager.delete_topic(dataset_id) do
       :ok
     end
