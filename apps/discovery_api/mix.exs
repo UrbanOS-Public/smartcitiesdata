705d4b4a68341f63e51ae86e5fed31e86fbb0817
Timothy Regan <tregan@pillartechnology.com>
2020-02-25 14:47:28 -0500

    Discovery Describe Query and Query Web Data Connector (#546)
    * Endpoint to describe an arbitrary query's schema
    
    smartcitiesdata/smartcitiesdata#518
    
    * WIP - Functional query mode. Needs refactored
    
    smartcitiesdata/smartcitiesdata#518
    
    * WIP - Reduce need for builders
    
    smartcitiesdata/smartcitiesdata#518
    
    * WIP - Alternate Metaprogramming
    
    smartcitiesdata/smartcitiesdata#518
    
    * refactored - started adding unit tests for 'query mode'
    
    smartcitiesdata/smartcitiesdata#518
    
    co-authored-by: mcsearchin <abrock17@gmail.com>
    
    * more refactoring; removed tableauInstance
    
    smartcitiesdata/smartcitiesdata#518
    
    co-authored-by: mcsearchin <abrock17@gmail.com>
    
    * Styling and error message
    
    smartcitiesdata/smartcitiesdata#518
    
    co-authored-by: mcsearchin <abrock17@gmail.com>
    
    * restructure test describes
    
    smartcitiesdata/smartcitiesdata#518
    
    co-authored-by: mcsearchin <abrock17@gmail.com>
    
    * error handling
    
    smartcitiesdata/smartcitiesdata#518
    
    co-authored-by: mcsearchin <abrock17@gmail.com>
    
    * version
    
    smartcitiesdata/smartcitiesdata#518
    
    co-authored-by: mcsearchin <abrock17@gmail.com>
    
    * fixed base url for wdc
    
    smartcitiesdata/smartcitiesdata#518
    
    co-authored-by: mcsearchin <abrock17@gmail.com>
    
    * PR feedback
    
    smartcitiesdata/smartcitiesdata#518
    
    co-authored-by: mcsearchin <abrock17@gmail.com>
    
    Co-authored-by: mcsearchin <abrock17@gmail.com>

--- apps/discovery_api/mix.exs
+++ apps/discovery_api/mix.exs
@@ -5,7 +5,7 @@ defmodule DiscoveryApi.Mixfile do
     [
       app: :discovery_api,
       compilers: [:phoenix, :gettext | Mix.compilers()],
-      version: "0.30.0",
+      version: "0.31.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
