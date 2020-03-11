1d138da05a7b5905f82ca772e7c434e6954fdb0b
kcarra <kevincarrabine23@gmail.com>
2020-03-11 10:37:34 -0400

    early exit from build script if provided app does not exist (Ex. discovery-api v discovery_api :|)
    co-authored-by: Benjamin Brewer <benjamin.brewer@accenture.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,11 @@ set -e
 app="${1}"
 version="${2}"
 
+if [[ ! -d apps/$app ]]; then
+  echo "Directory apps/$app does not exist. Please check the app name you provided."
+  exit 1
+fi
+
 docker rmi -f smartcitiesdata:build
 docker build -t smartcitiesdata:build .
 docker build -t smartcitiesdata/$app:$version apps/$app
