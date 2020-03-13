ec3e74c03a1e97d8997936085621445906745dc9
Timothy Regan <tregan@pillartechnology.com>
2020-03-13 13:31:32 -0400

    Refactoring
    smartcitiesdata/smartcitiesdata#574

--- apps/providers/lib/helpers/provisioner.ex
+++ apps/providers/lib/helpers/provisioner.ex
@@ -1,22 +1,22 @@
 defmodule Providers.Helpers.Provisioner do
-  def provision(map) do
+  def provision(map) when is_map(map) do
     map
-    |> Enum.map(&run_if_provider/1)
+    |> Enum.map(fn {key, value} -> {key, run_if_provider(value)} end)
     |> Enum.into(%{})
   end
 
-  def run_if_provider({key, %{provider: _, version: _} = provider}) do
-    {key, run_if_provider(provider)}
+  def provision(list) when is_list(list) do
+    Enum.map(list, &run_if_provider/1)
   end
 
-  def run_if_provider(%{provider: provider_name, version: version} = provider) do
+  defp run_if_provider(%{provider: provider_name, version: version} = provider) do
     provider_opts = Map.get(provider, :opts, [])
-    apply(String.to_existing_atom("Elixir.Providers.#{provider_name}"), :provide, [version, provider_opts])
+    apply(provider_module(provider_name), :provide, [version, provider_opts])
   end
 
-  def run_if_provider({key, value}) when is_map(value), do: {key, provision(value)}
-  def run_if_provider(value) when is_map(value), do: provision(value)
-  def run_if_provider({key, value}) when is_list(value), do: {key, Enum.map(value, &run_if_provider/1)}
+  defp run_if_provider(value) when is_map(value) or is_list(value), do: provision(value)
 
-  def run_if_provider(not_provider), do: not_provider |> IO.inspect()
+  defp run_if_provider(not_provider), do: not_provider
+
+  defp provider_module(provider_name), do: String.to_existing_atom("Elixir.Providers.#{provider_name}")
 end
