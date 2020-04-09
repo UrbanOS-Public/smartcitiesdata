774fd7090a4b6be8ad9f9a27c15aff368dc8e32d
Timothy Regan <tregan@pillartechnology.com>
2020-04-09 12:02:54 -0400

    timestamp provider v2

--- apps/providers/test/unit/providers/timestamp_test.exs
+++ apps/providers/test/unit/providers/timestamp_test.exs
@@ -9,4 +9,32 @@ defmodule Providers.TimestampTest do
     end
   end
 
+  describe "v2" do
+    test "provides a valid timestamp, defaulting to iso8601 format" do
+      timestamp = Providers.Timestamp.provide("2", %{})
+
+      assert {:ok, _timestamp} = NaiveDateTime.from_iso8601(timestamp)
+    end
+
+    test "provides a valid timestamp in the provided timex format" do
+      timestamp = Providers.Timestamp.provide("2", %{format: "{ISOtime}"})
+
+      assert {:ok, _timestamp} = Timex.parse(timestamp, "{ISOtime}")
+    end
+
+    test "provides a valid timestamp in the provided timezone" do
+      timestamp = Providers.Timestamp.provide("2", %{format: "{ISO:Basic}", timezone: "Asia/Ulaanbaatar"})
+
+      assert String.contains?(timestamp, "+0800")
+    end
+
+    test "provides a valid timestamp offset by seconds" do
+      format = "{YYYY}-{0M}-{0D}"
+      offset_in_days = -1
+      timestamp = Providers.Timestamp.provide("2", %{offset_in_seconds: offset_in_days * 24 * 60 * 60, format: format})
+
+      assert Timex.now() |> Timex.add(Timex.Duration.from_days(offset_in_days)) |> Timex.format!(format) == timestamp
+    end
+  end
+
 end
