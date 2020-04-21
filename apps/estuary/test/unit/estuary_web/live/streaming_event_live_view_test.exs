b7b41a7787439881b666d85ed03d56e4f1792b50
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-04-21 16:02:21 -0400

    Adding eventually

--- apps/estuary/test/unit/estuary_web/live/streaming_event_live_view_test.exs
+++ apps/estuary/test/unit/estuary_web/live/streaming_event_live_view_test.exs
@@ -9,6 +9,7 @@ defmodule EstuaryWeb.StreamingEventLiveViewTest do
   import FlokiHelpers, only: [get_text: 2]
 
   alias Estuary.MessageHandler
+  import SmartCity.TestHelper
 
   describe "GET events from /streaming-events" do
     @tag capture_log: true
@@ -35,15 +36,21 @@ defmodule EstuaryWeb.StreamingEventLiveViewTest do
       expected_events =
         "Author Create Timestamp Data Type Author-2020-04-21 23:29:20.171519Z1579649360Data-2020-04-21 23:29:20.171538ZType-2020-04-21 23:29:20.171543ZAuthor-2020-04-21 23:25:52.522084Z1579649152Data-2020-04-21 23:25:52.522107ZType-2020-04-21 23:25:52.522111Z"
 
-      allow(MessageHandler.handle_messages(messages))
-      # Process.sleep(50000)
+      MessageHandler.handle_messages(messages)
 
-      assert {:ok, _view, html} = live(conn, "/streaming-events")
-      actual_events = get_text(html, ".events-index__table") |> IO.inspect(label: "Show Meeee")
-      assert 2 == find_elements(html, ".events-table__tr") |> Enum.count()
-      assert 4 == find_elements(html, ".events-table__th") |> Enum.count()
-      assert 12 == find_elements(html, ".events-table__cell") |> Enum.count()
-      assert expected_events == actual_events
+      eventually(
+        fn ->
+          assert {:ok, view, html} = live(conn, "/streaming-events")
+          html = render(view)
+          actual_events = get_text(html, ".events-index__table") |> IO.inspect(label: "Show Meeee")
+          assert 2 == find_elements(html, ".events-table__tr") |> Enum.count()
+          assert 4 == find_elements(html, ".events-table__th") |> Enum.count()
+          assert 12 == find_elements(html, ".events-table__cell") |> Enum.count()
+          assert expected_events == actual_events
+        end,
+        2000,
+        10
+      )
     end
   end
 
