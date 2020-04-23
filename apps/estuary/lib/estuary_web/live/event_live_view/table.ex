b387b16d6feba270b977f90bbb32e4d930429e7b
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-04-23 11:39:56 -0400

    Adding no events

--- apps/estuary/lib/estuary_web/live/event_live_view/table.ex
+++ apps/estuary/lib/estuary_web/live/event_live_view/table.ex
@@ -14,22 +14,20 @@ defmodule EstuaryWeb.EventLiveView.Table do
         <th class="events-table__th events-table__cell" phx-value-field="create_ts">Create Timestamp </th>
         <th class="events-table__th events-table__cell" phx-value-field="data">Data </th>
         <th class="events-table__th events-table__cell" phx-value-field="type">Type </th>
-        </thead>
-        <%= case @events do %>
-          <%= nil -> %>
-            <tr><td class="events-table__cell" colspan="100%">Waiting For The Events!</td></tr>
-          <%= [] -> %>
-            <tr><td class="events-table__cell" colspan="100%">No Events Found!</td></tr>
-          <% _ -> %>
-            <%= for event <- @events do %>
-              <tr class="events-table__tr">
-                <td class="events-table__cell events-table__cell--break"><%= event["author"] %></td>
-                <td class="events-table__cell events-table__cell--break"><%= event["create_ts"] %></td>
-                <td class="events-table__cell events-table__cell--break"><%= event["data"] %></td>
-                <td class="events-table__cell events-table__cell--break"><%= event["type"] %></td>
-              </tr>
-            <% end %>
+      </thead>
+
+        <%= if @events == [] do %>
+          <tr><td class="events-table__cell" colspan="100%"><%= @no_events %></td></tr>
+        <% else %>
+          <%= for event <- @events do %>
+            <tr class="events-table__tr">
+              <td class="events-table__cell events-table__cell--break"><%= event["author"] %></td>
+              <td class="events-table__cell events-table__cell--break"><%= event["create_ts"] %></td>
+              <td class="events-table__cell events-table__cell--break"><%= event["data"] %></td>
+              <td class="events-table__cell events-table__cell--break"><%= event["type"] %></td>
+            </tr>
           <% end %>
+        <% end %>
       </table>
     </div>
     """
--- apps/estuary/lib/estuary_web/live/streaming_event_live_view.ex
+++ apps/estuary/lib/estuary_web/live/streaming_event_live_view.ex
@@ -8,31 +8,30 @@ defmodule EstuaryWeb.StreamingEventLiveView do
     ~L"""
     <div class="events-index">
       <h1 class="events-index__title">All Streaming Events</h1>
-      <%= live_component(@socket, Table, id: :events_table, events: @events, order: @order) %>
+      <%= live_component(@socket, Table, id: :events_table, events: @events, no_events: @no_events, order: @order) %>
     </div>
     """
   end
 
   def mount(_params, _session, socket) do
     EstuaryWeb.Endpoint.subscribe(@updated_event_stream)
-    {:ok, assign(socket, events: nil, order: {"create_ts", "desc"}, params: %{})}
+
+    {:ok,
+     assign(socket,
+       events: [],
+       no_events: "Waiting For The Events!",
+       order: {"create_ts", "desc"},
+       params: %{}
+     )}
   end
 
   def handle_info(%{topic: @updated_event_stream, payload: %{events: events}}, socket) do
     updated_events =
-      validate_events(events, socket.assigns.events)
+      (List.wrap(events) ++ List.wrap(socket.assigns.events))
+      |> Enum.filter(&(!is_nil(&1)))
       |> Enum.take(1000)
       |> Enum.sort(&(&1["create_ts"] >= &2["create_ts"]))
 
     {:noreply, assign(socket, :events, updated_events)}
-  end
-
-  defp validate_events(events, socket_events) do
-    cond do
-      events == nil and (socket_events == nil or socket_events == [nil]) -> nil
-      events == nil -> socket_events
-      socket_events == nil or socket_events == [nil] -> events
-      true -> events ++ socket_events
-    end
   end
 end
