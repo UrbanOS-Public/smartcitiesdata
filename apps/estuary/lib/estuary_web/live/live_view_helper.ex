208ec5054dedde5673a9b52c0bf0786e147a6b0d
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-05-01 10:58:33 -0400

    Fixing Filter events
    smartcitiesdata/smartcitiesdata#479
    
    co-authored-by: Sukaant Chaudhary <sukaant.chaudhary@gmail.com>

--- apps/estuary/lib/estuary_web/live/live_view_helper.ex
+++ apps/estuary/lib/estuary_web/live/live_view_helper.ex
@@ -2,7 +2,7 @@ defmodule EstuaryWeb.LiveViewHelper do
   def filter_on_search_change(search_value, socket) do
     case search_value == socket.assigns.search_text do
       false ->
-        List.wrap(socket.assigns.events)
+        socket.assigns.events
         |> refresh_events(search_value)
 
       _ ->
@@ -13,32 +13,16 @@ defmodule EstuaryWeb.LiveViewHelper do
   def refresh_events(events, search_value) do
     events
     |> Enum.filter(&(!is_nil(&1)))
-    |> filter_events?(search_value)
+    |> filter_events(search_value)
     |> Enum.take(1000)
     |> Enum.sort(&(&1["create_ts"] >= &2["create_ts"]))
   end
 
-  defp filter_events?(events, nil), do: events
+  defp filter_events(events, nil), do: events
 
-  defp filter_events?(events, ""), do: events
-
-  defp filter_events?(events, value) do
-    case is_list(events) do
-      true -> filter_streaming_events(events, value)
-      _ -> filter_events(events, value)
-    end
-  end
+  defp filter_events(events, ""), do: events
 
   defp filter_events(events, value) do
-    Stream.filter(events, fn event ->
-      search_contains?(event["author"], value) ||
-        search_contains?(event["create_ts"], value) ||
-        search_contains?(event["data"], value) ||
-        search_contains?(event["type"], value)
-    end)
-  end
-
-  defp filter_streaming_events(events, value) do
     Enum.filter(events, fn event ->
       search_contains?(event["author"], value) ||
         search_contains?(event["create_ts"], value) ||
