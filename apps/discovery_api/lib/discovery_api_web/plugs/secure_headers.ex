0789352d179393acd9c5a0446b5459ecbf173e17
Matt Dunson <dunson.142000@gmail.com>
2020-05-22 13:34:12 -0400

    Specified a custom stream for cowboy to use so that it stripped away the server header.

--- apps/discovery_api/lib/discovery_api_web/plugs/secure_headers.ex
+++ apps/discovery_api/lib/discovery_api_web/plugs/secure_headers.ex
@@ -1,4 +1,6 @@
 defmodule DiscoveryApiWeb.Plugs.SecureHeaders do
+  @behaviour :cowboy_stream
+
   @moduledoc """
   A plug to add secure response headers on http traffic
   """
@@ -6,5 +8,30 @@ defmodule DiscoveryApiWeb.Plugs.SecureHeaders do
 
   def call(conn, _default) do
     Phoenix.Controller.put_secure_browser_headers(conn)
+  end
+
+  def info(stream_id, {:response, status, headers, body}, state) do
+    headers = Map.drop(headers, ["server"])
+    :cowboy_stream.info(stream_id, {:response, status, headers, body}, state)
+  end
+
+  def info(stream_id, info, state) do
+    :cowboy_stream.info(stream_id, info, state)
+  end
+
+  def init(stream_id, req, opts) do
+    :cowboy_stream.init(stream_id, req, opts)
+  end
+
+  def data(stream_id, is_fin, info, state) do
+    :cowboy_stream.data(stream_id, is_fin, info, state)
+  end
+
+  def early_error(stream_id, reason, partial_req, resp, opts) do
+    :cowboy_stream.early_error(stream_id, reason, partial_req, resp, opts)
+  end
+
+  def terminate(stream_id, reason, state) do
+    :cowboy_stream.terminate(stream_id, reason, state)
   end
 end
