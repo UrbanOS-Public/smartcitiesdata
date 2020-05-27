70b42f1b1d3fce25b1d26359b25530f27bc179e0
cameronmarsh <cmarsh@pillartechnology.com>
2020-05-27 15:44:54 -0400

    Collapse sections (#695)
    * Create metadata form component
    
    * Create url form component
    
    * Make component headers collapsable
    
    * Add next and back buttons with anchoring
    
    * Move data dictionary form to separate component
    
    * Add snackbar
    
    * Add collapse icons and styling

---
+++ apps/andi/lib/andi_web/live/edit_live_view/data_dictionary_form.ex
@@ -0,0 +1,76 @@
+defmodule AndiWeb.EditLiveView.DataDictionaryForm do
+  @moduledoc """
+  LiveComponent for editing dataset schema
+  """
+  use Phoenix.LiveComponent
+  import Phoenix.HTML.Form
+
+  alias Phoenix.HTML.Link
+  alias AndiWeb.EditLiveView.DataDictionaryTree
+  alias AndiWeb.EditLiveView.DataDictionaryFieldEditor
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    action =
+      case assigns.visibility do
+        "collapsed" -> "EDIT"
+        "expanded" -> "MINIMIZE"
+      end
+
+    ~L"""
+    <div id="data-dictionary-form" class="form-component">
+      <div class="component-header" phx-click="toggle-component-visibility" phx-value-component="data_dictionary_form">
+        <h3 class="component-number component-number--<%= @visibility %>">2</h3>
+        <div class="component-title full-width">
+          <h2 class="component-title-text component-title-text--<%= @visibility %> ">Data Dictionary</h2>
+          <div class="component-title-action">
+            <div class="component-title-action-text--<%= @visibility %>"><%= action %></div>
+            <div class="component-title-icon--<%= @visibility %>"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-section">
+        <div class="component-edit-section--<%= @visibility %>">
+          <div class="data-dictionary-form-edit-section form-grid">
+            <div class="data-dictionary-form__tree-section">
+              <div class="data-dictionary-form__tree-header data-dictionary-form-tree-header">
+                <div class="label">Enter/Edit Fields</div>
+                <div class="label label--inline">TYPE</div>
+              </div>
+
+              <div class="data-dictionary-form__tree-content data-dictionary-form-tree-content">
+                <%= live_component(@socket, DataDictionaryTree, id: :data_dictionary_tree, root_id: :data_dictionary_tree, form: @technical, field: :schema, selected_field_id: @selected_field_id, new_field_initial_render: @new_field_initial_render) %>
+              </div>
+
+              <div class="data-dictionary-form__tree-footer data-dictionary-form-tree-footer" >
+                <div class="data-dictionary-form__add-field-button" phx-click="add_data_dictionary_field"></div>
+                <div class="data-dictionary-form__remove-field-button" phx-click="remove_data_dictionary_field" phx-target="#dataset-edit-page"></div>
+              </div>
+            </div>
+
+            <div class="data-dictionary-form__edit-section">
+              <%= live_component(@socket, DataDictionaryFieldEditor, id: :data_dictionary_field_editor, form: @current_data_dictionary_item) %>
+            </div>
+          </div>
+
+          <div class="edit-button-group form-grid">
+            <div class="edit-button-group__cancel-btn">
+              <a href="#metadata-form" id="back-button" class="btn btn--back btn--large" phx-click="toggle-component-visibility" phx-value-component-expand="metadata_form" phx-value-component-collapse="data_dictionary_form">Back</a>
+              <%= Link.button("Cancel", to: "/", method: "get", class: "btn btn--large") %>
+            </div>
+
+            <div class="edit-button-group__save-btn">
+              <a href="#url-form" id="next-button" class="btn btn--next btn--large btn--action" phx-click="toggle-component-visibility" phx-value-component-expand="url_form" phx-value-component-collapse="data_dictionary_form">Next</a>
+              <%= submit("Save", id: "save-button", name: "save-button", class: "btn btn--save btn--large", phx_value_action: "draft") %>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+end
---
+++ apps/andi/lib/andi_web/live/edit_live_view/url_form.ex
@@ -0,0 +1,82 @@
+defmodule AndiWeb.EditLiveView.UrlForm do
+  @moduledoc """
+  LiveComponent for editing dataset URL
+  """
+  use Phoenix.LiveComponent
+  import Phoenix.HTML.Form
+
+  alias Phoenix.HTML.Link
+  alias AndiWeb.ErrorHelpers
+  alias Andi.InputSchemas.DisplayNames
+  alias AndiWeb.EditLiveView.KeyValueEditor
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    action =
+      case assigns.visibility do
+        "collapsed" -> "EDIT"
+        "expanded" -> "MINIMIZE"
+      end
+
+    ~L"""
+      <div id="url-form" class="form-component">
+        <div class="component-header" phx-click="toggle-component-visibility" phx-value-component="url_form">
+          <h3 class="component-number component-number--<%= @visibility %>">3</h3>
+          <div class="component-title full-width">
+            <h2 class="component-title-text component-title-text--<%= @visibility %> ">Configure Upload</h2>
+            <div class="component-title-action">
+              <div class="component-title-action-text--<%= @visibility %>"><%= action %></div>
+              <div class="component-title-icon--<%= @visibility %>"></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-section">
+          <div class="component-edit-section--<%= @visibility %>">
+            <div class="url-form-edit-section form-grid">
+              <div class="url-form__source-url">
+                <%= label(@technical, :sourceUrl, DisplayNames.get(:sourceUrl), class: "label label--required") %>
+                <%= text_input(@technical, :sourceUrl, class: "input full-width", disabled: @testing) %>
+                <%= ErrorHelpers.error_tag(@technical, :sourceUrl) %>
+              </div>
+
+              <%= live_component(@socket, KeyValueEditor, id: :key_value_editor_source_query_params, css_label: "source-query-params", form: @technical, field: :sourceQueryParams ) %>
+              <%= live_component(@socket, KeyValueEditor, id: :key_value_editor_source_headers, css_label: "source-headers", form: @technical, field: :sourceHeaders ) %>
+
+              <div class="url-form__test-section">
+                <button type="button" class="url-form__test-btn btn--test btn btn--large btn--action" phx-click="test_url" <%= disabled?(@testing) %>>Test</button>
+                <%= if @test_results do %>
+                  <div class="test-status">
+                  Status: <span class="test-status__code <%= status_class(@test_results) %>"><%= @test_results |> Map.get(:status) %></span>
+                  Time: <span class="test-status__time"><%= @test_results |> Map.get(:time) %></span> ms
+                  </div>
+                <% end %>
+              </div>
+            </div>
+
+            <div class="edit-button-group form-grid">
+              <div class="edit-button-group__cancel-btn">
+                <a href="#data-dictionary-form" id="back-button" class="btn btn--back btn--large" phx-click="toggle-component-visibility" phx-value-component-collapse="url_form" phx-value-component-expand="data_dictionary_form">Back</a>
+                <%= Link.button("Cancel", to: "/", method: "get", class: "btn btn--large") %>
+              </div>
+
+              <div class="edit-button-group__save-btn">
+                <a href="#finalize_form" id="next-button" class="btn btn--next btn--large btn--action" phx-click="toggle-component-visibility" phx-value-component-collapse="url_form" phx-value-component-expand="finalize_form">Next</a>
+                <%= submit("Save", id: "save-button", name: "save-button", class: "btn btn--save btn--large", phx_value_action: "draft") %>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    """
+  end
+
+  defp disabled?(true), do: "disabled"
+  defp disabled?(_), do: ""
+
+  defp status_class(%{status: status}) when status in 200..399, do: "test-status__code--good"
+  defp status_class(%{status: _}), do: "test-status__code--bad"
+end
