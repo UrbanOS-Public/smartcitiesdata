48e5629b3e0599a350ecc0cdfd98ad7efa27c939
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-06-19 15:29:31 -0400

    Dynamic port fix for integration

--- apps/telemetry_event/lib/telemetry_event/application.ex
+++ apps/telemetry_event/lib/telemetry_event/application.ex
@@ -13,17 +13,7 @@ defmodule TelemetryEvent.Application do
   end
 
   def metrics_config() do
-    metrics_port = 9002
-    # Application.get_env(:telemetry_event, :metrics_port)
-    #Application.get_env(:reaper, :telemetry_event)
-    # Application.get_all_env(:telemetry_event)
-    #|> Keyword.fetch!(:metrics_port)
-    # :application.get_application(__MODULE__)
-    # :application.which_application |> List.first
-    # __ENV__
-    #|> IO.inspect(label: "Portttt")
-    # IO.puts("Telemetry Prometheus Metrics Hosted On Port No: #{metrics_port}")
-
+    metrics_port = Application.get_env(:telemetry_event, :metrics_port)
     [port: metrics_port, metrics: TelemetryEvent.TelemetryHelper.metrics()]
   end
 end
