1e5344871043b18fa1b533e5bafc1830eca58766
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-06-23 16:15:15 -0400

    Renaming function name

--- apps/telemetry_event/lib/telemetry_event/telemetry_event.ex
+++ apps/telemetry_event/lib/telemetry_event/telemetry_event.ex
@@ -6,31 +6,31 @@ defmodule TelemetryEvent do
     metrics_options = Application.get_env(:telemetry_event, :metrics_options)
 
     [
-      Metrics.counter(fetch(metrics_options, :metric_name), tags: fetch(metrics_options, :tags))
+      Metrics.counter(fetch_required(metrics_options, :metric_name), tags: fetch_required(metrics_options, :tags))
     ]
   end
 
   def add_event_count(options) do
     :telemetry.execute([:events_handled], %{}, %{
-      app: fetch(options, :app),
-      author: fetch(options, :author),
+      app: fetch_required(options, :app),
+      author: fetch_required(options, :author),
       dataset_id: Keyword.fetch!(options, :dataset_id),
-      event_type: fetch(options, :event_type)
+      event_type: fetch_required(options, :event_type)
     })
   rescue
     error -> {:error, error}
   end
 
-  defp fetch(options, keyword_name) do
+  defp fetch_required(options, keyword_name) do
     Keyword.fetch!(options, keyword_name)
-    |> fetch_value(keyword_name)
+    |> reject_nil(keyword_name)
   end
 
-  defp fetch_value(value, keyword_name) when is_nil(value) do
+  defp reject_nil(value, keyword_name) when is_nil(value) do
     raise "Keyword :#{keyword_name} cannot be nil"
   end
 
-  defp fetch_value(value, _) when not is_nil(value) do
+  defp reject_nil(value, _) when not is_nil(value) do
     value
   end
 end
