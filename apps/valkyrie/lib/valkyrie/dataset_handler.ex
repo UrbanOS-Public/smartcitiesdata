6b42e3e7211cfe42b9e958a64f1da5abbb1b4a8b
sukaant <sukaant.chaudhary@gmail.com>
2020-07-09 15:49:25 -0400

    Adding Telemetry to Valkyrie (#780)
    * Adding Telemetry to Valkyrie

--- apps/valkyrie/lib/valkyrie/dataset_handler.ex
+++ apps/valkyrie/lib/valkyrie/dataset_handler.ex
@@ -9,9 +9,13 @@ defmodule Valkyrie.DatasetHandler do
 
   def handle_event(%Brook.Event{
         type: data_ingest_start(),
-        data: %Dataset{technical: %{sourceType: source_type}} = dataset
+        data: %Dataset{technical: %{sourceType: source_type}} = dataset,
+        author: author
       })
       when source_type in ["ingest", "stream"] do
+    data_ingest_start()
+    |> add_event_count(author, dataset.id)
+
     Logger.debug("#{__MODULE__}: Preparing standardization for dataset: #{dataset.id}")
     Valkyrie.DatasetProcessor.start(dataset)
     merge(:datasets, dataset.id, dataset)
@@ -19,8 +23,12 @@ defmodule Valkyrie.DatasetHandler do
 
   def handle_event(%Brook.Event{
         type: data_standardization_end(),
-        data: %{"dataset_id" => dataset_id}
+        data: %{"dataset_id" => dataset_id},
+        author: author
       }) do
+    data_standardization_end()
+    |> add_event_count(author, dataset_id)
+
     Valkyrie.DatasetProcessor.stop(dataset_id)
     Logger.debug("#{__MODULE__}: Standardization finished for dataset: #{dataset_id}")
     delete(:datasets, dataset_id)
@@ -28,8 +36,12 @@ defmodule Valkyrie.DatasetHandler do
 
   def handle_event(%Brook.Event{
         type: dataset_delete(),
-        data: %Dataset{} = dataset
+        data: %Dataset{} = dataset,
+        author: author
       }) do
+    dataset_delete()
+    |> add_event_count(author, dataset.id)
+
     case Valkyrie.DatasetProcessor.delete(dataset.id) do
       :ok ->
         Logger.debug("#{__MODULE__}: Deleted dataset for dataset: #{dataset.id}")
@@ -39,5 +51,15 @@ defmodule Valkyrie.DatasetHandler do
     end
 
     delete(:datasets, dataset.id)
+  end
+
+  defp add_event_count(event_type, author, dataset_id) do
+    [
+      app: "valkyrie",
+      author: author,
+      dataset_id: dataset_id,
+      event_type: event_type
+    ]
+    |> TelemetryEvent.add_event_count()
   end
 end
