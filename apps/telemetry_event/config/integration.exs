18c76d03e34d26b1cf69765b714e2c47aeb6cc20
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-07-22 12:55:55 -0400

    Adding app name to test

--- apps/telemetry_event/config/integration.exs
+++ apps/telemetry_event/config/integration.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :telemetry_event,
+  app_name: "telemetry",
   metrics_options: [
     [
       metric_name: "events_handled.count",
--- apps/telemetry_event/config/test.exs
+++ apps/telemetry_event/config/test.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :telemetry_event,
+  app_name: "any_telemetry_app",
   metrics_options: [
     [
       metric_name: "any_events_handled.count",
