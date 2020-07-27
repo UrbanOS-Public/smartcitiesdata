1b67478d85b20984f512fcde6c123a42aa265d17
sukaant <sukaant.chaudhary@gmail.com>
2020-07-27 15:06:29 -0400

    Replacing nil reason with empty string for dead letter reason (#814)
    * Replacing nil reason with empty string for dead letter reason

--- apps/telemetry_event/test/unit/telemetry_event_test.exs
+++ apps/telemetry_event/test/unit/telemetry_event_test.exs
@@ -12,38 +12,23 @@ defmodule TelemetryEventTest do
              |> TelemetryEvent.add_event_count([:any_event_name])
   end
 
-  test "should return `:error` and error message when `author` is `nil`" do
-    expected_error = {:error, %RuntimeError{message: "Keyword :author cannot be nil"}}
-
-    assert expected_error ==
-             [
-               app: "any_app",
-               author: nil,
-               dataset_id: "any_dataset_id",
-               event_type: "any_event_type"
-             ]
-             |> TelemetryEvent.add_event_count([:any_event_name])
-  end
-
   test "should return `:ok` when `dataset_id` is missing" do
     assert :ok ==
              [
-               app: "any_app",
-               author: "any_author",
-               dataset_id: nil,
-               event_type: "any_event_type"
+               app: "",
+               author: "",
+               dataset_id: "",
+               event_type: ""
              ]
              |> TelemetryEvent.add_event_count([:any_event_name])
   end
 
-  test "should return `:error` and error message when `event_type` is `nil`" do
-    expected_error = {:error, %RuntimeError{message: "Keyword :event_type cannot be nil"}}
-
-    assert expected_error ==
+  test "should return `:ok` any value is nil" do
+    assert :ok ==
              [
-               app: "any_app",
-               author: "any_author",
-               dataset_id: "any_dataset_id",
+               app: nil,
+               author: nil,
+               dataset_id: nil,
                event_type: nil
              ]
              |> TelemetryEvent.add_event_count([:any_event_name])
