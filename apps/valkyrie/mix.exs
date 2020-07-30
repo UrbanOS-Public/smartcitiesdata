67298ba05e8065a40f7215868d6fe8e6b5b67493
sukaant <sukaant.chaudhary@gmail.com>
2020-07-30 10:24:41 -0400

    Adding telemetry integration test to valkyrie (#824)
    * Adding telemetry integration test to valkyrie

--- apps/valkyrie/mix.exs
+++ apps/valkyrie/mix.exs
@@ -4,7 +4,7 @@ defmodule Valkyrie.MixProject do
   def project do
     [
       app: :valkyrie,
-      version: "1.4.0",
+      version: "1.4.1",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
@@ -46,6 +46,7 @@ defmodule Valkyrie.MixProject do
       {:excoveralls, "~> 0.11.1", only: :test},
       {:tasks, in_umbrella: true, only: :dev},
       {:jason, "~> 1.2", override: true},
+      {:httpoison, "~> 1.6"},
       {:libcluster, "~> 3.1"},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:observer_cli, "~> 1.4"},
