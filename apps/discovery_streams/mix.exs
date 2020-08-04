dfb00dde73ca35fbebfa19f515f292cf39aade81
Jessie <jmorris2@pillartechnology.com>
2020-08-04 10:03:22 -0400

    bumping versions

--- apps/discovery_streams/mix.exs
+++ apps/discovery_streams/mix.exs
@@ -4,7 +4,7 @@ defmodule DiscoveryStreams.Mixfile do
   def project do
     [
       app: :discovery_streams,
-      version: "2.5.0",
+      version: "2.6.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
