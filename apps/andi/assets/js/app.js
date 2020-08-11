0334a2fe5eded08c2c32b97f8606e1a2a48c6163
Cameron Marsh <cmarsh@pillartechnology.com>
2020-08-11 11:59:27 -0400

    Prevent snackbar from appearing after save
    smartcitiesdata/smartcitiesdata#786

--- apps/andi/assets/js/app.js
+++ apps/andi/assets/js/app.js
@@ -28,7 +28,7 @@ Hooks.showSnackbar = {
     updated() {
         let snackbar = document.getElementById("snackbar");
         snackbar.className += " show";
-        setTimeout(function() { snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+        setTimeout(function() { snackbar.className = snackbar.className.replace(" show", ""); }, 3000);
     }
 }
 
