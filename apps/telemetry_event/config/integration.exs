3f6a423e04dc9237f7bf98f3a254a0e10dfe793d
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-08-14 00:44:13 -0400

    adding metrics in test

--- apps/telemetry_event/config/integration.exs
+++ apps/telemetry_event/config/integration.exs
@@ -12,5 +12,15 @@ config :telemetry_event,
       metric_name: "dataset_compaction_duration_total.duration",
       tags: [:app, :system_name],
       metric_type: :sum
+    ],
+    [
+      metric_name: "file_conversion_success.gauge",
+      tags: [:app, :dataset_id, :file, :start],
+      metric_type: :last_value
+    ],
+    [
+      metric_name: "file_conversion_duration.gauge",
+      tags: [:app, :dataset_id, :file, :start],
+      metric_type: :last_value
     ]
   ]
