3b60ad3eee46f4bc0145981f1758eaf2ff8f857b
Cameron Marsh <cmarsh@pillartechnology.com>
2020-08-14 11:11:55 -0400

    Fix table and style
    smartcitiesdata/smartcitiesdata#808

--- apps/andi/lib/andi_web/live/organization_live_view.ex
+++ apps/andi/lib/andi_web/live/organization_live_view.ex
@@ -1,7 +1,9 @@
 defmodule AndiWeb.OrganizationLiveView do
   use Phoenix.LiveView
 
+  alias AndiWeb.Router.Helpers, as: Routes
   alias AndiWeb.OrganizationLiveView.Table
+  alias Andi.InputSchemas.Organization
 
   def render(assigns) do
     ~L"""
@@ -40,27 +42,82 @@ defmodule AndiWeb.OrganizationLiveView do
      assign(socket,
        organizations: nil,
        search_text: nil,
-       include_remotes: false,
        order: {"org_title", "asc"},
        params: %{}
      )}
   end
 
   def handle_params(params, _uri, socket) do
-    order_by = Map.get(params, "order-by", "data_title")
+    order_by = Map.get(params, "order-by", "org_title")
     order_dir = Map.get(params, "order-dir", "asc")
     search_text = Map.get(params, "search", "")
 
-    # view_models =
-    #   filter_on_search_change(search_text, include_remotes, socket)
-    #   |> sort_by_dir(order_by, order_dir)
+    view_models =
+      filter_on_search_change(search_text, socket)
+      |> sort_by_dir(order_by, order_dir)
 
     {:noreply,
      assign(socket,
        search_text: search_text,
-       organizations: [],
+       organizations: view_models,
        order: %{order_by => order_dir},
        params: params,
      )}
   end
+
+  def handle_event("search", %{"search-value" => value}, socket) do
+    search_params = Map.merge(socket.assigns.params, %{"search" => value})
+    {:noreply, push_patch(socket, to: Routes.live_path(socket, __MODULE__, search_params))}
+  end
+
+  def handle_event("order-by", %{"field" => field}, socket) do
+    order_dir =
+      case socket.assigns.order do
+        %{^field => "asc"} -> "desc"
+        _ -> "asc"
+      end
+
+    params = Map.merge(socket.assigns.params, %{"order-by" => field, "order-dir" => order_dir})
+    {:noreply, push_patch(socket, to: Routes.live_path(socket, __MODULE__, params))}
+  end
+
+  defp filter_on_search_change(search_value, socket) do
+    case search_value == socket.assigns.search_text do
+      false -> refresh_orgs(search_value)
+      _ -> socket.assigns.organizations
+    end
+  end
+
+  defp refresh_orgs(search_value) do
+    Andi.Repo.all(Organization)
+    |> filter_orgs(search_value)
+    |> Enum.map(&to_view_model/1)
+  end
+
+  defp filter_orgs(orgs, ""), do: orgs
+
+  defp filter_orgs(orgs, value) do
+    Enum.filter(orgs, fn org -> search_contains?(org.orgTitle, value) end)
+  end
+
+  defp search_contains?(nil, _search_str), do: false
+
+  defp search_contains?(str, search_str) do
+    String.downcase(str) =~ String.downcase(search_str)
+  end
+
+  defp sort_by_dir(models, order_by, order_dir) do
+    case order_dir do
+      "asc" -> Enum.sort_by(models, fn model -> Map.get(model, order_by) end)
+      "desc" -> Enum.sort_by(models, fn model -> Map.get(model, order_by) end, &>=/2)
+      _ -> models
+    end
+  end
+
+  defp to_view_model(org) do
+    %{
+      "org_title" => org.orgTitle
+    }
+  end
+
 end
--- apps/andi/lib/andi_web/live/organization_live_view/table.ex
+++ apps/andi/lib/andi_web/live/organization_live_view/table.ex
@@ -4,16 +4,13 @@ defmodule AndiWeb.OrganizationLiveView.Table do
   """
 
   use Phoenix.LiveComponent
-  import Phoenix.HTML
-  alias Phoenix.HTML.Link
 
   def render(assigns) do
     ~L"""
     <div id="<%= @id %>" class="datasets-index__table">
       <table class="datasets-table">
       <thead>
-        <th class="datasets-table__th datasets-table__cell datasets-table__th--sortable datasets-table__th--<%= Map.get(@order, "data_title", "unsorted") %>" phx-click="order-by" phx-value-field="data_title">Dataset Name </th>
-        <th class="datasets-table__th datasets-table__cell datasets-table__th--sortable datasets-table__th--<%= Map.get(@order, "org_title", "unsorted") %>" phx-click="order-by" phx-value-field="org_title">Organization </th>
+        <th class="datasets-table__th datasets-table__cell datasets-table__th--sortable datasets-table__th--<%= Map.get(@order, "org_title", "unsorted") %>" phx-click="order-by" phx-value-field="org_title">Organization</th>
         <th class="datasets-table__th datasets-table__cell">Actions</th>
         </thead>
 
@@ -22,8 +19,6 @@ defmodule AndiWeb.OrganizationLiveView.Table do
         <% else %>
           <%= for org <- @organizations do %>
           <tr class="datasets-table__tr">
-            <td class="datasets-table__cell datasets-table__cell--break datasets-table__ingested-cell"><%= ingest_status(org) %></td>
-            <td class="datasets-table__cell datasets-table__cell--break"><%= org["data_title"] %></td>
             <td class="datasets-table__cell datasets-table__cell--break"><%= org["org_title"] %></td>
             <td class="datasets-table__cell datasets-table__cell--break"></td>
           </tr>
@@ -32,9 +27,5 @@ defmodule AndiWeb.OrganizationLiveView.Table do
       </table>
     </div>
     """
-  end
-
-  defp ingest_status(org) do
-    ""
   end
 end
