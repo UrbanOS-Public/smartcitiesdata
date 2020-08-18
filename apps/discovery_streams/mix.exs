21b9772f773baef7a59073d6267aee5693863bf1
sukaant <sukaant.chaudhary@gmail.com>
2020-08-18 14:10:30 -0400

    Formatting (#858)

--- apps/discovery_streams/mix.exs
+++ apps/discovery_streams/mix.exs
@@ -4,7 +4,7 @@ defmodule DiscoveryStreams.Mixfile do
   def project do
     [
       app: :discovery_streams,
-      version: "2.7.0",
+      version: "2.7.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
