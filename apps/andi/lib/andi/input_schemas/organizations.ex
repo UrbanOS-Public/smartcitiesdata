21095711bfff66b396010b1a64129e16afffb206
k.carrabine <kevincarrabine23@gmail.com>
2020-08-19 12:08:17 -0400

    wip

--- apps/andi/lib/andi/input_schemas/organizations.ex
+++ apps/andi/lib/andi/input_schemas/organizations.ex
@@ -61,11 +61,25 @@ defmodule Andi.InputSchemas.Organizations do
     Repo.insert_or_update(changeset)
   end
 
+  def update_harvested_dataset(harvested_dataset, changes) do
+    changes = changes |> AtomicMap.convert(safe: false, underscore: false)
+
+    HarvestedDatasets.changeset(harvested_dataset, changes)
+    |> save()
+  end
+
   def update_harvested_dataset(harvested_dataset) do
     changes = harvested_dataset |> AtomicMap.convert(safe: false, underscore: false)
 
     HarvestedDatasets.changeset(changes)
     |> save()
+  end
+
+  def update_harvested_dataset_include(source_id, val) when is_boolean(val) do
+    case get_harvested_dataset(source_id) do
+      nil -> Logger.error("Harvested dataset #{source_id} doesn't exist")
+      dataset -> update_harvested_dataset(dataset, %{include: val})
+    end
   end
 
   def is_unique?(id, org_name) do
