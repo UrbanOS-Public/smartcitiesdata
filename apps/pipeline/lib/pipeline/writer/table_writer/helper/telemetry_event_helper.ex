a44e77bc7d076a9c106c52ad703231286c03350b
sukaant <sukaant.chaudhary@gmail.com>
2020-08-25 13:59:01 -0400

    Adding dataset record count (#877)
    * Adding dataset record count

---
+++ apps/pipeline/lib/pipeline/writer/table_writer/helper/telemetry_event_helper.ex
@@ -0,0 +1,17 @@
+defmodule Pipeline.Writer.TableWriter.Helper.TelemetryEventHelper do
+  @moduledoc false
+
+  require Logger
+
+  def add_dataset_record_event_count(count, table_name) when is_integer(count) do
+    [
+      table_name: table_name
+    ]
+    |> TelemetryEvent.add_event_metrics([:dataset_record_total], value: %{count: count})
+  rescue
+    error ->
+      Logger.error("Unable to update the metrics: #{error}")
+  end
+
+  def add_dataset_record_event_count(_, _), do: :ok
+end
