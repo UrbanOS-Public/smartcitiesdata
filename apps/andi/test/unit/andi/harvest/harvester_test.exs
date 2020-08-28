2e80549c332316b531588314a77c29bd68875f9f
Cameron Marsh <cmarsh@pillartechnology.com>
2020-08-28 16:24:05 -0400

    Rebase probs
    smartcitiesdata/smartcitiesdata#743

--- apps/andi/test/unit/andi/harvest/harvester_test.exs
+++ apps/andi/test/unit/andi/harvest/harvester_test.exs
@@ -109,18 +109,6 @@ defmodule Andi.Harvest.HarvesterTest do
 
       assert date.day == current_date.day
     end
-
-    test "datasets without a modified date should be set to todays date", %{data_json: data_json, org: org} do
-      {:ok, data_json} = Jason.decode(data_json)
-      datasets = Harvester.map_data_json_to_dataset(data_json, org)
-
-      [unmodified_dataset] = datasets |> Enum.filter(fn dataset -> dataset.id == "e13fc009-7ccd-511f-8895-a7c0c50b5b86" end)
-
-      {:ok, date, _} = unmodified_dataset.business.modifiedDate |> DateTime.from_iso8601()
-      current_date = DateTime.utc_now()
-
-      assert date.day == current_date.day
-    end
   end
 
   defp get_schema_from_path(path) do
