fe495eb8671a793d08c7c79be81aa3fac5eec6b3
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-09-02 14:52:52 -0400

    Removing init_server module

--- apps/telemetry_event/lib/telemetry_event/telemetry_event.ex
+++ apps/telemetry_event/lib/telemetry_event/telemetry_event.ex
@@ -1,12 +1,11 @@
 defmodule TelemetryEvent do
   @moduledoc false
   alias TelemetryEvent.Helper.TelemetryEventHelper
-  alias TelemetryEvent.Helper.AddInitServer
 
   def config_init_server(child, app_name) do
     child
-    |> AddInitServer.add_metrics_prometheus(app_name)
-    |> AddInitServer.add_poller()
+    |> add_metrics_prometheus(app_name)
+    |> add_poller()
   end
 
   def add_event_metrics(event_tags_and_values, event_name, measurement \\ []) do
@@ -17,5 +16,19 @@ defmodule TelemetryEvent do
     )
   rescue
     error -> {:error, error}
+  end
+
+  defp add_metrics_prometheus(child, app_name) do
+    case Application.get_env(:telemetry_event, :init_server) do
+      false -> child
+      _ -> [{TelemetryMetricsPrometheus, TelemetryEventHelper.metrics_config(app_name)} | child]
+    end
+  end
+
+  defp add_poller(child) do
+    case Application.get_env(:telemetry_event, :add_poller) do
+      true -> [{:telemetry_poller, measurements: [], period: :timer.seconds(5)} | child]
+      _ -> child
+    end
   end
 end
