f939488435e358ef9a0e0763349a27bfe30cdc05
Cameron Marsh <cmarsh@pillartechnology.com>
2020-09-03 14:32:04 -0400

    Add icons and style status
    smartcitiesdata/smartcitiesdata#778

--- apps/andi/lib/andi_web/live/dataset_live_view/table.ex
+++ apps/andi/lib/andi_web/live/dataset_live_view/table.ex
@@ -25,9 +25,10 @@ defmodule AndiWeb.DatasetLiveView.Table do
           <%= for dataset <- @datasets do %>
             <% ingest_status = ingest_status(dataset) %>
             <% ingest_cell_class = get_ingest_cell_class(ingest_status) %>
+            <% ingest_status_html = get_html_from_ingest_status(ingest_status) %>
 
             <tr class="datasets-table__tr">
-              <td class="datasets-table__cell datasets-table__cell--break datasets-table__ingested-cell--<%= ingest_cell_class %>" style="width: 10%;"><%= ingest_status %></td>
+              <td class="datasets-table__cell datasets-table__cell--break datasets-table__ingested-cell--<%= ingest_cell_class %>" style="width: 10%;"><%= ingest_status_html %></td>
               <td class="datasets-table__cell datasets-table__cell--break datasets-table__data-title-cell"><%= dataset["data_title"] %></td>
               <td class="datasets-table__cell datasets-table__cell--break"><%= dataset["org_title"] %></td>
               <td class="datasets-table__cell datasets-table__cell--break" style="width: 10%;"><%= Link.link("Edit", to: "/datasets/#{dataset["id"]}", class: "btn") %></td>
@@ -37,6 +38,25 @@ defmodule AndiWeb.DatasetLiveView.Table do
       </table>
     </div>
     """
+  end
+
+  defp get_html_from_ingest_status(""), do: ""
+  defp get_html_from_ingest_status("Failure") do
+    ~E(
+      <div class="ingestion-status">
+        <div class="ingestion-failure-icon"></div>
+        <div class="ingestion-failure-text">Failure</div>
+      </div>
+    )
+  end
+
+  defp get_html_from_ingest_status("Success") do
+    ~E(
+      <div class="ingestion-status">
+        <div class="ingestion-success-icon"></div>
+        <div class="ingestion-success-text">Success</div>
+      </div>
+    )
   end
 
   defp get_ingest_cell_class(""), do: "unset"
