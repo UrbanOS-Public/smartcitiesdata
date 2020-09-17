128d6b38594a13dac107cd56091d62e7e98c29ca
sukaant <sukaant.chaudhary@gmail.com>
2020-09-17 08:50:05 -0400

    Dataset info (#945)
    * Adding dataset info

--- apps/telemetry_event/config/test.exs
+++ apps/telemetry_event/config/test.exs
@@ -34,5 +34,10 @@ config :telemetry_event,
       metric_name: "any_dataset_record_total.count",
       tags: [:any_table_name],
       metric_type: :last_value
+    ],
+    [
+      metric_name: "any_dataset_info.gauge",
+      tags: [:any_dataset_id, :any_dataset_title, :any_system_name, :any_source_type, :any_org_name],
+      metric_type: :last_value
     ]
   ]
