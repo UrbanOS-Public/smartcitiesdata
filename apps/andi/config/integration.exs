7bd773a7de9090ad153d896dde6b3f403fab2022
Cameron Marsh <cmarsh@pillartechnology.com>
2020-09-17 10:11:19 -0400

    Format
    smartcolumbusos/scosopedia#252

--- apps/andi/config/integration.exs
+++ apps/andi/config/integration.exs
@@ -33,13 +33,14 @@ config :andi, AndiWeb.Endpoint,
   http: [port: 4000],
   server: true,
   check_origin: false
-  # for auth0 login use
-  # https: [
-  #   port: 4443,
-  #   otp_app: :andi,
-  #   keyfile: "priv/key.pem",
-  #   certfile: "priv/cert.pem"
-  # ]
+
+# for auth0 login use, add to Endpoint config above
+# https: [
+#   port: 4443,
+#   otp_app: :andi,
+#   keyfile: "priv/key.pem",
+#   certfile: "priv/cert.pem"
+# ]
 
 config :andi, :brook,
   instance: :andi,
