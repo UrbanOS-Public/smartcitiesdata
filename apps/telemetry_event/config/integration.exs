4603f02b449a66b7ce09eea7fb135ad416462ffe
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-09-21 08:29:42 -0400

    Cleanup

--- apps/telemetry_event/config/integration.exs
+++ apps/telemetry_event/config/integration.exs
@@ -29,10 +29,5 @@ config :telemetry_event,
       metric_name: "dataset_info.gauge",
       tags: [:dataset_id, :dataset_title, :system_name, :source_type, :org_name],
       metric_type: :last_value
-    ],
-    [
-      metric_name: "dataset_info_total.count",
-      tags: [:dataset_id, :dataset_title, :system_name, :source_type, :org_name],
-      metric_type: :last_value
     ]
   ]
