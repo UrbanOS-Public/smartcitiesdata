7043f1d401730301da38b81d319ae31143c3c216
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-09-20 22:37:09 -0400

    Initial commit

--- apps/telemetry_event/config/integration.exs
+++ apps/telemetry_event/config/integration.exs
@@ -22,11 +22,16 @@ config :telemetry_event,
     ],
     [
       metric_name: "dataset_record_total.count",
-      tags: [:table_name],
+      tags: [:system_name],
       metric_type: :last_value
     ],
     [
       metric_name: "dataset_info.gauge",
+      tags: [:dataset_id, :dataset_title, :system_name, :source_type, :org_name],
+      metric_type: :last_value
+    ],
+    [
+      metric_name: "dataset_info_total.count",
       tags: [:dataset_id, :dataset_title, :system_name, :source_type, :org_name],
       metric_type: :last_value
     ]
