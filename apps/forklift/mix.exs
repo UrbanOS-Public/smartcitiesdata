bf65b58c3dc13b90ca92e149f82711a42cb53fe3
Benjamin Brewer <ben@benbrewer.me>
2020-09-25 14:13:46 -0400

    oops
    co-authored-by: Cameron Marsh <cameron.marsh@accenture.com>

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -56,7 +56,7 @@ defmodule Forklift.MixProject do
       {:telemetry_event, in_umbrella: true},
       {:pipeline, in_umbrella: true},
       {:httpoison, "~> 1.5"},
-      {:mox, "~> 0.5.1", only: [:dev, :test, :integration]}
+      {:mox, "~> 0.5.1", only: [:dev, :test, :integration]},
       {:performance, in_umbrella: true, only: :integration},
     ]
   end
