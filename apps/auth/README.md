b466da31ba669af46c588adb2412d5a5a284605e
Cameron Marsh <cmarsh@pillartechnology.com>
2020-10-02 16:13:17 -0400

    Remove auth boilerplate
    smartcolumbusos/scosopedia#252

--- apps/auth/README.md
+++ apps/auth/README.md
@@ -1,7 +1,5 @@
 # Auth
 
-**TODO: Add description**
-
 ## Installation
 
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed
--- apps/auth/lib/auth.ex
+++ apps/auth/lib/auth.ex
@@ -1,18 +1,3 @@
 defmodule Auth do
-  @moduledoc """
-  Documentation for Auth.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Auth.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
