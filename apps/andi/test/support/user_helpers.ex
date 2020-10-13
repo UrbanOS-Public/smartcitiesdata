ef59ba416ad1376ddb7101adb407664708745326
Timothy Regan <tregan@pillartechnology.com>
2020-10-13 16:13:43 -0400

    user helpers

---
+++ apps/andi/test/support/user_helpers.ex
@@ -0,0 +1,11 @@
+defmodule UserHelpers do
+  @moduledoc false
+
+  alias Andi.Schemas.User
+
+  import Placebo
+
+  def create_user(id \\ "123", subject_id \\ "auth0|test", email \\ "test@example.com") do
+    %User{id: id, subject_id: subject_id, email: email}
+  end
+end
