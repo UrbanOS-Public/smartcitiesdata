6b16cc96f8ba071cfdbf8cdd3d4e951d8f38168b
sukaant <sukaant.chaudhary@gmail.com>
2020-10-19 13:46:04 -0400

    Setting Compaction to gauge

--- apps/forklift/runtime.exs
+++ apps/forklift/runtime.exs
@@ -147,7 +147,7 @@ config :telemetry_event,
     [
       metric_name: "dataset_compaction_duration_total.duration",
       tags: [:app, :system_name],
-      metric_type: :sum
+      metric_type: :last_value
     ],
     [
       metric_name: "dataset_record_total.count",
