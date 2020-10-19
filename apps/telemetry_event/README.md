f54b059a38d2fa375e8b85f8c76b81e2c290373b
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-10-19 14:24:11 -0400

    Setting Compaction to gauge (#1010)

--- apps/telemetry_event/README.md
+++ apps/telemetry_event/README.md
@@ -84,7 +84,7 @@ config :telemetry_event,
     [
       metric_name: "dataset_compaction_duration_total.duration",
       tags: [:app, :system_name],
-      metric_type: :sum
+      metric_type: :last_value
     ],
     [
       metric_name: "dataset_record_total.count",
--- apps/telemetry_event/config/test.exs
+++ apps/telemetry_event/config/test.exs
@@ -18,7 +18,7 @@ config :telemetry_event,
     [
       metric_name: "any_dataset_compaction_duration_total.duration",
       tags: [:any_app, :any_system_name],
-      metric_type: :sum
+      metric_type: :last_value
     ],
     [
       metric_name: "any_file_conversion_success.gauge",
