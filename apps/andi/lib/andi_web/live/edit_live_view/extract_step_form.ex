037fea8df623c1f9d145dc8d8ea329a60411d994
Timothy Regan <tregan@pillartechnology.com>
2020-10-23 12:35:31 -0400

    Remove TODO
    datastillery/smartcitiesdata#983
    
    co-authored-by: Cameron Marsh <cameron.marsh@accenture.com>

--- apps/andi/lib/andi_web/live/edit_live_view/extract_step_form.ex
+++ apps/andi/lib/andi_web/live/edit_live_view/extract_step_form.ex
@@ -93,7 +93,6 @@ defmodule AndiWeb.EditLiveView.ExtractStepForm do
 
                 <%= live_component(@socket, KeyValueEditor, id: :key_value_editor_headers, css_label: "source-headers", form: f, field: :headers ) %>
 
-                <!-- TODO: should we make this default to valid JSON? -->
                 <%= if input_value(f, :method) == "POST" do %>
                   <div class="extract-step-form__body">
                     <%= label(f, :body, DisplayNames.get(:body), class: "label") %>
