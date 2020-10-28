aa252248d30da9505dad3c01009a3e132a5bb671
Benjamin Brewer <benjamin.brewer@accenture.com>
2020-10-28 12:27:18 -0400

    do credo conditionally

--- scripts/gh-action-static-checks.sh
+++ scripts/gh-action-static-checks.sh
@@ -5,7 +5,10 @@ app="$1"
 
 cd apps/$app
 mix format --check-formatted
-mix credo
+
+if mix help credo >/dev/null 2>&1; then
+    mix credo
+fi
 
 if mix help sobelow >/dev/null 2>&1; then
     mix sobelow
