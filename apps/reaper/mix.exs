c3b7f43bd2ba471a50c4e9b43eeafac8a7db6e95
Benjamin Brewer <benjamin.brewer@accenture.com>
2020-10-28 14:34:38 -0400

    bump versions

--- apps/reaper/mix.exs
+++ apps/reaper/mix.exs
@@ -4,7 +4,7 @@ defmodule Reaper.MixProject do
   def project do
     [
       app: :reaper,
-      version: "0.24.4",
+      version: "0.24.5",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
