8aebe31805405115f2e8c8e22c1929e302831c62
sukaant <sukaant.chaudhary@gmail.com>
2020-11-05 17:57:10 -0500

    Replacing Application.get_env with getter

--- apps/valkyrie/lib/valkyrie/topic_manager.ex
+++ apps/valkyrie/lib/valkyrie/topic_manager.ex
@@ -3,17 +3,21 @@ defmodule Valkyrie.TopicManager do
   Create Topics in kafka using the Elsa library.
   """
   use Retry
+  use Properties, otp_app: :valkyrie
 
-  @initial_delay Application.get_env(:valkyrie, :retry_initial_delay)
-  @retries Application.get_env(:valkyrie, :retry_count)
+  getter(:retry_initial_delay, generic: true)
+  getter(:retry_count, generic: true)
+  getter(:elsa_brokers, generic: true)
+  getter(:output_topic_prefix, generic: true)
+  getter(:input_topic_prefix, generic: true)
 
   @spec setup_topics(%SmartCity.Dataset{}) :: %{input_topic: String.t(), output_topic: String.t()}
   def setup_topics(dataset) do
     input_topic = input_topic(dataset.id)
     output_topic = output_topic(dataset.id)
 
-    Elsa.create_topic(endpoints(), input_topic)
-    Elsa.create_topic(endpoints(), output_topic)
+    Elsa.create_topic(elsa_brokers(), input_topic)
+    Elsa.create_topic(elsa_brokers(), output_topic)
     wait_for_topic(input_topic)
     wait_for_topic(output_topic)
 
@@ -23,13 +27,13 @@ defmodule Valkyrie.TopicManager do
   def delete_topics(dataset_id) do
     input_topic = input_topic(dataset_id)
     output_topic = output_topic(dataset_id)
-    Elsa.delete_topic(endpoints(), input_topic)
-    Elsa.delete_topic(endpoints(), output_topic)
+    Elsa.delete_topic(elsa_brokers(), input_topic)
+    Elsa.delete_topic(elsa_brokers(), output_topic)
   end
 
   def wait_for_topic(topic) do
-    retry with: @initial_delay |> exponential_backoff() |> Stream.take(@retries), atoms: [false] do
-      Elsa.topic?(endpoints(), topic)
+    retry with: retry_initial_delay() |> exponential_backoff() |> Stream.take(retry_count()), atoms: [false] do
+      Elsa.topic?(elsa_brokers(), topic)
     after
       true ->
         nil
@@ -38,9 +42,6 @@ defmodule Valkyrie.TopicManager do
     end
   end
 
-  defp endpoints(), do: Application.get_env(:valkyrie, :elsa_brokers)
-  defp output_topic_prefix(), do: Application.get_env(:valkyrie, :output_topic_prefix)
   defp output_topic(dataset_id), do: "#{output_topic_prefix()}-#{dataset_id}"
-  defp input_topic_prefix(), do: Application.get_env(:valkyrie, :input_topic_prefix)
   defp input_topic(dataset_id), do: "#{input_topic_prefix()}-#{dataset_id}"
 end
