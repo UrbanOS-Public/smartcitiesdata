5c68875d6456fa3f4bbaeeba034b1b59e9ebcfce
Benjamin Brewer <benjamin.brewer@accenture.com>
2020-11-12 14:01:59 -0500

    stylin

--- apps/andi/lib/andi_web/live/dataset_live_view/table.ex
+++ apps/andi/lib/andi_web/live/dataset_live_view/table.ex
@@ -11,7 +11,7 @@ defmodule AndiWeb.DatasetLiveView.Table do
     <div id="<%= @id %>" class="datasets-index__table">
       <table class="datasets-table">
       <thead>
-        <th class="datasets-table__th datasets-table__cell datasets-table__th--sortable datasets-table__th--<%= Map.get(@order, "ingested_time", "unsorted") %>" phx-click="order-by" phx-value-field="ingested_time">Status</th>
+        <th class="datasets-table__th datasets-table__cell datasets-table__status-cell datasets-table__th--sortable datasets-table__th--<%= Map.get(@order, "ingested_time", "unsorted") %>" phx-click="order-by" phx-value-field="ingested_time">Status</th>
         <th class="datasets-table__th datasets-table__cell datasets-table__th--sortable datasets-table__th--<%= Map.get(@order, "data_title", "unsorted") %>" phx-click="order-by" phx-value-field="data_title">Dataset Name </th>
         <th class="datasets-table__th datasets-table__cell datasets-table__th--sortable datasets-table__th--<%= Map.get(@order, "org_title", "unsorted") %>" phx-click="order-by" phx-value-field="org_title">Organization </th>
         <th class="datasets-table__th datasets-table__cell">Actions</th>
