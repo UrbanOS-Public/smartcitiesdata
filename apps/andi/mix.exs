a42466c7f24ccaddba7776aaadc3183b68a93243
cameronmarsh <cmarsh10@gmail.com>
2020-11-13 10:41:59 -0500

    Bump version

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.57.0",
+      version: "0.58.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
