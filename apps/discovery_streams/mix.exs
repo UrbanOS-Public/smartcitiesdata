efc7e77bc5cb7d5b1fe40b6ef8c9795b7b7d2b80
Timothy Regan <tregan@pillartechnology.com>
2020-11-20 10:15:47 -0500

    version bump v2

--- apps/discovery_streams/mix.exs
+++ apps/discovery_streams/mix.exs
@@ -4,7 +4,7 @@ defmodule DiscoveryStreams.Mixfile do
   def project do
     [
       app: :discovery_streams,
-      version: "2.10.4",
+      version: "2.10.5",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
