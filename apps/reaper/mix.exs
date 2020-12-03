ae65d8cfa6202c42368b1d9ed7a9ef360a6b03a0
Cameron Marsh <cmarsh@pillartechnology.com>
2020-12-03 13:02:39 -0500

    bump versions
    Datastillery/smartcitiesdata#1101

--- apps/reaper/mix.exs
+++ apps/reaper/mix.exs
@@ -4,7 +4,7 @@ defmodule Reaper.MixProject do
   def project do
     [
       app: :reaper,
-      version: "0.24.7",
+      version: "0.24.8",
       elixir: "~> 1.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
