4b51e391c21e46d121320e4a1f4bcf2d7c800224
Sukaant Chaudhary <sukaant.chaudhary@gmail.com>
2020-12-10 12:15:04 -0500

    Updating Hackney (#1112)

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.64.6",
+      version: "0.64.7",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
