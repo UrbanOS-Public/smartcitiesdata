110cc2b293ef4ebd97dbd4e8fb0b87dc8025ad8c
Kyle Smith <kyle@kmaismith.com>
2021-01-18 16:45:24 -0500

    "passing" integration test for multiple_data_controller
    datastillery/smartcitiesdata#1147
    
    co-authored-by: Cameron Marsh <cameron.marsh@accenture.com>

--- apps/discovery_api/test/integration/discovery_api_web/controllers/multiple_data_controller_test.exs
+++ apps/discovery_api/test/integration/discovery_api_web/controllers/multiple_data_controller_test.exs
@@ -1,22 +1,29 @@
 defmodule DiscoveryApiWeb.MultipleDataControllerTest do
   use ExUnit.Case
-  # use DiscoveryApi.DataCase
 
   use DiscoveryApiWeb.Test.AuthConnCase.IntegrationCase
   alias DiscoveryApi.Test.Helper
 
+  @organization_name "organization_alpha"
+
   setup_all do
-    Helper.sample_model(%{
-      systemName: "test__table"
-    })
-      |> Helper.save_model
+    _ = Helper.create_persisted_organization(%{orgName: @organization_name})
+
+    {table_name, dataset_id} = Helper.create_persisted_dataset("test_data", "test_data", @organization_name)
+
+    %{dataset_table: table_name, dataset_id: dataset_id}
   end
 
   describe "POST /query" do
-    test "something", %{authorized_conn: conn} do
-      _ = conn
+    test "something", %{
+        authorized_conn: authorized_conn,
+        authorized_subject: subject,
+        dataset_table: dataset_table
+      } do
+      _ = Helper.create_persisted_user(subject)
+      _ = authorized_conn
           |> put_req_header("content-type", "text/plain")
-          |> post("/api/v1/query", "SELECT * FROM test__table")
+          |> post("/api/v1/query", "SELECT * FROM #{dataset_table}")
           |> response(200)
     end
   end
