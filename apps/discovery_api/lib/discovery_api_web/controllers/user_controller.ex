9384e6149419155cf5c966ceca0b77f815c3ac44
cameronmarsh <cameron.marsh@accenture.com>
2021-03-09 13:18:05 -0500

    Emit and listen to user:login events (#1206)
    Datastillery/smartcitiesdata#1126

--- apps/discovery_api/lib/discovery_api_web/controllers/user_controller.ex
+++ apps/discovery_api/lib/discovery_api_web/controllers/user_controller.ex
@@ -2,15 +2,19 @@ defmodule DiscoveryApiWeb.UserController do
   require Logger
   use DiscoveryApiWeb, :controller
 
+  import SmartCity.Event, only: [user_login: 0]
   alias DiscoveryApi.Services.AuthService
   alias DiscoveryApi.Schemas.Users
   alias DiscoveryApiWeb.Auth.TokenHandler
+
+  @instance_name DiscoveryApi.instance_name()
 
   def logged_in(conn, _params) do
     with {:ok, user_info} <- AuthService.get_user_info(Guardian.Plug.current_token(conn)),
          {:ok, email} <- Map.fetch(user_info, "email"),
          subject_id <- Guardian.Plug.current_claims(conn)["sub"],
-         {:ok, _user} <- Users.create_or_update(subject_id, %{email: email}) do
+         {:ok, user} <- Users.create_or_update(subject_id, %{email: email}) do
+      Brook.Event.send(@instance_name, user_login(), __MODULE__, user)
       conn |> send_resp(:ok, "")
     else
       error ->
