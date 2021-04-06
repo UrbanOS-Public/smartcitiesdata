4f21c0bc8f5dc18f252400a68424f229db0c8a26
Timothy Regan <tregan@pillartechnology.com>
2021-04-06 10:27:45 -0400

    version-bump

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.75.1",
+      version: "0.76.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
