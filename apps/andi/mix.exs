aa1ac2c772e57dbbf89fc39baf2825b0fc689ab3
Timothy Regan <tregan@pillartechnology.com>
2021-04-21 13:37:19 -0400

    ssri update (#1230)

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.76.2",
+      version: "0.76.3",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
