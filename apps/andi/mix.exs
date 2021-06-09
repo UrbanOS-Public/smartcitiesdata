d0fe103ac27ee0b6769e49dbfd6f02d9a99ae151
christinepoydence <73911735+christinepoydence@users.noreply.github.com>
2021-06-09 12:04:38 -0400

    update patch version (#1254)

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.76.7",
+      version: "0.76.8",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
