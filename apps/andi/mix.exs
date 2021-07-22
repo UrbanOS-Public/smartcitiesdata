eca092ab07dd6e7c0fd21bbaeff3d600b0aaf703
Christine (Walsh) Poydence <73911735+christinepoydence@users.noreply.github.com>
2021-07-22 10:11:40 -0400

    Update mix.exs version in ANDI (#1274)
    Datastillery/charts#269
    
    co-authored-by: Saad Rasheed <saad.a.rasheed@accenture.com>

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "0.77.0",
+      version: "1.0.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
