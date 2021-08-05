df26ea75f15007be45eb682df5d71e085ba268e0
Christine (Walsh) Poydence <73911735+christinepoydence@users.noreply.github.com>
2021-08-05 16:09:40 -0400

    increment version (#1278)
    Datastillery/internal#39
    
    Co-authored-by: Christine Walsh <christinepoydence@gmail.com>
    Co-authored-by: Saad Rasheed <saad.a.rasheed@accenture.com>

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "2.0.0",
+      version: "2.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
