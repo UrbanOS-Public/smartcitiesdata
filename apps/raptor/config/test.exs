bbbf00d0882a3cbb8e953b586b5762a6fd11f4c2
Nicole Arruda <narruda789@gmail.com>
2021-08-26 16:54:46 -0400

    1283: Removing unused test module (#1289)
    * Removing unused test module
    
    Datastillery/smartcitiesdata#1283
    
    co-authored-by: Christine Walsh <christine.poydence@gmail.com>
    co-authored-by: Nicole Arruda <narruda789@gmail.com>

--- apps/raptor/config/test.exs
+++ apps/raptor/config/test.exs
@@ -1,9 +1,5 @@
 use Mix.Config
 
-host = "localhost"
-endpoints = [{to_charlist(host), 9092}]
-redix_args = [host: host]
-
 config :raptor, RaptorWeb.Endpoint,
   url: [scheme: "https", host: "data.integrationtests.example.com", port: 443],
   http: [protocol_options: [inactivity_timeout: 4_000_000, idle_timeout: 4_000_000]]
@@ -12,9 +8,6 @@ config :raptor,
   allowed_origins: ["integrationtests.example.com", "localhost:9001"],
   divo: "test/integration/docker-compose.yaml",
   divo_wait: [dwell: 2000, max_tries: 35]
-
-config :redix,
-  args: redix_args
 
 config :phoenix,
   serve_endpoints: true,
