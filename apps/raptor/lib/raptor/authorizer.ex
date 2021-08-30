77089e2b2169c2644171d454456c10fb611d5c05
Christine Walsh <christine.walsh@accenture.com>
2021-08-30 16:23:38 -0400

    add authorizer logic Datastillery/internal#34

--- apps/raptor/lib/raptor/authorizer.ex
+++ apps/raptor/lib/raptor/authorizer.ex
@@ -1,10 +1,21 @@
 defmodule Raptor.Authorizer do
   alias Raptor.Services.Auth0Management
+  require Logger
 
   def validate_user_list(user_list) do
-    case user_list do
-      [] -> false
-      _ -> true
+    case length(user_list) do
+      0 ->
+        Logger.error("No user found with given API Key.")
+        false
+
+      1 ->
+        user = user_list |> Enum.at(0)
+        # Only users who have validated their email address may make API calls
+        user["email_verified"]
+
+      _ ->
+        Logger.error("Multiple users cannot have the same API Key.")
+        false
     end
   end
 
