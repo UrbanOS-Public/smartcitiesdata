5d83937c41059e5e40d8e5efc49518fb4e51c18b
Nicole Arruda <nicole.arruda@accenture.com>
2021-09-14 14:00:29 -0400

    More unit tests
    Datastillery/internal#35
    
    co-authored-by: Christine Walsh <christine.poydence@gmail.com>
    
    Co-authored-by: Mario Vongphachant <MVongphachant@users.noreply.github.com>

---
+++ apps/raptor/test/unit/schemas/dataset_test.exs
@@ -0,0 +1,18 @@
+defmodule Schemas.DatasetTest do
+
+  use RaptorWeb.ConnCase
+  alias Raptor.Schemas.Dataset
+  alias SmartCity.TestDataGenerator, as: TDG
+
+  test "convert from dataset event to Raptor dataset schema", %{conn: conn} do
+    dataset = TDG.create_dataset(%{id: "cool_data", technical: %{systemName: "awesome__cool_data", orgId: "awesome"}})
+    {:ok, actualDataset} = Dataset.from_event(dataset)
+    expected = %Dataset{
+      dataset_id: "cool_data",
+      system_name: "awesome__cool_data",
+      org_id: "awesome"
+    }
+
+    assert expected == actualDataset
+  end
+end
