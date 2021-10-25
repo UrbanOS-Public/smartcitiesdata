da379fddcca1d9e390eb5439dd41579188fe094c
Nicole Arruda <narruda789@gmail.com>
2021-10-25 12:32:57 -0400

    Using Raptor to allow private dataset streaming (hardcoded API key and system name for now)
    Datastillery/internal#37

--- apps/discovery_streams/lib/discovery-streams/event/event_handler.ex
+++ apps/discovery_streams/lib/discovery-streams/event/event_handler.ex
@@ -12,24 +12,34 @@ defmodule DiscoveryStreams.Event.EventHandler do
         data: %Dataset{id: id, technical: %{sourceType: "stream", private: false, systemName: system_name}} = dataset,
         author: author
       }) do
-    add_event_count(data_ingest_start(), author, id)
-
-    save_dataset_to_viewstate(id, system_name)
-    DiscoveryStreams.Stream.Supervisor.start_child(dataset.id)
+    begin_data_ingestion(author, id, system_name, dataset.id)
     :ok
   end
 
   def handle_event(%Brook.Event{
-        type: dataset_update(),
-        data: %Dataset{technical: %{private: true}} = dataset,
-        author: author
-      }) do
-    add_event_count(dataset_update(), author, dataset.id)
-
-    delete_from_viewstate(dataset.id, dataset.technical.systemName)
-    DiscoveryStreams.Stream.Supervisor.terminate_child(dataset.id)
-
+    type: data_ingest_start(),
+    data: %Dataset{id: id, technical: %{sourceType: "stream", private: true, systemName: system_name}} = dataset,
+    author: author
+  }) do
+    {:ok, auth} = get_auth()
+    if auth["is_authorized"] do
+      begin_data_ingestion(author, id, system_name, dataset.id)
+    end
     :ok
+  end
+
+  def begin_data_ingestion(author, id, system_name, dataset_id) do
+    add_event_count(data_ingest_start(), author, id)
+    save_dataset_to_viewstate(id, system_name)
+    DiscoveryStreams.Stream.Supervisor.start_child(dataset_id)
+  end
+
+  def get_auth() do
+    case HTTPoison.get("http://localhost:4002/api/authorize?apiKey=5JIaldG1vCorgLoHgtCRXlhj&systemName=nicole_llc__data") do
+      {:ok, %{body: body}} ->
+        Jason.decode(body)
+      error -> error
+    end
   end
 
   def handle_event(%Brook.Event{
