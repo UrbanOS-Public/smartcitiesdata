4c2309e5eda5da91ece1aec59eab2f53b66eade7
Nicole Arruda <narruda789@gmail.com>
2021-10-29 10:58:04 -0400

    Fixing path to Raptor in prod (#1306)
    Datastillery/internal#37

--- apps/discovery_streams/config/prod.exs
+++ apps/discovery_streams/config/prod.exs
@@ -9,7 +9,8 @@ config :discovery_streams, DiscoveryStreamsWeb.Endpoint,
 
 config :discovery_streams, DiscoveryStreamsWeb.Endpoint, check_origin: false
 
-config :discovery_streams, :raptor, url: "http://raptor/api/authorize"
+config :discovery_streams,
+  raptor_url: "http://raptor.admin/api/authorize"
 
 config :logger,
   level: :info
