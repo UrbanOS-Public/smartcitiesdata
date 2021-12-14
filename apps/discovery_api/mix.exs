d488f1f84872e655be085e0d7021c191a943956d
Ben Mitchinson <mitchinson.dev@gmail.com>
2021-12-14 16:04:47 -0600

    version bump to streams + api

--- apps/discovery_api/mix.exs
+++ apps/discovery_api/mix.exs
@@ -5,7 +5,7 @@ defmodule DiscoveryApi.Mixfile do
     [
       app: :discovery_api,
       compilers: [:phoenix, :gettext | Mix.compilers()],
-      version: "1.2.1",
+      version: "1.2.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
