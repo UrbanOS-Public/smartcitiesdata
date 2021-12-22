8395465e8a1d9a33b47fa4912a2475fcf628eb79
Ben Mitchinson <mitchinson.dev@gmail.com>
2021-12-22 13:10:38 -0600

    Use raptor service for plug + model query service (#1318)

--- apps/discovery_api/mix.exs
+++ apps/discovery_api/mix.exs
@@ -5,7 +5,7 @@ defmodule DiscoveryApi.Mixfile do
     [
       app: :discovery_api,
       compilers: [:phoenix, :gettext | Mix.compilers()],
-      version: "1.2.2",
+      version: "1.2.3",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
