ec25ab6f13af56d109aa462e365d00f452ea7fc6
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-01-05 17:19:40 -0600

    SSL Redis - Reaper / Forklift / Disc_Streams (#1319)

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -4,7 +4,7 @@ defmodule Forklift.MixProject do
   def project do
     [
       app: :forklift,
-      version: "0.17.13",
+      version: "0.17.14",
       elixir: "~> 1.10",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
+cd "$(dirname ${BASH_SOURCE[0]})"
+cd ".."
+
+nl=$'\n'
+
+if [[ $1 == "" || $1 == "help" || $2 == "" ]]; then
+  echo "build {app_name} {image_tag} {rebuild_build_image} {repository}${nl}"
+  echo "  Defaults:"
+  echo "  - rebuild_build_image: true"
+  echo "  - repository: smartcitiesdata/{app_name}${nl}"
+  echo "  Note: build_image is *all* of the elixir apps compiled."
+  echo "        Only needs to be rebuilt once for changes across multiple apps.${nl}"
+  exit 1
+fi
 
 set -e
 
 app="${1}"
-version="${2}"
+tag="${2}"
 rebuild_build_image="${3:-true}"
+repository="${4:-smartcitiesdata/$app}"
 
 if [[ ! -d apps/$app ]]; then
   echo "Directory apps/$app does not exist. Please check the app name you provided."
@@ -12,8 +27,12 @@ if [[ ! -d apps/$app ]]; then
 fi
 
 if ([[ "$(docker images -q smartcitiesdata:build 2> /dev/null)" == "" ]] || $rebuild_build_image ); then
-  docker rmi -f smartcitiesdata:build
+  echo "Rebuilding elixir build image..."
+  docker rmi -f smartcitiesdata:build 2> /dev/null
   docker build -t smartcitiesdata:build .
 fi
 
-docker build -t smartcitiesdata/$app:$version apps/$app
+echo "Building $repository:$tag"
+docker build -t $repository:$tag apps/$app
+
+echo "${nl}Built ${repository}:${tag}${nl}"
