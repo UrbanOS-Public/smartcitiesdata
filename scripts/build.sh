284cb4a713c75b8f9e0c1b4d844be2080836db8f
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-01-10 11:30:59 -0600

    Add optional "push" flag to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,8 @@ if [[ $1 == "" || $1 == "help" || $2 == "" ]]; then
   echo "  Defaults:"
   echo "  - rebuild_build_image: true"
   echo "  - repository: smartcitiesdata/{app_name}${nl}"
+  echo "  Flags:"
+  echo "   --push: Runs docker push on the built image"
   echo "  Note: build_image is *all* of the elixir apps compiled."
   echo "        Only needs to be rebuilt once for changes across multiple apps.${nl}"
   exit 1
@@ -35,4 +37,11 @@ fi
 echo "Building $repository:$tag"
 docker build -t $repository:$tag apps/$app
 
-echo "${nl}Built ${repository}:${tag}${nl}"
+echo "${nl}Built ${repository}:${tag}$"
+
+if [[ $* == *--push* ]]; then
+  echo "Pushing ${repository}:${tag}"
+  docker push "${repository}:${tag}"
+fi
+
+echo "Done ${nl}"
