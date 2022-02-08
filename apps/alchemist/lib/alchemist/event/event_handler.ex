de0670eb5d747bf16f2dde70c987d3ae695e4d9d
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-02-08 13:13:42 -0600

    Revert lockfile + use smart_city v3

--- apps/alchemist/lib/alchemist/event/event_handler.ex
+++ apps/alchemist/lib/alchemist/event/event_handler.ex
@@ -5,13 +5,18 @@ defmodule Alchemist.Event.EventHandler do
 
   import SmartCity.Event,
     only: [
-      ingestion_update: 0,
+      # ingestion_update: 0,
       organization_update: 0,
       user_organization_associate: 0,
       user_organization_disassociate: 0
     ]
 
-  alias SmartCity.{UserOrganizationAssociate, UserOrganizationDisassociate, Organization, IngestionDataset}
+  alias SmartCity.{
+    UserOrganizationAssociate,
+    UserOrganizationDisassociate,
+    Organization
+    # Ingestion
+  }
 
   @instance_name Alchemist.instance_name()
 
@@ -44,14 +49,14 @@ defmodule Alchemist.Event.EventHandler do
     :discard
   end
 
-  def handle_event(
-        %Brook.Event{
-          type: ingestion_update(),
-          data: %Organization{} = data,
-          author: author
-        } = event
-      ) do
-    IO.inspect(data, label: "I received this ingestion event")
-    :discard
-  end
+  # def handle_event(
+  #       %Brook.Event{
+  #         type: ingestion_update(),
+  #         data: %Ingestion{} = data,
+  #         author: author
+  #       } = event
+  #     ) do
+  #   IO.inspect(data, label: "I received this ingestion event")
+  #   :discard
+  # end
 end
