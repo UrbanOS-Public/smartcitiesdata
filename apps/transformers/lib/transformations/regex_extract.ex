6eb3e3642abba15dabd2922b229b29c13ec8978f
Nicole Arruda <narruda789@gmail.com>
2022-02-17 16:41:37 -0500

    Happy path of regex transform
    Datastillery/internal#508
    
    co-authored-by: Ben Mitchinson <mitchinson.dev@gmail.com>
    co-authored-by: Michael Pawlowski <dicearx@gmail.com>

--- apps/transformers/lib/transformations/regex_extract.ex
+++ apps/transformers/lib/transformations/regex_extract.ex
@@ -1,8 +1,26 @@
 defmodule Transformers.RegexExtract do
   @behaviour Transformation
 
-  @impl
-  def transform(message, parameters) do
-    message
+  @impl Transformation
+  def transform(payload, parameters) do
+    source_field = parameters.sourceField
+
+    case Map.fetch(payload, source_field) do
+      :error ->
+        {:error, "Field #{source_field} not found"}
+
+      {:ok, value} ->
+        {:ok, regex} = Regex.compile(parameters.regex)
+
+        case Regex.run(regex, value, capture: :all_but_first) do
+          nil ->
+            transformed_payload = Map.put(payload, parameters.targetField, nil)
+            {:ok, transformed_payload}
+
+          [extracted_value | _] ->
+            transformed_payload = Map.put(payload, parameters.targetField, extracted_value)
+            {:ok, transformed_payload}
+        end
+    end
   end
 end
--- apps/transformers/test/unit/transformations/regex_extract_test.exs
+++ apps/transformers/test/unit/transformations/regex_extract_test.exs
@@ -3,23 +3,53 @@ defmodule Transformers.RegexExtractTest do
 
   alias SmartCity.TestDataGenerator, as: TDG
 
-  # describe "The regex extract transform" do
-  #    ? validate that it has all the parameters it needs?
-  #      ^ not needed. On all instances of a Transformation, if "transform"
-  #       fails, we'll handle that in whoever is consuming this library (alchemist)
-  #   # We don't want a struct for each transformations parameter set right?
-  #   test "returns an error if the specified source field does not exist" do
-  #     params = %{
-  #       sourceField: "source_field",
-  #       targetField: "target_field",
-  #       regex: "^\((\d{3})\)"
-  #     }
+  describe "The regex extract transform" do
+    test "returns an error if the specified source field does not exist" do
+      params = %{
+        sourceField: "source_field",
+        targetField: "target_field",
+        regex: "^\((\d{3})\)"
+      }
 
-  #     message = TDG.create_data(%{})
+      message_payload = %{"some_other_field" => "not what you were expecting"}
 
-  #     {:error, reason} = Transformers.RegexExtract.transform(message, params)
+      {:error, reason} = Transformers.RegexExtract.transform(message_payload, params)
 
-  #     assert reason == :invalid_message
-  #   end
-  # end
+      assert reason == "Field source_field not found"
+    end
+
+    test "returns payload with extracted value in target field" do
+      params = %{
+        sourceField: "phone_number",
+        targetField: "area_code",
+        regex: "^\\((\\d{3})\\)"
+      }
+
+      message_payload = %{"phone_number" => "(555) 123-4567"}
+
+      {:ok, transformed_payload} = Transformers.RegexExtract.transform(message_payload, params)
+
+      {:ok, actual_target_field} = Map.fetch(transformed_payload, "area_code")
+      assert actual_target_field == "555"
+    end
+
+    test "returns payload with null value in target field if no regex match" do
+      params = %{
+        sourceField: "phone_number",
+        targetField: "area_code",
+        regex: "bananas"
+      }
+
+      message_payload = %{"phone_number" => "(555) 123-4567"}
+
+      {:ok, transformed_payload} = Transformers.RegexExtract.transform(message_payload, params)
+
+      {:ok, actual_target_field} = Map.fetch(transformed_payload, "area_code")
+      assert actual_target_field == nil
+    end
+
+    #  If target field already exists, overwrite it
+    #  Error case: if regex does not compile, message is sent to dead letter queue
+    #  Error case: if no matching source field, message is sent to dead letter queue
+  end
 end
