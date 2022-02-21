b772d107387260240bf79f1f3412eb824c702f96
Nicole Arruda <narruda789@gmail.com>
2022-02-21 13:56:16 -0500

    Refactoring
    Datastillery/internal#508
    
    co-authored-by: Michael Pawlowski <dicearx@gmail.com>

--- apps/transformers/lib/transformations/regex_extract.ex
+++ apps/transformers/lib/transformations/regex_extract.ex
@@ -2,30 +2,28 @@ defmodule Transformers.RegexExtract do
   @behaviour Transformation
 
   @impl Transformation
+
   def transform(payload, parameters) do
     source_field = parameters.sourceField
 
-    case Map.fetch(payload, source_field) do
+    with {:ok, value} <- Map.fetch(payload, source_field),
+         {:ok, regex} <- Regex.compile(parameters.regex) do
+      case Regex.run(regex, value, capture: :all_but_first) do
+        nil ->
+          transformed_payload = Map.put(payload, parameters.targetField, nil)
+          {:ok, transformed_payload}
+
+        [extracted_value | _] ->
+          transformed_payload = Map.put(payload, parameters.targetField, extracted_value)
+
+          {:ok, transformed_payload}
+      end
+    else
+      {:error, {message, index}} ->
+        {:error, "Invalid regular expression: #{message} at index #{index}"}
+
       :error ->
         {:error, "Field #{source_field} not found"}
-
-      {:ok, value} ->
-        case Regex.compile(parameters.regex) do
-          {:error, {message, index}} ->
-            {:error, "Invalid regular expression: #{message} at index #{index}"}
-
-          {:ok, regex} ->
-            case Regex.run(regex, value, capture: :all_but_first) do
-              nil ->
-                transformed_payload = Map.put(payload, parameters.targetField, nil)
-                {:ok, transformed_payload}
-
-              [extracted_value | _] ->
-                transformed_payload = Map.put(payload, parameters.targetField, extracted_value)
-
-                {:ok, transformed_payload}
-            end
-        end
     end
   end
 end
