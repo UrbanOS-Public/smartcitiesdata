49dfb24e8cf855ce788e181e2e5472a20338853c
Nicole Arruda <narruda789@gmail.com>
2022-02-22 17:37:32 -0500

    Handling attempt to build unsupported transformation function
    Datastillery/internal#508

--- apps/transformers/lib/function_builder.ex
+++ apps/transformers/lib/function_builder.ex
@@ -2,4 +2,8 @@ defmodule Transformers.FunctionBuilder do
   def build(:regex_extract, parameters) do
     fn payload -> Transformers.RegexExtract.transform(payload, parameters) end
   end
+
+  def build(unsupported, _) do
+    raise "Unsupported transformation type: #{unsupported}"
+  end
 end
--- apps/transformers/test/unit/function_builder_test.exs
+++ apps/transformers/test/unit/function_builder_test.exs
@@ -16,4 +16,10 @@ defmodule Transformers.FunctionBuilderTest do
     assert first_name_extractor_function.(payload) ==
              Transformers.RegexExtract.transform(payload, first_name_extractor_parameters)
   end
+
+  test "attempting to build function for unsupported transformation raises error" do
+    assert_raise RuntimeError,
+                 "Unsupported transformation type: unsupported_transformation",
+                 fn -> Transformers.FunctionBuilder.build(:unsupported_transformation, %{}) end
+  end
 end
