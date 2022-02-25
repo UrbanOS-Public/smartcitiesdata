643e7edcfe19c1b6e4cb61229d143d4414e83bda
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-02-25 16:38:59 -0500

    Return descriptive error when field param is missing

--- apps/transformers/lib/transformations/type_conversion.ex
+++ apps/transformers/lib/transformations/type_conversion.ex
@@ -4,14 +4,22 @@ defmodule Transformers.TypeConversion do
   @impl Transformation
   def transform(payload, params) do
 
-    with {:ok, field} <- Map.fetch(params, :field),
+    with {:ok, field} <- fetchParameter(params, :field),
          {:ok, value} <- Map.fetch(payload, field) do
       if(value == nil or value == "") do
         Map.put(payload, field, nil)
       end
     else
+      {:error, reason} -> {:error, reason}
       :error -> {:error, "Field to convert does not exist in message"}
     end
-    
+
+  end
+
+  defp fetchParameter(params, field_name) do
+    case Map.fetch(params, field_name) do
+      {:ok, field} -> {:ok, field}
+      :error -> {:error, "Missing transformation parameter: #{field_name}"}
+    end
   end
 end
--- apps/transformers/test/unit/transformations/type_conversion_test.exs
+++ apps/transformers/test/unit/transformations/type_conversion_test.exs
@@ -7,7 +7,7 @@ defmodule Transformers.TypeConversionTest do
 
     result = Transformers.TypeConversion.transform(payload, parameters)
 
-    assert payload = result
+    assert payload == result
   end
 
   test "when field is empty string do nothing" do
@@ -16,7 +16,7 @@ defmodule Transformers.TypeConversionTest do
 
     result = Transformers.TypeConversion.transform(payload, parameters)
 
-    assert %{"nothing" => nil} = result
+    assert %{"nothing" => nil} == result
   end
 
   test "when field is missing return error" do
@@ -25,6 +25,15 @@ defmodule Transformers.TypeConversionTest do
 
     result = Transformers.TypeConversion.transform(payload, parameters)
 
-    assert {:error, "Field to convert does not exist in message"} = result
+    assert {:error, "Field to convert does not exist in message"} == result
+  end
+
+  test "if params do not contain field return error" do
+    payload = %{"something" => "some_value"}
+    parameters = %{sourceType: "string", targetType: "float"}
+
+    result = Transformers.TypeConversion.transform(payload, parameters)
+
+    assert {:error, "Missing transformation parameter: field"} == result
   end
 end
