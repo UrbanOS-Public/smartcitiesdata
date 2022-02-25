5d97d2be7c14dea0cf3f8d36f10ff2775b0d77be
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-02-25 18:38:08 -0500

    Reordering supported conversion clauses

--- apps/transformers/lib/transformations/type_conversion.ex
+++ apps/transformers/lib/transformations/type_conversion.ex
@@ -35,10 +35,10 @@ defmodule Transformers.TypeConversion do
 
   defp pick_conversion(source_type, target_type) do
     case {source_type, target_type} do
-      {"integer", "float"} -> {:ok, fn value -> value / 1 end}
-      {"integer", "string"} -> {:ok, fn value -> to_string(value) end}
       {"float", "integer"} -> {:ok, fn value -> Float.round(value) end}
       {"float", "string"} -> {:ok, fn value -> to_string(value) end}
+      {"integer", "float"} -> {:ok, fn value -> value / 1 end}
+      {"integer", "string"} -> {:ok, fn value -> to_string(value) end}
       {"string", "integer"} -> {:ok, fn value -> String.to_integer(value) end}
       {"string", "float"} -> {:ok, fn value -> String.to_float(value) end}
       _ -> {:error, "Conversion from #{source_type} to #{target_type} is not supported"}
@@ -65,7 +65,7 @@ defmodule Transformers.TypeConversion do
       {:error, "Field #{field} not of expected type: #{source_type}"}
     end
   end
-  
+
   defp parse_or_error(conversion_function, payload, field, value, target_type) do
     try do
       transformed_value = conversion_function.(value)
