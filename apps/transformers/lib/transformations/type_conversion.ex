242f08af4db3ecd21fb42cb5eac4e7e6573b1624
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-02-25 18:37:10 -0500

    Reordering functions

--- apps/transformers/lib/transformations/type_conversion.ex
+++ apps/transformers/lib/transformations/type_conversion.ex
@@ -19,12 +19,17 @@ defmodule Transformers.TypeConversion do
 
   end
 
-  defp parse_or_error(conversion_function, payload, field, value, target_type) do
-    try do
-      transformed_value = conversion_function.(value)
-      Map.put(payload, field, transformed_value)
-    rescue
-      _ -> {:error, "Cannot parse field #{field} with value #{value} into #{target_type}"}
+  defp fetch_parameter(params, field_name) do
+    case Map.fetch(params, field_name) do
+      {:ok, field} -> {:ok, field}
+      :error -> {:error, "Missing transformation parameter: #{field_name}"}
+    end
+  end
+
+  defp fetch_value(payload, field_name) do
+    case Map.fetch(payload, field_name) do
+      {:ok, field} -> {:ok, field}
+      :error -> {:error, "Missing field in payload: #{field_name}"}
     end
   end
 
@@ -48,20 +53,6 @@ defmodule Transformers.TypeConversion do
     end
   end
 
-  defp fetch_parameter(params, field_name) do
-    case Map.fetch(params, field_name) do
-      {:ok, field} -> {:ok, field}
-      :error -> {:error, "Missing transformation parameter: #{field_name}"}
-    end
-  end
-
-  defp fetch_value(payload, field_name) do
-    case Map.fetch(payload, field_name) do
-      {:ok, field} -> {:ok, field}
-      :error -> {:error, "Missing field in payload: #{field_name}"}
-    end
-  end
-
   defp check_field_is_of_sourcetype(field, value, source_type) do
     function = case source_type do
       "float" -> fn value -> is_float(value) end
@@ -74,4 +65,15 @@ defmodule Transformers.TypeConversion do
       {:error, "Field #{field} not of expected type: #{source_type}"}
     end
   end
+  
+  defp parse_or_error(conversion_function, payload, field, value, target_type) do
+    try do
+      transformed_value = conversion_function.(value)
+      Map.put(payload, field, transformed_value)
+    rescue
+      _ -> {:error, "Cannot parse field #{field} with value #{value} into #{target_type}"}
+    end
+  end
+
+
 end
