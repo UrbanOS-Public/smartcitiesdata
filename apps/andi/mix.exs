8e381ce74c1d7d800327f6223358436738ec34a3
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-02-25 10:26:46 -0500

    Bumping other service versions because of dependency change

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "2.0.4",
+      version: "2.0.5",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/estuary/mix.exs
+++ apps/estuary/mix.exs
@@ -4,7 +4,7 @@ defmodule Estuary.MixProject do
   def project do
     [
       app: :estuary,
-      version: "0.11.8",
+      version: "0.11.9",
       elixir: "~> 1.10",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
