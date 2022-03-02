ee82c0e32eff685aeac01b5c3d08e18dcb48eac1
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-03-02 13:45:31 -0600

    Up version numbers

--- apps/transformers/mix.exs
+++ apps/transformers/mix.exs
@@ -4,7 +4,7 @@ defmodule Transformers.MixProject do
   def project do
     [
       app: :transformers,
-      version: "0.2.1",
+      version: "0.2.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
