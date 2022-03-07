c41204a6bce5b73d14e7daf0dd0fcfb5d7989005
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-03-07 14:16:24 -0500

    Formatting

--- apps/transformers/lib/transformations/remove.ex
+++ apps/transformers/lib/transformations/remove.ex
@@ -7,8 +7,8 @@ defmodule Transformers.Remove do
   def transform(payload, parameters) do
     with {:ok, source_field} <- FieldFetcher.fetch_parameter(parameters, :sourceField),
          {:ok, _} <- FieldFetcher.fetch_value(payload, source_field) do
-           transformed_payload = Map.delete(payload, source_field)
-           {:ok, transformed_payload}
+      transformed_payload = Map.delete(payload, source_field)
+      {:ok, transformed_payload}
     else
       {:error, reason} -> {:error, reason}
     end
