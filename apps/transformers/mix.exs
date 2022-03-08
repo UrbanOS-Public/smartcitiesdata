6f8ca121728274bbf6b1bd5f19324719e8e5a490
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-03-08 16:50:31 -0500

    Also for transformers

--- apps/transformers/mix.exs
+++ apps/transformers/mix.exs
@@ -4,7 +4,7 @@ defmodule Transformers.MixProject do
   def project do
     [
       app: :transformers,
-      version: "0.2.4",
+      version: "0.2.5",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
