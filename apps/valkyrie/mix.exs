40be65fe4cb94269996e04954b0cc4146d1bf32b
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-03-09 14:08:05 -0600

    Up mix version #

--- apps/valkyrie/mix.exs
+++ apps/valkyrie/mix.exs
@@ -4,7 +4,7 @@ defmodule Valkyrie.MixProject do
   def project do
     [
       app: :valkyrie,
-      version: "1.7.9",
+      version: "1.7.10",
       elixir: "~> 1.10",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
