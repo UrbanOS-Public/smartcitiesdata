e15aa77a38b054256b677006ddafa5af67d84f34
Christine Walsh <christine.walsh@accenture.com>
2022-03-14 16:33:23 -0400

    create ingestions list table
    Datastillery/internal#595
    
    co-authored-by: Nicole Arruda <narruda789@gmail.com>

---
+++ apps/andi/lib/andi_web/live/ingestion_live_view/table.ex
@@ -0,0 +1,37 @@
+defmodule AndiWeb.IngestionLiveView.Table do
+  @moduledoc """
+  LiveComponent for ingestion table
+  """
+
+  use Phoenix.LiveComponent
+  alias Phoenix.HTML.Link
+
+  def render(assigns) do
+    ~L"""
+    <div id="<%= @id %>">
+      <table class="ingestions-table">
+        <thead>
+          <th class="ingestions-table__th ingestions-table__cell">Ingestion Name</th>
+          <th class="ingestions-table__th ingestions-table__cell">Dataset</th>
+          <th class="ingestions-table__th ingestions-table__cell">Action</th>
+        </thead>
+
+        <%= if @ingestions == [] do %>
+          <tr><td class="ingestions-table__cell" colspan="100%">No Ingestions Found!</td></tr>
+        <% else %>
+          <%= for ingestion <- @ingestions do %>
+
+            <tr class="ingestions-table__tr">
+              <td class="ingestions-table__cell ingestions-table__cell--break ingestions-table__data-title-cell"><%= ingestion["ingestion_name"] %></td>
+              <td class="ingestions-table__cell ingestions-table__cell--break"><%= ingestion["dataset_name"] %></td>
+              <td class="ingestions-table__cell ingestions-table__cell--break" style="width: 10%;"><%= Link.link("Edit", to: "/ingestions/#{ingestion["id"]}", class: "btn") %></td>
+            </tr>
+          <% end %>
+        <% end %>
+      </table>
+    </div>
+    """
+  end
+
+
+end
