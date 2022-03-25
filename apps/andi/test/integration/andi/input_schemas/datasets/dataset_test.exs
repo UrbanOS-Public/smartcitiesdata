706cb543437a5b6b7b191cd9325332a0914a8198
Christine Walsh <christine.walsh@accenture.com>
2022-03-25 13:44:35 -0400

    test fixes
    Datastillery/internal#570

--- apps/andi/test/integration/andi/input_schemas/datasets/dataset_test.exs
+++ apps/andi/test/integration/andi/input_schemas/datasets/dataset_test.exs
@@ -25,19 +25,20 @@ defmodule Andi.InputSchemas.Datasets.DatasetTest do
       {:ok, dataset} = Dataset.associate_with_access_group(access_group_id, dataset_id)
 
       eventually(fn ->
-        assert [%{id: access_group_id}] = Map.get(dataset, :access_groups)
+        assert [%{id: ^access_group_id}] = Map.get(dataset, :access_groups)
       end)
     end
 
     test "a dataset can be associated with multiple access groups", %{dataset_id: dataset_id, access_group_id: access_group_id} do
       access_group_2 = TDG.create_access_group(%{})
+      access_group_2_id = access_group_2.id
       {:ok, _andi_access_group} = AccessGroups.update(access_group_2)
 
       {:ok, dataset} = Dataset.associate_with_access_group(access_group_id, dataset_id)
       {:ok, dataset} = Dataset.associate_with_access_group(access_group_2.id, dataset_id)
 
       eventually(fn ->
-        assert [%{id: access_group_id}, %{id: access_group_2.id}] = Map.get(dataset, :access_groups)
+        assert [%{id: ^access_group_id}, %{id: ^access_group_2_id}] = Map.get(dataset, :access_groups)
       end)
     end
   end
@@ -48,22 +49,25 @@ defmodule Andi.InputSchemas.Datasets.DatasetTest do
       {:ok, _andi_dataset} = Datasets.update(dataset)
 
       access_group = TDG.create_access_group(%{})
+      access_group_id = access_group.id
       {:ok, _andi_access_group} = AccessGroups.update(access_group)
 
       %{dataset_id: dataset.id, access_group_id: access_group.id}
 
-      {:ok, dataset} = Dataset.associate_with_access_group(access_group_id, dataset_id)
+      {:ok, dataset} = Dataset.associate_with_access_group(access_group_id, dataset.id)
 
       eventually(fn ->
-        assert [%{id: access_group_id}] = Map.get(dataset, :access_groups)
+        assert [%{id: ^access_group_id}] = Map.get(dataset, :access_groups)
       end)
+
+      %{dataset_id: dataset.id, access_group_id: access_group.id}
     end
 
     test "disassociates a dataset from an access group", %{dataset_id: dataset_id, access_group_id: access_group_id} do
       {:ok, dataset} = Dataset.disassociate_with_access_group(access_group_id, dataset_id)
 
       eventually(fn ->
-        assert [] = Map.get(dataset, :access_groups)
+        assert [] == Map.get(dataset, :access_groups)
       end)
     end
   end
