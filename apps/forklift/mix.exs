491224fdaf969c157942d29ca887cb63ea9aa413
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-04-01 12:18:47 -0400

    Bumping version of apps which use smart_city

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -4,7 +4,7 @@ defmodule Forklift.MixProject do
   def project do
     [
       app: :forklift,
-      version: "0.17.17",
+      version: "0.17.18",
       elixir: "~> 1.10",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
--- apps/raptor/mix.exs
+++ apps/raptor/mix.exs
@@ -5,7 +5,7 @@ defmodule Raptor.MixProject do
     [
       app: :raptor,
       compilers: [:phoenix] ++ Mix.compilers(),
-      version: "1.1.3",
+      version: "1.1.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
