b3e53cb5cf7be4c66ce7188246c4f153621d6c1b
jennifer.folkerth <57450454+jfolk2015@users.noreply.github.com>
2022-04-11 17:33:43 -0400

    Estuary upgrade minimist (#1377)
    * Updating dependencies for security reasons
    
    * Bumping version

--- apps/estuary/assets/webpack.config.js
+++ apps/estuary/assets/webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
+      new TerserWebpackPlugin({ parallel: true }),
+      new CssMinimizerPlugin()
     ]
   },
   entry: {
@@ -50,6 +50,6 @@ module.exports = (env, options) => ({
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+    new CopyWebpackPlugin({patterns: [{ from: 'static/', to: '../' }]})
   ]
 });
--- apps/estuary/config/dev.exs
+++ apps/estuary/config/dev.exs
@@ -10,7 +10,8 @@ config :estuary, EstuaryWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
+      "--watch-options-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
   ],
