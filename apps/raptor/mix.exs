1040705594913c2c3c76e8f1d1b0bd3a36025ed4
Christine Walsh <christine.walsh@accenture.com>
2022-04-29 14:55:47 -0400

    update mix.exs
    Datastillery/internal#577
    
    co-authored-by: Nicole Arruda <narruda789@gmail.com>

--- apps/raptor/mix.exs
+++ apps/raptor/mix.exs
@@ -5,7 +5,7 @@ defmodule Raptor.MixProject do
     [
       app: :raptor,
       compilers: [:phoenix] ++ Mix.compilers(),
-      version: "1.2.0",
+      version: "1.2.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
