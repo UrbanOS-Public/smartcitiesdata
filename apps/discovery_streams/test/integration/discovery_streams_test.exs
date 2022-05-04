755e253076d6ab4497c9d777cfc012853df11eb8
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-05-04 16:29:09 -0500

    attempt to fix streams mock

--- apps/discovery_streams/test/integration/discovery_streams_test.exs
+++ apps/discovery_streams/test/integration/discovery_streams_test.exs
@@ -13,24 +13,10 @@ defmodule DiscoveryStreams.DiscoveryStreamsTest do
   @instance_name DiscoveryStreams.instance_name()
   @unauthorized_private_system_name "private__data"
 
-  setup_all do
-    bypass = Bypass.open()
+  setup do
+    allow(RaptorService.is_authorized(any(), any(), @unauthorized_private_system_name), return: false)
+    allow(RaptorService.is_authorized(any(), any(), any()), return: true)
 
-    Bypass.stub(bypass, "GET", "/api/authorize", fn conn ->
-      system_name =
-        conn
-        |> Plug.Conn.fetch_query_params()
-        |> Map.get(:query_params)
-        |> Map.get("systemName")
-
-      if system_name == @unauthorized_private_system_name do
-        Plug.Conn.resp(conn, 200, "{\"is_authorized\":false}")
-      else
-        Plug.Conn.resp(conn, 200, "{\"is_authorized\":true}")
-      end
-    end)
-
-    Application.put_env(:discovery_streams, :raptor_url, "http://localhost:#{bypass.port}/api/authorize")
     :ok
   end
 
