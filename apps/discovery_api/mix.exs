ac08267dba550112a4a778d8df0a4da76f0b2794
Christine (Walsh) Poydence <73911735+christinepoydence@users.noreply.github.com>
2022-05-06 12:37:19 -0400

    Update mix.exs (#1387)

--- apps/discovery_api/mix.exs
+++ apps/discovery_api/mix.exs
@@ -5,7 +5,7 @@ defmodule DiscoveryApi.Mixfile do
     [
       app: :discovery_api,
       compilers: [:phoenix, :gettext | Mix.compilers()],
-      version: "1.2.9",
+      version: "1.2.10",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
