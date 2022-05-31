ccbe673ea0288b16264962c152c149b61d500e35
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-05-31 11:31:21 -0600

    lock ex_json_schema for use in test validator

--- apps/discovery_api/mix.exs
+++ apps/discovery_api/mix.exs
@@ -51,7 +51,7 @@ defmodule DiscoveryApi.Mixfile do
       {:credo, "~> 1.0", only: [:dev, :test, :integration], runtime: false},
       {:checkov, "~> 1.0", only: [:test, :integration]},
       {:divo, "~> 1.3", only: [:dev, :test, :integration]},
-      {:ex_json_schema, "~> 0.7", only: [:test, :integration]},
+      {:ex_json_schema, "== 0.7.3", only: [:test, :integration]},
       {:ecto_sql, "~> 3.0"},
       {:elastix, "~> 0.8.0"},
       {:guardian, "~> 2.0"},
