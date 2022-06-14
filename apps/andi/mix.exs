954acc7604fbb8fc2f57d99c5bb655c15fc99b7c
Christine Walsh <christine.walsh@accenture.com>
2022-06-14 13:11:49 -0400

    Create Transformations Section on Ingestions Page
    Datastillery/internal#747
    
    co-authored-by: Benjamin Mitchinson <mitchinson.dev@gmail.com>

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "2.2.10",
+      version: "2.2.11",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
