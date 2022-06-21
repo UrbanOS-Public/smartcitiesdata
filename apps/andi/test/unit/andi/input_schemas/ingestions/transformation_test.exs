90d99bb755c21702e3b0acb88464c74c7d3bd73d
Christine Walsh <christine.walsh@accenture.com>
2022-06-21 17:39:00 -0400

    add additional unit tests
    Datastillery/internal#749
    
    co-authored-by: Jennifer Folkerth <jennifer.folkerth@accenture.com>

--- apps/andi/test/unit/andi/input_schemas/ingestions/transformation_test.exs
+++ apps/andi/test/unit/andi/input_schemas/ingestions/transformation_test.exs
@@ -60,4 +60,56 @@ defmodule Andi.InputSchemas.Ingestions.TransformationTest do
     assert changeset.errors[:type] != nil
     assert changeset.errors[:type] == {"invalid type: invalid", []}
   end
+
+  test "sucessfully converts a transformation to a changeset" do
+    id = UUID.uuid4()
+    ingestion_id = UUID.uuid4()
+    transformation = %Transformation{id: id, name: "turtle", ingestion_id: ingestion_id}
+
+    changeset = Transformation.convert_andi_transformation_to_changeset(transformation)
+
+    assert %Ecto.Changeset{changes: %{id: ^id, name: "turtle", ingestion_id: ^ingestion_id}} = changeset
+  end
+
+  test "sucessfully creates an invalid changeset from form data when there is no transformation type selected" do
+    id = UUID.uuid4()
+
+    form_data = %{
+      name: "Transformation Name",
+      id: id
+    }
+
+    changeset = Transformation.changeset_from_form_data(form_data)
+
+    assert %Ecto.Changeset{changes: %{id: ^id, name: "Transformation Name", parameters: %{}}} = changeset
+    refute changeset.valid?
+    assert changeset.errors == [type: {"is required", [validation: :required]}]
+  end
+
+  test "sucessfully creates a valid changeset from form data when there is a transformation type selected" do
+    id = UUID.uuid4()
+
+    form_data = %{
+      name: "Transformation Name",
+      id: id,
+      type: "concatenation",
+      sourceFields: ["other", "name"],
+      separator: ".",
+      targetField: "name"
+    }
+
+    changeset = Transformation.changeset_from_form_data(form_data) |> IO.inspect(label: "wat")
+
+    assert %Ecto.Changeset{
+             changes: %{
+               id: ^id,
+               name: "Transformation Name",
+               type: "concatenation",
+               parameters: %{sourceFields: ["other", "name"], separator: ".", targetField: "name"}
+             }
+           } = changeset
+
+    assert changeset.valid?
+    assert changeset.errors == []
+  end
 end
