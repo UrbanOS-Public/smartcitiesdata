d4226ee2ccf24daadb1ebce6d674c35c65e8de1f
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-06-28 11:08:52 -0600

    up smart city + test to 5.2.4

--- apps/pipeline/mix.exs
+++ apps/pipeline/mix.exs
@@ -26,7 +26,7 @@ defmodule Pipeline.MixProject do
 
   defp deps do
     [
-      {:smart_city, "~> 5.2.2"},
+      {:smart_city, "~> 5.2.4"},
       {:elsa, "~> 0.12", override: true},
       {:ex_aws, "~> 2.1"},
       {:ex_aws_s3, "~> 2.0",
@@ -44,7 +44,7 @@ defmodule Pipeline.MixProject do
       {:temp, "~> 0.4"},
       {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false},
       {:placebo, "~> 2.0.0-rc2", only: [:dev, :test, :integration]},
-      {:smart_city_test, "~> 2.2.2", only: [:test, :integration]},
+      {:smart_city_test, "~> 2.2.4", only: [:test, :integration]},
       {:divo, "~> 1.3", only: [:dev, :integration]},
       {:divo_kafka, "~> 0.1.5", only: [:dev, :integration]},
       {:telemetry_event, in_umbrella: true}
