c8b312d6c0576e35cef87cd0ac9464c1bf408d32
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-07-16 12:38:59 -0400

    Integration tests for move buttons

--- apps/andi/test/integration/andi_web/live/ingestion_live_view/transformations/move_buttons_test.exs
+++ apps/andi/test/integration/andi_web/live/ingestion_live_view/transformations/move_buttons_test.exs
@@ -36,7 +36,6 @@ defmodule AndiWeb.IngestionLiveView.Transformations.MoveButtonsTest do
     assert Floki.find(document, ".move-down") |> Enum.count() == 3
   end
 
-  @tag :skip
   test "clicking move up slides that transformation up one row", %{view: view, html: html, ingestion: ingestion} do
     {:ok, starting_document} = Floki.parse_document(html)
     assert ["Black", "Blue", "Green"] == starting_document
@@ -49,12 +48,28 @@ defmodule AndiWeb.IngestionLiveView.Transformations.MoveButtonsTest do
       |> element(".move-up-#{second_transformation.id}")
       |> render_click()
 
-    html = render(view)
-
-    {:ok, changed_document} = Floki.parse_document(html)
-    changed_order_names = Floki.find(changed_document, ".transformation-header-name")
+    {:ok, changed_document} = render(view) |> Floki.parse_document()
+    changed_order_names = Floki.find(changed_document, ".transformation-name")
       |> Floki.attribute("value")
-    assert ["Black", "Blue", "Green"] == changed_order_names
+    assert ["Blue", "Black", "Green"] == changed_order_names
+  end
+
+  test "clicking move down slides that transformation down one row", %{view: view, html: html, ingestion: ingestion} do
+    {:ok, starting_document} = Floki.parse_document(html)
+    assert ["Black", "Blue", "Green"] == starting_document
+      |> Floki.find(".transformation-name")
+      |> Floki.attribute("value")
+    [_ | [second_transformation | _]] = Transformations.all_for_ingestion(ingestion.id)
+
+    find_live_child(view, "transformations_form_editor")
+      |> find_live_child("transform-#{second_transformation.id}")
+      |> element(".move-down-#{second_transformation.id}")
+      |> render_click()
+
+    {:ok, changed_document} = render(view) |> Floki.parse_document()
+    changed_order_names = Floki.find(changed_document, ".transformation-name")
+      |> Floki.attribute("value")
+    assert ["Black", "Green", "Blue"] == changed_order_names
   end
 
   defp create_transformation_with_name(name, ingestion) do
