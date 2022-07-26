75dc1509f7e1315211d9fdf4a1352f02810888b6
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-26 14:52:20 -0500

    780 animate new transformation (#1426)
    * started animation
    
    * new transformation
    
    * added reduced motion
    
    * fixed missing animation unit
    
    * changed mix to 20
    
    Co-authored-by: Isabel Lopez <lisabel@umich.edu>

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "2.2.19",
+      version: "2.2.20",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
