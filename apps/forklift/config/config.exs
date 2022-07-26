116cde0c3873d9e732bfaddf2c5c76ac9425e8cc
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-26 09:11:05 -0600

    ensure overwrite_mode is false by default in all contexts

--- apps/forklift/config/config.exs
+++ apps/forklift/config/config.exs
@@ -12,7 +12,8 @@ config :forklift,
   cache_processing_batch_size: 1_000,
   message_processing_cadence: 10_000,
   number_of_empty_reads_to_delete: 50,
-  input_topic_prefix: input_topic_prefix
+  input_topic_prefix: input_topic_prefix,
+  overwrite_mode: false
 
 config :logger,
   backends: [:console],
