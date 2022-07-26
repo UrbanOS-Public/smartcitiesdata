90962eefeaeace30f20c3b897e91bf87d37da23d
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-07-26 16:56:56 -0400

    Formatting

--- apps/andi/test/integration/andi_web/live/ingestion_live_view/transformations/move_buttons_test.exs
+++ apps/andi/test/integration/andi_web/live/ingestion_live_view/transformations/move_buttons_test.exs
@@ -81,10 +81,10 @@ defmodule AndiWeb.IngestionLiveView.Transformations.MoveButtonsTest do
 
     save(view)
 
-    eventually fn ->
+    eventually(fn ->
       {:ok, _, refreshed_html} = navigate_to_edit_page(conn, ingestion)
       assert ["Blue", "Black", "Green"] == find_ordered_names(refreshed_html)
-    end
+    end)
   end
 
   test "cancelling with unsaved move changes prompts confirmation", %{conn: conn, view: view, html: html, ingestion: ingestion} do
