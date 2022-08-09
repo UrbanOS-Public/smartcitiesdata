ace6f10766afc1ece9987d35b0459258d5e5e242
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-08-09 18:04:17 -0400

    New validation function for type conversion transformation

--- apps/transformers/lib/transformations/type_conversion.ex
+++ apps/transformers/lib/transformations/type_conversion.ex
@@ -27,6 +27,71 @@ defmodule Transformers.TypeConversion do
     end
   end
 
+  def validate_new(parameters) do
+    required_fields()
+      |> validate_fields(parameters)
+      |> validate_conversion(parameters)
+      |> ordered_values_or_errors()
+  end
+
+  defp validate_conversion(accumulator, parameters) do
+    prior_error_source_type = get_in(accumulator, [:errors, "sourceType"])
+    prior_error_target_type = get_in(accumulator, [:errors, "targetType"])
+
+    if prior_error_source_type != nil || prior_error_target_type != nil do
+      accumulator
+    else
+      with {:ok, source_type} <- FieldFetcher.fetch_parameter(parameters, "sourceType"),
+           {:ok, target_type} <- FieldFetcher.fetch_parameter(parameters, "targetType"),
+           {:ok, conversion_function} <- pick_conversion(source_type, target_type) do
+        Map.update(accumulator, :values, %{}, fn values -> Map.put(values, "conversionFunction", conversion_function) end)
+      else
+        {:error, reason} -> Map.update(accumulator, :errors, %{}, fn errors -> Map.put(errors, "sourceType", reason) end)
+          |> Map.update(:errors, %{}, fn errors -> Map.put(errors, "targetType", reason) end)
+      end
+    end
+  end
+
+  defp required_fields() do
+    ["field", "sourceType", "targetType"]
+  end
+
+  defp validate_fields(required_fields, parameters) do
+    Enum.reduce(required_fields, %{values: %{}, errors: %{}}, fn required_field, accumulator ->
+      update_with_value_or_error(required_field, accumulator, parameters)
+    end)
+  end
+
+  defp update_with_value_or_error(required_field, accumulator, parameters) do
+    result = FieldFetcher.fetch_value_or_error(parameters, required_field)
+    case result do
+      {:ok, value} -> update_with_value(accumulator, required_field, value)
+      {:error, reason} -> update_with_error(accumulator, reason)
+    end
+  end
+
+  defp update_with_value(accumulator, field, value) do
+    Map.update(accumulator, :values, %{}, fn values -> Map.put(values, field, value) end)
+  end
+
+  defp update_with_error(accumulator, error_reason) do
+    Map.update(accumulator, :errors, %{}, fn errors -> Map.merge(errors, error_reason) end)
+  end
+
+  defp ordered_values_or_errors(accumulated) do
+    errors = Map.get(accumulated, :errors)
+    if length(Map.keys(errors)) > 0 do
+      {:error, errors}
+    else
+      values = Map.get(accumulated, :values)
+      field = Map.get(values, "field")
+      sourceType = Map.get(values, "sourceType")
+      targetType = Map.get(values, "targetType")
+      conversion_function = Map.get(values, "conversionFunction")
+      {:ok, [field, sourceType, targetType, conversion_function]}
+    end
+  end
+
   defp pick_conversion(source_type, target_type) do
     case {source_type, target_type} do
       {"float", "integer"} -> {:ok, fn value -> Float.round(value) end}
