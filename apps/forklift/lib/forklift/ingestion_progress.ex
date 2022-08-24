84303b967fc129a06319f37032a1b05f46d22d39
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-08-24 15:21:30 -0600

    Green: IngestionProgress fully tested

--- apps/forklift/lib/forklift/ingestion_progress.ex
+++ apps/forklift/lib/forklift/ingestion_progress.ex
@@ -1,9 +1,22 @@
 defmodule Forklift.IngestionProgress do
   @spec new_message(String.t()) :: :in_progress | :ingestion_complete
   def new_message(ingestion_id) do
-    msgs_received = increment_ingestion_count(ingestion_id)
+    increment_ingestion_count(ingestion_id)
 
-    case(is_ingestion_done(msgs_received, ingestion_id)) do
+    case(is_ingestion_done(ingestion_id)) do
+      false ->
+        :in_progress
+
+      true ->
+        complete_ingestion(ingestion_id)
+    end
+  end
+
+  @spec store_target(String.t(), Integer.t()) :: :in_progress | :ingestion_complete
+  def store_target(ingestion_id, target) do
+    set_ingestion_target(ingestion_id, target)
+
+    case(is_ingestion_done(ingestion_id)) do
       false ->
         :in_progress
 
@@ -17,14 +30,22 @@ defmodule Forklift.IngestionProgress do
     Redix.command!(:redix, ["INCR", get_count_key(ingestion_id)])
   end
 
-  @spec is_ingestion_done(Integer.t(), String.t()) :: boolean()
-  defp is_ingestion_done(msgs_received, ingestion_id) do
-    case Redix.command!(:redix, ["GET", get_target_key(ingestion_id)]) do
+  @spec set_ingestion_target(String.t(), Integer.t()) :: integer()
+  defp set_ingestion_target(ingestion_id, target) do
+    Redix.command!(:redix, ["SET", get_target_key(ingestion_id), target])
+  end
+
+  @spec is_ingestion_done(String.t()) :: boolean()
+  defp is_ingestion_done(ingestion_id) do
+    target = Redix.command!(:redix, ["GET", get_target_key(ingestion_id)])
+    current = Redix.command!(:redix, ["GET", get_count_key(ingestion_id)])
+
+    case target && current do
       nil ->
         false
 
-      target ->
-        msgs_received >= String.to_integer(target)
+      _ ->
+        String.to_integer(current) >= String.to_integer(target)
     end
   end
 
