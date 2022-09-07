e959bed1e6aefb0a64099e49e4ac1c240618cb03
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-09-06 20:28:31 -0600

    Make ingestion_progress incorporate extraction_time

--- apps/forklift/lib/forklift/ingestion_progress.ex
+++ apps/forklift/lib/forklift/ingestion_progress.ex
@@ -1,44 +1,48 @@
 defmodule Forklift.IngestionProgress do
-  @spec new_message(String.t()) :: :in_progress | :ingestion_complete
-  def new_message(ingestion_id) do
-    increment_ingestion_count(ingestion_id)
+  @spec new_message(String.t(), Integer.t()) :: :in_progress | :ingestion_complete
+  def new_message(ingestion_id, extract_time) do
+    extract_id = get_extract_id(ingestion_id, extract_time)
 
-    case(is_ingestion_done(ingestion_id)) do
+    increment_ingestion_count(extract_id)
+
+    case(is_extract_done(extract_id)) do
       false ->
         :in_progress
 
       true ->
-        complete_ingestion(ingestion_id)
+        complete_extract(extract_id)
     end
   end
 
-  @spec store_target(String.t(), Integer.t()) :: :in_progress | :ingestion_complete
-  def store_target(ingestion_id, target) do
-    set_ingestion_target(ingestion_id, target)
+  @spec store_target(String.t(), Integer.t(), Integer.t()) :: :in_progress | :ingestion_complete
+  def store_target(ingestion_id, extract_time, target) do
+    extract_id = get_extract_id(ingestion_id, extract_time)
 
-    case(is_ingestion_done(ingestion_id)) do
+    set_extract_target(extract_id, target)
+
+    case(is_extract_done(extract_id)) do
       false ->
         :in_progress
 
       true ->
-        complete_ingestion(ingestion_id)
+        complete_extract(extract_id)
     end
   end
 
   @spec increment_ingestion_count(String.t()) :: integer()
-  defp increment_ingestion_count(ingestion_id) do
-    Redix.command!(:redix, ["INCR", get_count_key(ingestion_id)])
+  defp increment_ingestion_count(extract_id) do
+    Redix.command!(:redix, ["INCR", get_count_key(extract_id)])
   end
 
-  @spec set_ingestion_target(String.t(), Integer.t()) :: integer()
-  defp set_ingestion_target(ingestion_id, target) do
-    Redix.command!(:redix, ["SET", get_target_key(ingestion_id), target])
+  @spec set_extract_target(String.t(), Integer.t()) :: integer()
+  defp set_extract_target(extract_id, target) do
+    Redix.command!(:redix, ["SET", get_target_key(extract_id), target])
   end
 
-  @spec is_ingestion_done(String.t()) :: boolean()
-  defp is_ingestion_done(ingestion_id) do
-    target = Redix.command!(:redix, ["GET", get_target_key(ingestion_id)])
-    current = Redix.command!(:redix, ["GET", get_count_key(ingestion_id)])
+  @spec is_extract_done(String.t()) :: boolean()
+  defp is_extract_done(extract_id) do
+    target = Redix.command!(:redix, ["GET", get_target_key(extract_id)])
+    current = Redix.command!(:redix, ["GET", get_count_key(extract_id)])
 
     case target && current do
       nil ->
@@ -49,17 +53,22 @@ defmodule Forklift.IngestionProgress do
     end
   end
 
-  defp get_count_key(ingestion_id) do
-    ingestion_id <> "_count"
+  defp get_count_key(extract_id) do
+    extract_id <> "_count"
   end
 
-  defp get_target_key(ingestion_id) do
-    ingestion_id <> "_target"
+  defp get_target_key(extract_id) do
+    extract_id <> "_target"
   end
 
-  defp complete_ingestion(ingestion_id) do
-    Redix.command!(:redix, ["GETDEL", get_count_key(ingestion_id)])
-    Redix.command!(:redix, ["GETDEL", get_target_key(ingestion_id)])
+  defp complete_extract(extract_id) do
+    Redix.command!(:redix, ["GETDEL", get_count_key(extract_id)])
+    Redix.command!(:redix, ["GETDEL", get_target_key(extract_id)])
     :ingestion_complete
+  end
+
+  @spec get_extract_id(String.t(), Integer.t()) :: String.t()
+  defp get_extract_id(ingestion_id, extract_time) do
+    ingestion_id <> "_" <> (extract_time |> Integer.to_string())
   end
 end
--- apps/forklift/test/integration/forklift/ingestion_progress_test.exs
+++ apps/forklift/test/integration/forklift/ingestion_progress_test.exs
@@ -10,72 +10,92 @@ defmodule Forklift.IngestionProgressTest do
 
   setup do
     {:ok, _} = Redix.command(:redix, ["flushall"])
-    [ingestion_id: Faker.UUID.v4()]
+    [ingestion_id: Faker.UUID.v4(), extract_time: Timex.now() |> Timex.to_unix()]
   end
 
   describe "IngestionTest" do
-    test "new_message updates the message count when called", %{ingestion_id: ingestion_id} do
-      IngestionProgress.new_message(ingestion_id)
-      resulting_count = Redix.command!(:redix, ["GET", ingestion_id <> "_count"])
+    test "new_message updates the message count when called", %{ingestion_id: ingestion_id, extract_time: extract_time} do
+      IngestionProgress.new_message(ingestion_id, extract_time)
+      resulting_count = Redix.command!(:redix, ["GET", get_extract_id(ingestion_id, extract_time) <> "_count"])
       assert resulting_count == "1"
     end
 
     test "new_message returns :in_progress if message count *has not* met existing ingestion target", %{
-      ingestion_id: ingestion_id
+      ingestion_id: ingestion_id,
+      extract_time: extract_time
     } do
-      Redix.command!(:redix, ["SET", ingestion_id <> "_target", 2])
-      result = IngestionProgress.new_message(ingestion_id)
+      Redix.command!(:redix, ["SET", get_extract_id(ingestion_id, extract_time) <> "_target", 2])
+      result = IngestionProgress.new_message(ingestion_id, extract_time)
       assert result == :in_progress
     end
 
     test "new_message returns :in_progress if message count ingestion target does not exist", %{
-      ingestion_id: ingestion_id
+      ingestion_id: ingestion_id,
+      extract_time: extract_time
     } do
-      result = IngestionProgress.new_message(ingestion_id)
+      result = IngestionProgress.new_message(ingestion_id, extract_time)
       assert result == :in_progress
     end
 
     test "new_message returns :ingestion_complete if message count *has* met ingestion target", %{
-      ingestion_id: ingestion_id
+      ingestion_id: ingestion_id,
+      extract_time: extract_time
     } do
-      Redix.command!(:redix, ["SET", ingestion_id <> "_target", 1])
-      result = IngestionProgress.new_message(ingestion_id)
+      Redix.command!(:redix, ["SET", get_extract_id(ingestion_id, extract_time) <> "_target", 1])
+      result = IngestionProgress.new_message(ingestion_id, extract_time)
       assert result == :ingestion_complete
     end
 
     test "new_message resets _count and _target if message count *has* met ingestion target", %{
-      ingestion_id: ingestion_id
+      ingestion_id: ingestion_id,
+      extract_time: extract_time
     } do
-      Redix.command!(:redix, ["SET", ingestion_id <> "_target", 1])
-      IngestionProgress.new_message(ingestion_id)
-      assert Redix.command!(:redix, ["GET", ingestion_id <> "_target"]) == nil
-      assert Redix.command!(:redix, ["GET", ingestion_id <> "_count"]) == nil
+      Redix.command!(:redix, ["SET", get_extract_id(ingestion_id, extract_time) <> "_target", 1])
+      IngestionProgress.new_message(ingestion_id, extract_time)
+      assert Redix.command!(:redix, ["GET", get_extract_id(ingestion_id, extract_time) <> "_target"]) == nil
+      assert Redix.command!(:redix, ["GET", get_extract_id(ingestion_id, extract_time) <> "_count"]) == nil
     end
 
-    test "store_target stores target value in redis", %{ingestion_id: ingestion_id} do
-      IngestionProgress.store_target(ingestion_id, 7)
-      assert Redix.command!(:redix, ["GET", ingestion_id <> "_target"]) == "7"
+    test "store_target stores target value in redis", %{ingestion_id: ingestion_id, extract_time: extract_time} do
+      IngestionProgress.store_target(ingestion_id, extract_time, 7)
+      assert Redix.command!(:redix, ["GET", get_extract_id(ingestion_id, extract_time) <> "_target"]) == "7"
     end
 
-    test "store_target returns :in_progress if count doesn't exist", %{ingestion_id: ingestion_id} do
-      assert IngestionProgress.store_target(ingestion_id, 7) == :in_progress
+    test "store_target returns :in_progress if count doesn't exist", %{
+      ingestion_id: ingestion_id,
+      extract_time: extract_time
+    } do
+      assert IngestionProgress.store_target(ingestion_id, extract_time, 7) == :in_progress
     end
 
-    test "store_target returns :in_progress if count is *less than* new target", %{ingestion_id: ingestion_id} do
-      Redix.command!(:redix, ["SET", ingestion_id <> "_count", 6])
-      assert IngestionProgress.store_target(ingestion_id, 7) == :in_progress
+    test "store_target returns :in_progress if count is *less than* new target", %{
+      ingestion_id: ingestion_id,
+      extract_time: extract_time
+    } do
+      Redix.command!(:redix, ["SET", get_extract_id(ingestion_id, extract_time) <> "_count", 6])
+      assert IngestionProgress.store_target(ingestion_id, extract_time, 7) == :in_progress
     end
 
-    test "store_target returns :ingestion_complete if count meets new target", %{ingestion_id: ingestion_id} do
-      Redix.command!(:redix, ["SET", ingestion_id <> "_count", 3])
-      assert IngestionProgress.store_target(ingestion_id, 3) == :ingestion_complete
+    test "store_target returns :ingestion_complete if count meets new target", %{
+      ingestion_id: ingestion_id,
+      extract_time: extract_time
+    } do
+      Redix.command!(:redix, ["SET", get_extract_id(ingestion_id, extract_time) <> "_count", 3])
+      assert IngestionProgress.store_target(ingestion_id, extract_time, 3) == :ingestion_complete
     end
 
-    test "ingestion count and target are cleared when target is achieved", %{ingestion_id: ingestion_id} do
-      Redix.command!(:redix, ["SET", ingestion_id <> "_count", 3])
-      IngestionProgress.store_target(ingestion_id, 3)
-      assert Redix.command!(:redix, ["GET", ingestion_id <> "_target"]) == nil
-      assert Redix.command!(:redix, ["GET", ingestion_id <> "_count"]) == nil
+    test "ingestion count and target are cleared when target is achieved", %{
+      ingestion_id: ingestion_id,
+      extract_time: extract_time
+    } do
+      Redix.command!(:redix, ["SET", get_extract_id(ingestion_id, extract_time) <> "_count", 3])
+      IngestionProgress.store_target(ingestion_id, extract_time, 3)
+      assert Redix.command!(:redix, ["GET", get_extract_id(ingestion_id, extract_time) <> "_target"]) == nil
+      assert Redix.command!(:redix, ["GET", get_extract_id(ingestion_id, extract_time) <> "_count"]) == nil
+    end
+
+    defp get_extract_id(ingestion_id, extract_time) do
+      ingestion_id <> "_" <> (extract_time |> Integer.to_string())
     end
   end
 end
