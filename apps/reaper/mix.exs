b5b338aab1192d16351bad587aebf9954cd76ad7
Chris Duncan <c.m.duncan@accenture.com>
2022-09-09 11:28:12 -0500

    feat(830): update smart_cities version to 5.2.6

--- apps/reaper/mix.exs
+++ apps/reaper/mix.exs
@@ -72,7 +72,7 @@ defmodule Reaper.MixProject do
       {:redix, "~> 0.10"},
       {:retry, "~> 0.13"},
       {:sftp_ex, "~> 0.2"},
-      {:smart_city, "~> 5.2.5"},
+      {:smart_city, "~> 5.2.6"},
       {:saxy, "~> 0.10"},
       {:sweet_xml, "~> 0.6"},
       {:telemetry_event, in_umbrella: true},
