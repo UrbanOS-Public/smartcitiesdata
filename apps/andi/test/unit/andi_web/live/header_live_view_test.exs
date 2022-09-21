8d094da7f80050625d29f9152ce7b2e50f2ce8b6
Ian Abbott <ij.abbott2306@gmail.com>
2022-09-20 20:06:53 -0500

    add tests for configurable logo

--- apps/andi/test/unit/andi_web/live/header_live_view_test.exs
+++ apps/andi/test/unit/andi_web/live/header_live_view_test.exs
@@ -7,6 +7,28 @@ defmodule AndiWeb.HeaderLiveViewTest do
   import Phoenix.LiveViewTest
 
   describe "Header Live View" do
+    test "Displays default logo when none is provided" do
+      html = render_component(AndiWeb.HeaderLiveView, is_curator: true, path: "test")
+
+      header_logo = Floki.parse_fragment!(html)
+      |> Floki.attribute("img", "src")
+      |> List.first()
+
+      assert header_logo == "/images/UrbanOS.svg"
+    end
+
+    test "Displays provided logo" do
+      allow(Application.get_env(:andi, :logo_url), return: "/images/RuralOS.svg")
+
+      html = render_component(AndiWeb.HeaderLiveView, is_curator: true, path: "test")
+
+      header_logo = Floki.parse_fragment!(html)
+      |> Floki.attribute("img", "src")
+      |> List.first()
+
+      assert header_logo == "/images/RuralOS.svg"
+    end
+
     test "Displays default header text when none is provided" do
       html = render_component(AndiWeb.HeaderLiveView, is_curator: true, path: "test")
 
