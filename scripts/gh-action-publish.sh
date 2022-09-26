82beb2a458f6bcb75fa2e7a18ab93f3c80990364
Ryan Conaway <ryan.r.conaway@accenture.com>
2022-09-26 19:46:38 -0400

    Changed publishing strategy to push image matching elixir version and tag as development

--- scripts/gh-action-publish.sh
+++ scripts/gh-action-publish.sh
@@ -3,6 +3,21 @@
 set -e
 
 app="${1}"
+echo "app:"
+printf "%s \n\n" "$app"
 
-./scripts/build.sh $app development false
-./scripts/publish.sh $app development
+tag_as_development="${2:-true}"
+echo "tag_as_development:"
+printf "%s \n\n" "$tag_as_development"
+
+mix_vsn=$(mix cmd --app $app mix app.version | tail -1)
+echo "mix_vsn:"
+printf "%s \n\n" "$mix_vsn"
+
+./scripts/build.sh $app $mix_vsn false
+./scripts/publish.sh $app $mix_vsn
+
+if [[ $tag_as_development == true ]]; then
+  docker tag smartcitiesdata/$app:$mix_vsn smartcitiesdata/$app:deployment
+  ./scripts/publish.sh $app development
+fi
