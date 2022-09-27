75085bf28f17ce78f62d5f78a249e5be240b7e7e
nicholas-goodwin <111888703+nicholas-goodwin@users.noreply.github.com>
2022-09-27 13:57:26 -0500

    839 andi footer configurable from chart values (#1485)
    * added footer and config to populate footer
    
    * added untracked files
    
    * version bump
    
    * formatting
    
    * made configuration more generic
    
    * whoops formatting

--- apps/andi/config/test.exs
+++ apps/andi/config/test.exs
@@ -38,9 +38,9 @@ System.put_env("AWS_ACCESS_KEY_ID", "minioadmin")
 System.put_env("AWS_ACCESS_KEY_SECRET", "minioadmin")
 System.put_env("ANDI_LOGO_URL", "/images/UrbanOS.svg")
 System.put_env("ANDI_HEADER_TEXT", "Data Submission Tool")
-System.put_env("ANDI_FOOTER_LEFT_SIDE_TEXT", "Copyright 2022 State of Michigan")
+System.put_env("ANDI_FOOTER_LEFT_SIDE_TEXT", "Some Left Side Text")
 
 System.put_env(
   "footer_links",
-  "[{\"linkText\":\"ANDI\", \"url\":\"https://127.0.0.1.nip.io:4443\"}, {\"linkText\":\"DiscoveryUI\", \"url\":\"https://discovery.urbanos-demo.com/dataset\"}, {\"linkText\":\"Policies\", \"url\":\"https://www.michigan.gov/som/footer/policies\"}]"
+  "[{\"linkText\":\"Example 1\", \"url\":\"https://www.example.com\"}, {\"linkText\":\"Example 2\", \"url\":\"https://www.google.com\"}]"
 )
