0838149fa9f83b441abe2e081c67cb35c55ed31b
nicholas.goodwin <nicholas.goodwin@accenture.com>
2022-09-27 09:47:23 -0500

    formatting

--- apps/andi/test/unit/andi_web/live/footer_live_view_test.exs
+++ apps/andi/test/unit/andi_web/live/footer_live_view_test.exs
@@ -6,9 +6,9 @@ defmodule AndiWeb.FooterLiveViewTest do
     test "Should get links" do
       links = AndiWeb.FooterLiveView.get_links()
 
-      assert %{ "linkText" => "ANDI", "url" => "https://127.0.0.1.nip.io:4443" } == Enum.at(links, 0)
-      assert %{ "linkText" => "DiscoveryUI", "url" => "https://discovery.urbanos-demo.com/dataset" } == Enum.at(links, 1)
-      assert %{ "linkText" => "Policies", "url" => "https://www.michigan.gov/som/footer/policies" } == Enum.at(links, 2)
+      assert %{"linkText" => "ANDI", "url" => "https://127.0.0.1.nip.io:4443"} == Enum.at(links, 0)
+      assert %{"linkText" => "DiscoveryUI", "url" => "https://discovery.urbanos-demo.com/dataset"} == Enum.at(links, 1)
+      assert %{"linkText" => "Policies", "url" => "https://www.michigan.gov/som/footer/policies"} == Enum.at(links, 2)
     end
 
     test "Should get left side text" do
