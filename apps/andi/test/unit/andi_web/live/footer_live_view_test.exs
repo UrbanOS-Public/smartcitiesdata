e3764c6251b2efd022e36193675c9bccf1a4caef
nicholas.goodwin <nicholas.goodwin@accenture.com>
2022-09-27 12:57:06 -0500

    made configuration more generic

--- apps/andi/test/unit/andi_web/live/footer_live_view_test.exs
+++ apps/andi/test/unit/andi_web/live/footer_live_view_test.exs
@@ -6,15 +6,14 @@ defmodule AndiWeb.FooterLiveViewTest do
     test "Should get links" do
       links = AndiWeb.FooterLiveView.get_links()
 
-      assert %{"linkText" => "ANDI", "url" => "https://127.0.0.1.nip.io:4443"} == Enum.at(links, 0)
-      assert %{"linkText" => "DiscoveryUI", "url" => "https://discovery.urbanos-demo.com/dataset"} == Enum.at(links, 1)
-      assert %{"linkText" => "Policies", "url" => "https://www.michigan.gov/som/footer/policies"} == Enum.at(links, 2)
+      assert %{"linkText" => "Example 1", "url" => "https://www.example.com"} == Enum.at(links, 0)
+      assert %{"linkText" => "Example 2", "url" => "https://www.google.com"} == Enum.at(links, 1)
     end
 
     test "Should get left side text" do
       leftSideText = AndiWeb.FooterLiveView.get_left_side_text()
 
-      assert "Copyright 2022 State of Michigan" == leftSideText
+      assert "Some Left Side Text" == leftSideText
     end
   end
 end
