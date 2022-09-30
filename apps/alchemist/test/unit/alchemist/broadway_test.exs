7a3f7d5ad0629e3655075e9022e58e3a6a0a525f
nicholas.goodwin <nicholas.goodwin@accenture.com>
2022-09-30 10:59:32 -0500

    proformance enhancement on alchemist handle message

--- apps/alchemist/test/unit/alchemist/broadway_test.exs
+++ apps/alchemist/test/unit/alchemist/broadway_test.exs
@@ -45,8 +45,6 @@ defmodule Alchemist.BroadwayTest do
           transformations: [transform1, transform2]
         })
 
-      allow Brook.get!(any(), :ingestions, @ingestion_id), return: ingestion
-
       {:ok, broadway} =
         Alchemist.Broadway.start_link(
           # ingestion is destructured in handle message as the th~ird argument
@@ -200,8 +198,6 @@ defmodule Alchemist.BroadwayTest do
           targetDataset: @dataset_id,
           transformations: [transform]
         })
-
-      allow Brook.get!(any(), :ingestions, @ingestion_id), return: ingestion
 
       {:ok, broadway} =
         Alchemist.Broadway.start_link(
