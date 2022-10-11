5322218ea3a7bf69d9c06b6256e5f2d2a2d44149
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-10-11 16:09:54 -0400

    WIP - trying to self host font

--- apps/andi/assets/webpack.config.js
+++ apps/andi/assets/webpack.config.js
@@ -35,13 +35,15 @@ module.exports = (env, options) => ({
         ]
       },
       {
-        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'url-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: '../fonts/'
+              outputPath: '../fonts/',
+              mimetype: 'font/woff2',
+              encoding: false
             }
           }
         ]
