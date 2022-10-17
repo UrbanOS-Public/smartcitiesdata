f7fe978553d44b2645221c21095669985e361d99
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2022-10-17 15:10:50 -0400

    Webpacking additional types of font files

--- apps/andi/assets/webpack.config.js
+++ apps/andi/assets/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = (env, options) => ({
         ]
       },
       {
-        test: /\.(woff|svg)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
         use: [
           {
             loader: 'file-loader',
