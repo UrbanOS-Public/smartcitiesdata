e92015db54b62b61180f25c2d04b5592000d42ce
nicholas.goodwin <nicholas.goodwin@accenture.com>
2022-10-28 12:39:35 -0500

    formatting

--- apps/forklift/lib/forklift/data_writer.ex
+++ apps/forklift/lib/forklift/data_writer.ex
@@ -143,8 +143,7 @@ defmodule Forklift.DataWriter do
            ),
          write_end <- Data.Timing.current_time(),
          write_timing <-
-           Data.Timing.new(@instance_name, "presto_insert_time", write_start, write_end)
-    do
+           Data.Timing.new(@instance_name, "presto_insert_time", write_start, write_end) do
       ingestion_status = IngestionProgress.new_messages(Enum.count(data), ingestion_id, extraction_start_time)
 
       if ingestion_status == :ingestion_complete do
@@ -152,7 +151,6 @@ defmodule Forklift.DataWriter do
       end
 
       {:ok, write_timing}
-
     else
       error ->
         Logger.error(inspect(error))
