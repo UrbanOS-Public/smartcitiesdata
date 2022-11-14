30e55d017f2eccb213f3b5b29b84b24f266ad367
Ryan Conaway <ryan.r.conaway@accenture.com>
2022-11-14 17:09:14 -0500

    Adjusting sleeps

--- apps/andi/lib/andi_web/live/ingestion_live_view/edit_ingestion_live_view.ex
+++ apps/andi/lib/andi_web/live/ingestion_live_view/edit_ingestion_live_view.ex
@@ -157,9 +157,6 @@ defmodule AndiWeb.IngestionLiveView.EditIngestionLiveView do
   def handle_event("save", _, socket) do
     ingestion_id = socket.assigns.ingestion.id
     AndiWeb.Endpoint.broadcast_from(self(), "form-save", "save-all", %{ingestion_id: ingestion_id})
-    # Todo: Rearchitect how concurrent events are handled and remove these sleeps from draft-save and publish of datasets and ingestions
-    # This sleep is needed because other save events are executing. publish_ingestion will load the ingestion from the database.
-    Process.sleep(1_000)
 
     andi_ingestion = Ingestions.get(ingestion_id)
     ingestion_changeset = InputConverter.andi_ingestion_to_full_ui_changeset(andi_ingestion)
