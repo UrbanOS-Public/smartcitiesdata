6050f51fe5a4601f29f10e029d6367f041de6bf0
nicholas.goodwin <nicholas.goodwin@accenture.com>
2022-12-09 14:01:38 -0600

    removed navigation buttons

--- apps/andi/lib/andi_web/live/edit_live_view/data_dictionary_form.ex
+++ apps/andi/lib/andi_web/live/edit_live_view/data_dictionary_form.ex
@@ -131,11 +131,6 @@ defmodule AndiWeb.EditLiveView.DataDictionaryForm do
                 <% end %>
               </div>
             </div>
-
-            <div class="edit-button-group form-grid">
-              <a href="#metadata-form" id="back-button" class="btn btn--secondary btn--back btn--large" phx-click="toggle-component-visibility" phx-value-component-expand="metadata_form">Back</a>
-              <a href="#extract-step-form" id="next-button" class="btn btn--primary-outline btn--next btn--large btn--action" phx-click="toggle-component-visibility" phx-value-component-expand="extract_step_form">Next</a>
-            </div>
           </div>
         </form>
       </div>
