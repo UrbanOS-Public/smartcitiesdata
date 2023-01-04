8657c1f6f2e443407512366a1c65475b2d4372ac
Chris Duncan <c.m.duncan@accenture.com>
2023-01-04 15:56:30 -0600

    version bump

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "2.4.31",
+      version: "2.4.32",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
