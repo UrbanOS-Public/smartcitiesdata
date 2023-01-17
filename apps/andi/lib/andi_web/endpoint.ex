6448c61c2ce77acf9327b131a9e612d9fe691d67
Ian Abbott <ij.abbott2306@gmail.com>
2023-01-17 09:41:24 -0600

    set secure cookie using env var

--- apps/andi/lib/andi_web/endpoint.ex
+++ apps/andi/lib/andi_web/endpoint.ex
@@ -2,6 +2,7 @@ defmodule AndiWeb.Endpoint do
   @session_options [
     store: :cookie,
     key: "_andi_key",
+    secure: String.to_atom(Andi.Application.get_env_variable("SECURE_COOKIE", false)),
     signing_salt: "SekoFX7T"
   ]
 
