225c721846ef8182d835a04c02ddb65e497af3de
Ian Abbott <ij.abbott2306@gmail.com>
2023-01-25 16:04:11 -0600

    version bump

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "2.5.51",
+      version: "2.5.52",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
