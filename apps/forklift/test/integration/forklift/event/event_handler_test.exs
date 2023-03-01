53a4f0f83698939d56b0ac11075ecd0170b469e0
Ryan Conaway <ryan.r.conaway@accenture.com>
2023-02-28 19:25:08 -0500

    wip

--- apps/forklift/test/integration/forklift/event/event_handler_test.exs
+++ apps/forklift/test/integration/forklift/event/event_handler_test.exs
@@ -125,42 +125,42 @@ defmodule Forklift.Event.EventHandlerTest do
   end
 
   #
-  #  describe "Migration Last Insert Date Start" do
-  #    test "A failing message gets placed on dead letter queue and discarded" do
-  #      id_for_fake_event = UUID.uuid4()
-  #
-  #      id_for_valid_dataset = UUID.uuid4()
-  #      valid_dataset = TDG.create_dataset(%{id: id_for_valid_dataset})
-  #      allow(Redix.command!(:redix, ["KEYS", "forklift:last_insert_date:*"]), exec: fn _ -> raise "nope" end)
-  #
-  #      Brook.Event.send(@instance_name, "migration:last_insert_date:start", __MODULE__, %{fake_event: id_for_fake_event})
-  #      Brook.Event.send(@instance_name, dataset_update(), __MODULE__, valid_dataset)
-  #
-  #      eventually(fn ->
-  #        cached_dataset = Forklift.Datasets.get!(id_for_valid_dataset)
-  #
-  #        failed_messages =
-  #          Elsa.Fetch.fetch(elsa_brokers(), "dead-letters")
-  #          |> elem(2)
-  #          |> Enum.filter(fn message ->
-  #            actual = Jason.decode!(message.value)
-  #
-  #            case actual["original_message"] do
-  #              %{"data" => %{"fake_event" => fake_id}} ->
-  #                fake_id == id_for_fake_event
-  #
-  #              _ ->
-  #                false
-  #            end
-  #          end)
-  #
-  #        assert cached_dataset != nil
-  #        assert cached_dataset.id == id_for_valid_dataset
-  #        assert 1 == length(failed_messages)
-  #      end)
-  #    end
-  #  end
-  #
+  describe "Migration Last Insert Date Start" do
+    test "A failing message gets placed on dead letter queue and discarded" do
+      id_for_fake_event = UUID.uuid4()
+
+      id_for_valid_dataset = UUID.uuid4()
+      valid_dataset = TDG.create_dataset(%{id: id_for_valid_dataset})
+      allow(Redix.command!(:redix, ["KEYS", "forklift:last_insert_date:*"]), exec: fn _ -> raise "nope" end)
+
+      Brook.Event.send(@instance_name, "migration:last_insert_date:start", __MODULE__, %{fake_event: id_for_fake_event})
+      Brook.Event.send(@instance_name, dataset_update(), __MODULE__, valid_dataset)
+
+      eventually(fn ->
+        cached_dataset = Forklift.Datasets.get!(id_for_valid_dataset)
+
+        failed_messages =
+          Elsa.Fetch.fetch(elsa_brokers(), "dead-letters")
+          |> elem(2)
+          |> Enum.filter(fn message ->
+            actual = Jason.decode!(message.value)
+
+            case actual["original_message"] do
+              %{"data" => %{"fake_event" => fake_id}} ->
+                fake_id == id_for_fake_event
+
+              _ ->
+                false
+            end
+          end)
+
+        assert cached_dataset != nil
+        assert cached_dataset.id == id_for_valid_dataset
+        assert 1 == length(failed_messages)
+      end)
+    end
+  end
+
   #  describe "Dataset Delete Start" do
   #    test "A failing message gets placed on dead letter queue and discarded" do
   #      id_for_invalid_dataset = UUID.uuid4()
