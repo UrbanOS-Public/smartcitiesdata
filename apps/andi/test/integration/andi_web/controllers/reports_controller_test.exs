a3c47fede2166f2ed0e88726f49cebb071e6e218
Mikey C <m.a.cox@accenture.com>
2023-03-17 10:59:33 -0400

    update integration test

--- apps/andi/test/integration/andi_web/controllers/reports_controller_test.exs
+++ apps/andi/test/integration/andi_web/controllers/reports_controller_test.exs
@@ -7,7 +7,7 @@ defmodule Andi.ReportsControllerTest do
     test "returns csv file when called", %{curator_conn: conn} do
       result = get(conn, "/report")
       assert result.status == 200
-      assert result.resp_body == "test,csv\r\n"
+      assert result.resp_body == "Dataset ID,Users\r\n12345,\"user1@fakemail.com, user2@fakemail.com\"\r\n"
     end
   end
 end
