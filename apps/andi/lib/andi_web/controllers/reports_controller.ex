29ab2b51fdf1bdc6fc67e221562a9d8e0afb2a6f
Mikey C <m.a.cox@accenture.com>
2023-03-17 10:51:11 -0400

    make blank csv formatted better

--- apps/andi/lib/andi_web/controllers/reports_controller.ex
+++ apps/andi/lib/andi_web/controllers/reports_controller.ex
@@ -4,7 +4,8 @@ defmodule AndiWeb.ReportsController do
   access_levels(download_report: [:private])
 
   def download_report(conn, _params) do
-    csv = CSV.encode([["test", "csv"]]) |> Enum.to_list() |> to_string()
+    # Note: Each list is a different row
+    csv = CSV.encode([["Dataset ID", "Users"], ["12345", "user1@fakemail.com, user2@fakemail.com"]]) |> Enum.to_list() |> to_string()
 
     conn
     |> put_resp_content_type("text/csv")
