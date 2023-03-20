9cadd7bbec8f5690327636a5311001ef53c549b1
Mikey C <m.a.cox@accenture.com>
2023-03-20 09:37:08 -0400

    add logic and formatting to reports controller and tests

--- apps/andi/lib/andi_web/controllers/reports_controller.ex
+++ apps/andi/lib/andi_web/controllers/reports_controller.ex
@@ -1,15 +1,56 @@
 defmodule AndiWeb.ReportsController do
   use AndiWeb, :controller
+  import Ecto.Query, only: [from: 2]
+  alias Andi.Schemas.User
+  alias Andi.InputSchemas.Datasets
+  alias Andi.InputSchemas.Datasets.Dataset
 
   access_levels(download_report: [:private])
 
   def download_report(conn, _params) do
     # Note: Each list is a different row
-    csv = CSV.encode([["Dataset ID", "Users"], ["12345", "user1@fakemail.com, user2@fakemail.com"]]) |> Enum.to_list() |> to_string()
-
+    csv = CSV.encode(build_csv()) |> Enum.to_list() |> to_string()
     conn
     |> put_resp_content_type("text/csv")
     |> put_resp_header("content-disposition", "attachment; filename=\"record.csv\"")
     |> resp(200, csv)
+  end
+
+  defp build_csv() do
+    values = get_dataset_ids()
+    |> Enum.map(fn dataset_id -> [dataset_id, get_users_with_dataset_id(dataset_id)] end)
+    [["Dataset ID", "Users"] | values]
+  end
+
+  defp get_users_with_dataset_id(id) do
+    get_users()
+    |> Enum.filter(fn user -> Enum.any?(user.userdatasetids, fn user_dataset_id -> user_dataset_id == id end) end)
+    |> Enum.map(fn user -> user.email end)
+    |> Enum.join(", ")
+  end
+
+  defp get_dataset_ids() do
+    query =
+      from(dataset in Dataset,
+      select: dataset
+      )
+
+      Andi.Repo.all(query)
+      |> get_dataset_ids()
+  end
+
+  defp get_dataset_ids(datasets) do
+    datasets |> Enum.map(fn dataset -> dataset.id end)
+  end
+
+  defp get_users() do
+    query =
+      from(user in User,
+        join: d in assoc(user, :datasets),
+        preload: [datasets: d]
+      )
+
+    Andi.Repo.all(query)
+    |> Enum.map(fn user -> %{email: user.email, userdatasetids: get_dataset_ids(user.datasets)} end)
   end
 end
--- apps/andi/test/integration/andi_web/controllers/reports_controller_test.exs
+++ apps/andi/test/integration/andi_web/controllers/reports_controller_test.exs
@@ -1,13 +1,55 @@
 defmodule Andi.ReportsControllerTest do
   use ExUnit.Case
   use Andi.DataCase
+  use Placebo
   use AndiWeb.Test.AuthConnCase.IntegrationCase
+  alias Andi.InputSchemas.Datasets
+  alias Andi.InputSchemas.Datasets.Dataset
+  alias Andi.Schemas.User
 
   describe "download_report" do
     test "returns csv file when called", %{curator_conn: conn} do
+      dataset1 = %Dataset{id: "12345"}
+      user = %User{
+        subject_id: UUID.uuid4(),
+        email: "user1@fakemail.com",
+        name: "user1",
+        datasets: [
+          dataset1
+        ]
+      }
+
+      allow Andi.Repo.all(any()), seq: [[dataset1], [user]]
       result = get(conn, "/report")
       assert result.status == 200
-      assert result.resp_body == "Dataset ID,Users\r\n12345,\"user1@fakemail.com, user2@fakemail.com\"\r\n"
+      assert result.resp_body == "Dataset ID,Users\r\n12345,user1@fakemail.com\r\n"
+    end
+
+    test "filters users to their respective dataset ids", %{curator_conn: conn} do
+      dataset1 = %Dataset{id: "12345"}
+      dataset2 = %Dataset{id: "6789"}
+      user1 = %User{
+        subject_id: UUID.uuid4(),
+        email: "user1@fakemail.com",
+        name: "user1",
+        datasets: [
+          dataset1,
+          dataset2
+        ]
+      }
+      user2 = %User{
+        subject_id: UUID.uuid4(),
+        email: "user2@fakemail.com",
+        name: "user2",
+        datasets: [
+          dataset1
+        ]
+      }
+
+      allow Andi.Repo.all(any()), seq: [[dataset1, dataset2], [user1, user2]]
+      result = get(conn, "/report")
+      assert result.status == 200
+      assert result.resp_body == "Dataset ID,Users\r\n12345,\"user1@fakemail.com, user2@fakemail.com\"\r\n6789,user1@fakemail.com\r\n"
     end
   end
 end
