dd8b01a03b395d085b79e8fae55ab30eeeef1c9c
Michael Cox <51802118+accenture-mikeyc@users.noreply.github.com>
2023-03-31 15:16:05 -0400

    add not equals operation option and add string operation mapper for conditional transform (#1641)

--- apps/andi/lib/andi_web/live/ingestion_live_view/transformations/transformation_field_builder.ex
+++ apps/andi/lib/andi_web/live/ingestion_live_view/transformations/transformation_field_builder.ex
@@ -27,7 +27,7 @@ defmodule AndiWeb.IngestionLiveView.Transformations.TransformationFieldBuilder d
       </div>
       <div>
         <%= label(form, :conditionOperation, "Comparison", class: "label label--required", for: "transformation_condition_#{@id}__comparison") %>
-        <%= select(form, :conditionOperation, ["", "Is Equal To", "Is Greater Than", "Is Less Than"], [value: get_in(form.source.changes, [:parameters, :conditionOperation]), id: "transformation_condition_#{@id}__comparison", class: "select transformation-type", required: true]) %>
+        <%= select(form, :conditionOperation, ["", "Is Equal To", "Is Not Equal To", "Is Greater Than", "Is Less Than"], [value: get_in(form.source.changes, [:parameters, :conditionOperation]), id: "transformation_condition_#{@id}__comparison", class: "select transformation-type", required: true]) %>
         <%= ErrorHelpers.error_tag(form.source, :conditionOperation, bind_to_input: false, id: "#{@id}_transformation_condition_comparison_error") %>
       </div>
       <div>
