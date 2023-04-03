13ccde07eae648350c20f536dcd49efc03ddfb27
Mikey C <m.a.cox@accenture.com>
2023-04-03 11:49:02 -0400

    fixed issue with caps in data type strings

--- apps/transformers/mix.exs
+++ apps/transformers/mix.exs
@@ -4,7 +4,7 @@ defmodule Transformers.MixProject do
   def project do
     [
       app: :transformers,
-      version: "1.0.20",
+      version: "1.0.21",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
