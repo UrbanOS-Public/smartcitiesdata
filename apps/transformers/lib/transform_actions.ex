4d279fde7c66a40cd28e3ed008dc9942aa272df1
nicholas.goodwin <nicholas.goodwin@accenture.com>
2023-04-11 13:39:04 -0500

    added in flattening and split payloads for transformation

--- apps/transformers/lib/transform_actions.ex
+++ apps/transformers/lib/transform_actions.ex
@@ -32,8 +32,19 @@ defmodule Transformers do
     end)
   end
 
+  def perform(operations, initial_payload) do
+    if(OperationUtils.allOperationsItemsAreFunctions(operations)) do
+      executeOperations(operations, initial_payload)
+    else
+      IO.inspect(operations, label: "Error occurred executing these ops")
+      {:error, "Invalid list of functions passed to performTransformations"}
+    end
+  end
+
   defp executeOperations(operations, initial_payload) do
-    Enum.reduce_while(operations, {:ok, initial_payload}, fn op, {:ok, acc_payload} ->
+    flatten_payload = flatten_payload(initial_payload)
+
+    result_payload = Enum.reduce_while(operations, {:ok, flatten_payload}, fn op, {:ok, acc_payload} ->
       case op.(acc_payload) do
         {:ok, result} ->
           {:cont, {:ok, result}}
@@ -42,14 +53,56 @@ defmodule Transformers do
           {:halt, {:error, reason}}
       end
     end)
+
+    case result_payload do
+      {:ok, payload} -> {:ok, split_payload(payload)}
+      error -> error
+    end
+
   end
 
-  def perform(operations, initial_payload) do
-    if(OperationUtils.allOperationsItemsAreFunctions(operations)) do
-      executeOperations(operations, initial_payload)
-    else
-      IO.inspect(operations, label: "Error occurred executing these ops")
-      {:error, "Invalid list of functions passed to performTransformations"}
+  defp flatten_payload(payload, parent_key \\ "") do
+    Enum.reduce(payload, %{}, fn {key, value}, acc ->
+      case value do
+        value when is_map(value) ->
+          child_payload = flatten_payload(value, concat_key(key, parent_key))
+          Map.merge(acc, child_payload)
+        value when is_list(value) ->
+          IO.inspect("Nicholas - list")
+          acc
+        value -> Map.put(acc, concat_key(key, parent_key), value)
+      end
+    end)
+  end
+
+  defp concat_key(key, parent_key) do
+    case parent_key do
+      "" -> key
+      _ -> "#{parent_key}.#{key}"
+    end
+  end
+
+  defp split_payload(payload) do
+    Enum.reduce(payload, %{}, fn {key, value}, acc ->
+      case String.split(key, ".") do
+        hierarchy when length(hierarchy) == 1 -> Map.put(acc, hd(hierarchy), value)
+        hierarchy ->
+          {parent_key, child_hierarchy} = List.pop_at(hierarchy, 0)
+          parent_map = Map.get(acc, parent_key, %{})
+
+          updated_parent_map = create_child_map(child_hierarchy, value)
+            |> Map.merge(parent_map)
+
+          Map.put(acc, parent_key, updated_parent_map)
+      end
+    end)
+  end
+
+  defp create_child_map(hierarchy, value) do
+    {parent_key, child_hierarchy} = List.pop_at(hierarchy, 0)
+    case hierarchy do
+      hierarchy when length(hierarchy) == 1 -> Map.new([{hd(hierarchy), value}])
+      _ -> Map.new([{parent_key, create_child_map(child_hierarchy, value)}])
     end
   end
 end
