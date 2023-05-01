fe26581fb2dabc0d4f79fe86d63e8ef332b20e6c
nicholas-goodwin <111888703+nicholas-goodwin@users.noreply.github.com>
2023-05-01 14:29:25 -0500

    added support for nested lists (#1670)
    * added support for nested lists
    
    * formatting and version bump
    
    * removed list nested maps tests
    
    * handled nested lists within maps
    
    * formatting
    
    * version bump alchemist and andi to get new transformations version

--- apps/transformers/test/unit/transformation_action_test.exs
+++ apps/transformers/test/unit/transformation_action_test.exs
@@ -1,6 +1,8 @@
 defmodule TransformationActionTest do
   use ExUnit.Case
 
+  import Checkov
+
   alias Transformers
   alias SmartCity.TestDataGenerator, as: TDG
 
@@ -182,63 +184,116 @@ defmodule TransformationActionTest do
       assert reason == "Missing field in payload: [INVALID]"
     end
 
-    test "should perform operations with lists" do
+    data_test "should flatten and split correctly: #{test_name}" do
       transformation1 =
         TDG.create_transformation(%{
           name: "sample",
           type: "add",
           parameters: %{
             "condition" => "false",
-            "addends" => "5, parent_list[0].child",
-            "targetField" => "parent.add"
+            "addends" => addends,
+            "targetField" => "target_field"
           },
           sequence: 0
         })
 
-      transformation2 =
-        TDG.create_transformation(%{
-          name: "sample",
-          type: "concatenation",
-          parameters: %{
-            "condition" => "false",
-            "sourceFields" => "one, parent.child.two",
-            "separator" => ", ",
-            "targetField" => "concat"
-          },
-          sequence: 1
-        })
-
       operations =
-        [transformation1, transformation2]
+        [transformation1]
         |> Transformers.construct()
 
-      initial_payload = %{
-        "one" => "something",
-        "parent" => %{
-          "child" => %{
-            "two" => "else"
-          }
-        },
-        "parent_list" => [
-          %{"child" => 9}
+      {:ok, resulting_payload} = Transformers.perform(operations, payload)
+
+      expected_payload = Map.put(payload, "target_field", 7)
+
+      assert expected_payload == resulting_payload
+
+      where([
+        [:test_name, :payload, :addends],
+        ["single level", %{"foo" => 4, "bar" => 3}, "foo, bar"],
+        ["1 level map child", %{"parent" => %{"foo" => 4}, "bar" => 3}, "parent.foo, bar"],
+        [
+          "2 level map child",
+          %{"parent" => %{"child" => %{"foo" => 4}}, "bar" => 3},
+          "parent.child.foo, bar"
+        ],
+        [
+          "both are inside map with doubled value",
+          %{"parent" => %{"child" => %{"foo" => 4}, "bar" => 3}, "bar" => 34542},
+          "parent.child.foo, parent.bar"
+        ],
+        ["get first value of list", %{"parent" => [4, 2_438_509], "bar" => 3}, "parent[0], bar"],
+        [
+          "fully nested lists",
+          %{"list" => [[135_893, 4], [23425, 2842]], "list2" => [[3324, 54353], [3_452_435, 3]]},
+          "list[0][1], list2[1][1]"
+        ],
+        [
+          "nested lists within maps",
+          %{"parent" => %{"child" => %{"list" => [[4, 13243], [342_134, 3]]}}},
+          "parent.child.list[0][0], parent.child.list[1][1]"
+        ],
+        [
+          "multiple objects in list",
+          %{
+            "list" => [[135_893, 4], [23425, 2842]],
+            "list2" => [[3324, 54353], [3_452_435, 3_321_421]],
+            "parent" => %{"child" => %{"foo" => 3}}
+          },
+          "list[0][1], parent.child.foo"
+        ],
+        [
+          "a large number of nests within a lists",
+          %{
+            "list" => [
+              [
+                [
+                  [
+                    [1, 4],
+                    [24, 543]
+                  ],
+                  [
+                    [134, 4653],
+                    [234_231, 2_353_454]
+                  ]
+                ],
+                [
+                  [
+                    [1_023_983, 239_845],
+                    [45332, 43545]
+                  ],
+                  [
+                    [3, 9],
+                    [3242, 44]
+                  ]
+                ]
+              ],
+              [
+                [
+                  [
+                    [1234, 546],
+                    [12312, 5_439_645]
+                  ],
+                  [
+                    [2, 55],
+                    [4665, 76754]
+                  ]
+                ],
+                [
+                  [
+                    [93_543_245, 8_141_232],
+                    [4564, 4567]
+                  ],
+                  [
+                    [10_214_235, 143_534_251],
+                    [124_112, 153_425_343]
+                  ]
+                ]
+              ]
+            ]
+          },
+          "list[0][0][0][0][1], list[0][1][1][0][0]"
         ]
-      }
-
-      {:ok, resulting_payload} = Transformers.perform(operations, initial_payload)
-
-      assert resulting_payload == %{
-               "one" => "something",
-               "parent" => %{
-                 "add" => 14,
-                 "child" => %{
-                   "two" => "else"
-                 }
-               },
-               "parent_list" => [
-                 %{"child" => 9}
-               ],
-               "concat" => "something, else"
-             }
+      ])
     end
 
     test "should access nested source values when specified in conditionals" do
@@ -276,48 +331,6 @@ defmodule TransformationActionTest do
 
       assert resulting_payload == %{
                "sourceTestField" => %{"inner" => "knownValue"},
-               "targetTestField" => %{"inner" => "knownValue"},
-               "parent" => %{
-                 "child" => 14
-               }
-             }
-    end
-
-    test "should access listed source values when specified in conditionals" do
-      transformation1 =
-        TDG.create_transformation(%{
-          name: "sample",
-          type: "add",
-          parameters: %{
-            "condition" => "true",
-            "conditionCompareTo" => "Target Field",
-            "conditionDataType" => "string",
-            "sourceConditionField" => "sourceTestField[0].inner",
-            "conditionOperation" => "=",
-            "targetConditionField" => "targetTestField.inner",
-            "targetConditionValue" => nil,
-            "addends" => [5, "parent.child"],
-            "targetField" => "parent.child"
-          },
-          sequence: 0
-        })
-
-      operations =
-        [transformation1]
-        |> Transformers.construct()
-
-      initial_payload = %{
-        "sourceTestField" => [%{"inner" => "knownValue"}],
-        "targetTestField" => %{"inner" => "knownValue"},
-        "parent" => %{
-          "child" => 9
-        }
-      }
-
-      {:ok, resulting_payload} = Transformers.perform(operations, initial_payload)
-
-      assert resulting_payload == %{
-               "sourceTestField" => [%{"inner" => "knownValue"}],
                "targetTestField" => %{"inner" => "knownValue"},
                "parent" => %{
                  "child" => 14
