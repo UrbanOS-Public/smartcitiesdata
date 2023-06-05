76dd930c8cef8280e396d92956aec57595b11f7d
ian-j-abbott-accenture <79878368+ian-j-abbott-accenture@users.noreply.github.com>
2023-06-05 17:03:47 -0500

    Kafka error fix attempt (#1695)
    * discard all kafka messages in event_handler
    
    * version bump
    
    * format
    
    ---------
    
    Co-authored-by: Ian Abbott <ij.abbott2306@gmail.com>

--- apps/forklift/mix.exs
+++ apps/forklift/mix.exs
@@ -4,7 +4,7 @@ defmodule Forklift.MixProject do
   def project do
     [
       app: :forklift,
-      version: "0.19.15",
+      version: "0.19.16",
       elixir: "~> 1.10",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
