062cc88638c0476e73b6de519166647e217eaeb5
Ian Abbott <ij.abbott2306@gmail.com>
2023-08-31 15:23:48 -0500

    format

--- apps/e2e/test/e2e_test.exs
+++ apps/e2e/test/e2e_test.exs
@@ -350,13 +350,11 @@ defmodule E2ETest do
   # This series of tests should be extended as more apps are added to the umbrella.
   describe "ingested data" do
     test "is written by reaper", %{ingestion: ingestion} do
-      IO.inspect(ingestion, label: "ingestion oooo")
       allow(Reaper.Cache.cache(any(), any()), exec: fn _, _ -> Process.sleep(30000) end)
       topic = "#{Application.get_env(:reaper, :output_topic_prefix)}-#{ingestion["id"]}"
 
       eventually(fn ->
-        {:ok, _, [message, end_of_data_message]} =
-          Elsa.fetch(@brokers, topic) |> IO.inspect(label: "brokers")
+        {:ok, _, [message, end_of_data_message]} = Elsa.fetch(@brokers, topic)
 
         {:ok, data} = SmartCity.Data.new(message.value)
 
