ac2097232c7ec1728fe2a731e37acad307c95cd4
Ian Abbott <ij.abbott2306@gmail.com>
2023-09-11 13:48:29 -0500

    remove IO inspect

--- apps/andi/priv/repo/migrations/20230907190131_modify_event_log_timestamp_type.exs
+++ apps/andi/priv/repo/migrations/20230907190131_modify_event_log_timestamp_type.exs
@@ -2,7 +2,6 @@ defmodule Andi.Repo.Migrations.ModifyEventLogTimestampType do
   use Ecto.Migration
 
   def change do
-    IO.inspect("running migration")
     alter table(:event_log) do
       modify :timestamp, :utc_datetime_usec, from: :utc_datetime
     end
