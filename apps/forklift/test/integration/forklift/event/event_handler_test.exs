75e4efb25cf376bb598de9160b9a9a4eff78a6aa
RyanRConaway <79863335+RyanRConaway@users.noreply.github.com>
2023-09-28 12:00:24 -0400

    Initializing the data reader on every data_extract_start within forklift (#1722)
    * Initializing the data reader on every data_extract_start within forklift

--- apps/forklift/test/integration/forklift/event/event_handler_test.exs
+++ apps/forklift/test/integration/forklift/event/event_handler_test.exs
@@ -226,4 +226,46 @@ defmodule Forklift.Event.EventHandlerTest do
       end)
     end
   end
+
+  describe "Data Extract Start" do
+    test "A failing message gets placed on dead letter queue and discarded" do
+      id_for_invalid_ingestion = UUID.uuid4()
+      id_for_target_dataset = UUID.uuid4()
+      invalid_ingestion = TDG.create_ingestion(%{id: id_for_invalid_ingestion, targetDatasets: [id_for_target_dataset]})
+
+      id_for_valid_dataset = UUID.uuid4()
+      valid_dataset = TDG.create_dataset(%{id: id_for_valid_dataset})
+      allow(Brook.get!(@instance_name, :datasets, id_for_target_dataset), exec: fn _, _, _ -> raise "nope" end)
+
+      Brook.Event.send(@instance_name, data_extract_start(), __MODULE__, invalid_ingestion)
+      Brook.Event.send(@instance_name, dataset_update(), __MODULE__, valid_dataset)
+
+      eventually(fn ->
+        cached_dataset_id =
+          case Brook.ViewState.get(@instance_name, :datasets, id_for_valid_dataset) do
+            {:ok, %{id: id}} -> id
+            _ -> nil
+          end
+
+        failed_messages =
+          Elsa.Fetch.fetch(elsa_brokers(), "dead-letters")
+          |> elem(2)
+          |> Enum.filter(fn message ->
+            actual = Jason.decode!(message.value)
+
+            case actual["original_message"] do
+              %{"id" => message_dataset_id} ->
+                message_dataset_id == id_for_invalid_ingestion
+
+              _ ->
+                false
+            end
+          end)
+
+        assert cached_dataset_id != nil
+        assert cached_dataset_id == id_for_valid_dataset
+        assert 1 == length(failed_messages)
+      end)
+    end
+  end
 end
