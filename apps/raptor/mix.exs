7fb3b8cf7fb9935f4cc923208147286e13fba851
ian-j-abbott-accenture <79878368+ian-j-abbott-accenture@users.noreply.github.com>
2024-07-02 13:14:51 -0500

    bump remaining application versions to trigger new image builds (#1737)
    * bump remaining application versions to trigger new image builds
    
    * swap zookeeper image to official image

--- apps/raptor/mix.exs
+++ apps/raptor/mix.exs
@@ -5,7 +5,7 @@ defmodule Raptor.MixProject do
     [
       app: :raptor,
       compilers: [:phoenix] ++ Mix.compilers(),
-      version: "1.3.3",
+      version: "1.3.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
