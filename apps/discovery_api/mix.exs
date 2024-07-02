4d0f3a91b8403f06c808103583f6e029fdc4ffe0
Ian Abbott <ian.j.abbott@accenture.com>
2024-07-02 12:22:56 -0500

    bump remaining application versions to trigger new image builds

--- apps/discovery_api/mix.exs
+++ apps/discovery_api/mix.exs
@@ -5,7 +5,7 @@ defmodule DiscoveryApi.Mixfile do
     [
       app: :discovery_api,
       compilers: [:phoenix, :gettext | Mix.compilers()],
-      version: "1.3.18",
+      version: "1.3.19",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
