8e432c1a17fbf53f55549c42d5fdd571fa3a7f14
Ian Abbott <ian.j.abbott@accenture.com>
2025-01-24 14:44:58 -0600

    adjust rm command in integration script

--- scripts/gh-action-integration-test.sh
+++ scripts/gh-action-integration-test.sh
@@ -5,7 +5,7 @@ DOCKER_COMPOSE_VERSION=1.29.2
 
 app="$1"
 
-sudo rm /usr/local/bin/docker-compose
+sudo rm -f /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
