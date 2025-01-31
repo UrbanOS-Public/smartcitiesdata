78a7e546e598de8e89aa5d358bae324ce20ff8bb
ian-j-abbott-accenture <79878368+ian-j-abbott-accenture@users.noreply.github.com>
2025-01-31 15:05:47 -0600

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,3 +9,8 @@ echo "Logging into DockerHub..."
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 docker push smartcitiesdata/$app:$version
+
+echo "Logging into Quay..."
+echo "${QUAY_PASSWORD}" | docker login -u "${QUAY_USERNAME}" --password-stdin quay.io
+
+docker push quay.io/urbanos/$app:$version
