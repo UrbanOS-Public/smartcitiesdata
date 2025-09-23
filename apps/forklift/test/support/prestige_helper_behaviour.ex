e113ea67451bcf793070ae39c26573782ca1696c
Robert Seward <rseward@bluestone-consulting.com>
2025-09-23 18:39:43 +0000

    Unit test bug fix for gh actions run

--- apps/forklift/test/support/prestige_helper_behaviour.ex
+++ apps/forklift/test/support/prestige_helper_behaviour.ex
@@ -1,3 +1,4 @@
 defmodule Forklift.Test.PrestigeHelperBehaviour do
   @callback count_query(String.t()) :: {:ok, integer()} | {:error, term()}
+  @callback table_exists?(String.t()) :: boolean()
 end
