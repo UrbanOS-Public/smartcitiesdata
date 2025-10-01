d93b43b1cab82006e741eb8ec4c267430eba3c28
Robert Seward <rseward@bluestone-consulting.com>
2025-10-01 14:24:29 +0000

    Resolving discovery_api deployment issues.

--- apps/discovery_api/test/integration/discovery_api/search/dataset_index_test.exs
+++ apps/discovery_api/test/integration/discovery_api/search/dataset_index_test.exs
@@ -948,9 +948,11 @@ defmodule DiscoveryApi.Data.Search.DatasetIndexTest do
 
     Brook.Event.send(@instance_name, "dataset:update", __MODULE__, dataset)
 
+    # Wait longer for Brook event processing to complete
     eventually(fn ->
-      assert nil != Model.get(dataset.id)
-    end)
+      model = Model.get(dataset.id)
+      assert nil != model, "Dataset #{dataset.id} not found after event processing"
+    end, 2000, 20) # Increase timeout: 2 seconds * 20 = 40 seconds total
 
     dataset
   end
