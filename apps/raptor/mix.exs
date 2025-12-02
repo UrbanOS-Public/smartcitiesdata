521c8014874ba391e489d43f700a7ae6a3d399cf
Robert Seward <rseward@bluestone-consulting.com>
2025-12-02 21:20:23 +0000

    Raptor 2.0.1 image build.

--- apps/raptor/mix.exs
+++ apps/raptor/mix.exs
@@ -5,7 +5,7 @@ defmodule Raptor.MixProject do
     [
       app: :raptor,
       compilers: [:phoenix] ++ Mix.compilers(),
-      version: "2.0.0",
+      version: "2.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
