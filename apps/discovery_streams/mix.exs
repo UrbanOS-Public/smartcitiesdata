4579c9695fba3060df9db09f89d6c10a79078e86
Robert Seward <rseward@bluestone-consulting.com>
2025-12-05 22:15:30 +0000

    Reaper v3.0.2 for the win!

--- apps/discovery_streams/mix.exs
+++ apps/discovery_streams/mix.exs
@@ -4,7 +4,7 @@ defmodule DiscoveryStreams.Mixfile do
   def project do
     [
       app: :discovery_streams,
-      version: "4.0.0",
+      version: "4.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
@@ -35,7 +35,9 @@ defmodule DiscoveryStreams.Mixfile do
   defp deps do
     [
       {:annotated_retry, in_umbrella: true},
-      {:brook_stream, "~> 1.0"},
+      {:brook_stream,
+       git: "https://github.com/UrbanOS-Public/brook_stream.git",
+       branch: "20251205-v1.0.0-handle-already-started"},
       {:bypass, "~> 2.0", only: [:test, :integration]},
       {:cachex, "~> 3.4"},
       {:checkov, "~> 1.0", only: [:dev, :test, :integration]},
