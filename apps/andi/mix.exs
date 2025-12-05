0f5d2980746de4a1cee67407c2817921b2c67866
Robert Seward <rseward@bluestone-consulting.com>
2025-12-05 22:21:08 +0000

    Reaper v3.0.2 for the win!

--- apps/andi/mix.exs
+++ apps/andi/mix.exs
@@ -4,7 +4,7 @@ defmodule Andi.MixProject do
   def project do
     [
       app: :andi,
-      version: "3.0.0",
+      version: "3.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
@@ -39,7 +39,9 @@ defmodule Andi.MixProject do
       {:accessible, "~> 0.2.1"},
       {:atomic_map, "~> 0.9"},
       {:auth, in_umbrella: true},
-      {:brook_stream, "~> 1.0"},
+      {:brook_stream,
+       git: "https://github.com/UrbanOS-Public/brook_stream.git",
+       branch: "20251205-v1.0.0-handle-already-started"},
       {:bypass, "~> 2.0", only: [:test, :integration]},
       {:checkov, "~> 1.0", only: [:dev, :test, :integration]},
       # {:cowlib, "== 2.12.1", override: true},
