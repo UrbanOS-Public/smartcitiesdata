cbfa811164939a316282b905955d23f8200bdd70
Robert Seward <rseward@bluestone-consulting.com>
2025-12-05 22:08:01 +0000

    Reaper v3.0.2 for the win!

--- apps/alchemist/mix.exs
+++ apps/alchemist/mix.exs
@@ -4,7 +4,7 @@ defmodule Alchemist.MixProject do
   def project do
     [
       app: :alchemist,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.14",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
@@ -33,7 +33,9 @@ defmodule Alchemist.MixProject do
 
   defp deps do
     [
-      {:brook_stream, "~> 1.0"},
+      {:brook_stream,
+       git: "https://github.com/UrbanOS-Public/brook_stream.git",
+       branch: "20251205-v1.0.0-handle-already-started"},
       {:cachex, "~> 3.6"},
       {:checkov, "~> 1.0", only: [:dev, :test, :integration]},
       # {:cowlib, "== 2.12.1", override: true},
