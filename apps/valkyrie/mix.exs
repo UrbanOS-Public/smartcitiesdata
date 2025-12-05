4527ec72a6dcb7635089521b15694e45407b484d
Robert Seward <rseward@bluestone-consulting.com>
2025-12-05 21:55:43 +0000

    Reaper v3.0.2 for the win!

--- apps/valkyrie/mix.exs
+++ apps/valkyrie/mix.exs
@@ -4,7 +4,7 @@ defmodule Valkyrie.MixProject do
   def project do
     [
       app: :valkyrie,
-      version: "2.0.7",
+      version: "2.0.8",
       elixir: "~> 1.14",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
@@ -34,7 +34,9 @@ defmodule Valkyrie.MixProject do
 
   defp deps do
     [
-      {:brook_stream, "~> 1.0"},
+      {:brook_stream,
+       git: "https://github.com/UrbanOS-Public/brook_stream.git",
+       branch: "20251205-v1.0.0-handle-already-started"},
       {:cachex, "~> 3.4"},
       {:checkov, "~> 1.0", only: [:dev, :test, :integration]},
       # {:cowlib, "== 2.12.1", override: true},
