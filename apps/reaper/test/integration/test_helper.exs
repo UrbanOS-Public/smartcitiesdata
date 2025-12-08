512444f12d018b3a413adcc38b0df0866ab34dd9
Robert Seward <rseward@bluestone-consulting.com>
2025-12-08 19:11:37 +0000

    Reaper v3.0.3 for the win!

--- apps/reaper/test/integration/test_helper.exs
+++ apps/reaper/test/integration/test_helper.exs
@@ -1,1 +1,14 @@
 ExUnit.start(exclude: [:skip, :performance])
+
+Application.load(:reaper)
+
+Application.spec(:reaper, :applications)
+|> Enum.each(&Application.ensure_all_started/1)
+
+# Ensure tzdata is started for DateTime operations in tests
+Application.ensure_all_started(:tzdata)
+
+# Ensure bypass is started for HTTP mocking in tests
+Application.ensure_all_started(:bypass)
+
+{:ok, _} = TelemetryEvent.Mock.start_link()
