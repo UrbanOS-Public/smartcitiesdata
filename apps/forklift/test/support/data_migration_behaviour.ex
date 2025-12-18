126f41a5e9630de54b4e951895cf528d6d0d8646
Robert Seward <rseward@bluestone-consulting.com>
2025-12-18 16:29:32 +0000

    Resolved forklift integration compile test failure.

--- apps/forklift/test/support/data_migration_behaviour.ex
+++
@@ -1,6 +0,0 @@
-defmodule Forklift.Test.DataMigrationBehaviour do
-  @moduledoc """
-  Test behaviour for mocking data migration operations.
-  """
-  @callback compact(any(), any(), any()) :: {:ok, any()} | {:error, any()}
-end
